18cf54b2d93a021fbcbf538b0492d68f79808b3e
John Yeo <johnyeocx@gmail.com>
2025-01-24 18:22:54 +0000

    fixed search bar

--- frontend/src/services/useAxiosSwr.tsx
+++ frontend/src/services/useAxiosSwr.tsx
@@ -1,24 +1,6 @@
 import useSWR, { SWRConfiguration } from "swr";
-import { useState, useEffect } from "react";
-import { useAuth } from "@clerk/nextjs";
 import { useAxiosInstance } from "./useAxiosInstance";
 import { AppEnv } from "@autumn/shared";
-
-// export function useClerkToken() {
-//   const { getToken } = useAuth();
-//   const [token, setToken] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const fetchToken = async () => {
-//       const fetchedToken = await getToken();
-//       setToken(fetchedToken);
-//     };
-
-//     fetchToken();
-//   }, [getToken]);
-
-//   return token;
-// }
 
 export function useAxiosSWR({
   url,
@@ -49,22 +31,33 @@ export function useAxiosSWR({
   });
 }
 
-// export function useAuthenticatedPostSWR<T>(
-//   url: string,
-//   appEnv: AppEnv,
-//   data: any,
-//   isAuth = true
-// ) {
-//   const axiosInstance = useAxiosInstance(appEnv, isAuth);
+export function useAxiosPostSWR({
+  url,
+  env,
+  data,
+  withAuth = true,
+  options = {},
+}: {
+  url: string;
+  env: AppEnv;
+  data: any;
+  withAuth?: boolean;
+  options?: SWRConfiguration;
+}) {
+  const axiosInstance = useAxiosInstance({ env, isAuth: withAuth });
 
-//   const fetcher = async (url: string) => {
-//     try {
-//       let res = await axiosInstance.post(url, data);
-//       return res.data;
-//     } catch (error) {
-//       throw error;
-//     }
-//   };
+  const fetcher = async (url: string) => {
+    try {
+      const res = await axiosInstance.post(url, data);
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  };
 
-//   return useSWR(url, fetcher);
-// }
+  return useSWR(url, fetcher, {
+    refreshInterval: 0,
+    revalidateOnFocus: false,
+    ...options,
+  });
+}
--- server/src/clean.ts
+++ server/src/clean.ts
@@ -1,15 +1,13 @@
-import { createSupabaseClient } from "./external/supabaseUtils.js";
-import { createPgClient } from "./middleware/envMiddleware.js";
 import dotenv from "dotenv";
+import pg from "pg";
 dotenv.config();
 
 const clean = async () => {
-  const sb = createSupabaseClient();
-  const pg = createPgClient();
-  await pg.connect();
+  const pgCli = new pg.Client(process.env.SUPABASE_CONNECTION_STRING || "");
+  await pgCli.connect();
 
   // 1. Delete custom entitlements not attached to any customer entitlement
-  const { rows } = await pg.query(
+  const { rows } = await pgCli.query(
     `DELETE FROM entitlements
       WHERE is_custom = TRUE
       AND NOT EXISTS (
@@ -20,7 +18,7 @@ const clean = async () => {
   );
 
   // Do the same for prices
-  const { rows: pricesRows } = await pg.query(
+  const { rows: pricesRows } = await pgCli.query(
     `DELETE FROM prices
       WHERE is_custom = TRUE
       AND NOT EXISTS (
@@ -30,8 +28,23 @@ const clean = async () => {
       )`
   );
 
-  await pg.end();
+  const { rows: events } = await pgCli.query(
+    `DELETE FROM events
+      WHERE NOT EXISTS (
+          SELECT 1 
+          FROM customers 
+          WHERE customers.id = events.customer_id
+      )`
+  );
+
+  await pgCli.end();
   console.log("Cleaned up custom entitlements and prices");
 };
 
-clean();
+const init = async () => {
+  await clean();
+  dotenv.config({ path: ".env.prod", override: true });
+  await clean();
+};
+
+init();
