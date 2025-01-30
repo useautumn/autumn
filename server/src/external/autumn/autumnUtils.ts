ef7ac07d013afd88295151af7962b9c804719ac0
John Yeo <johnyeocx@gmail.com>
2025-01-30 18:06:53 +0000

    made entitled router a POST request

--- server/src/external/autumn/autumnUtils.ts
+++ server/src/external/autumn/autumnUtils.ts
@@ -1,5 +1,11 @@
-import { AppEnv, Organization } from "@autumn/shared";
+import { AppEnv, ErrCode, Organization } from "@autumn/shared";
 import { Autumn } from "./autumnCli.js";
+import RecaseError from "@/utils/errorUtils.js";
+
+export enum FeatureId {
+  Products = "products",
+  Features = "features",
+}
 
 export const sendFeatureEvent = async ({
   org,
@@ -10,9 +16,9 @@ export const sendFeatureEvent = async ({
   env: AppEnv;
   incrementBy: number;
 }) => {
-  // if (env !== AppEnv.Live) {
-  //   return;
-  // }
+  if (env !== AppEnv.Live) {
+    return;
+  }
 
   try {
     const autumn = new Autumn();
@@ -41,9 +47,9 @@ export const sendProductEvent = async ({
   env: AppEnv;
   incrementBy: number;
 }) => {
-  // if (env !== AppEnv.Live) {
-  //   return;
-  // }
+  if (env !== AppEnv.Live) {
+    return;
+  }
 
   try {
     const autumn = new Autumn();
@@ -60,5 +66,46 @@ export const sendProductEvent = async ({
     });
   } catch (error: any) {
     console.log("Failed to send product event", error?.message || error);
+  }
+};
+
+export const isEntitled = async ({
+  orgId,
+  env,
+  featureId,
+}: {
+  orgId: string;
+  env: AppEnv;
+  featureId: FeatureId;
+}) => {
+  if (env !== AppEnv.Live) {
+    return true;
+  }
+
+  const autumn = new Autumn();
+
+  try {
+    const result = await autumn.entitled({
+      customerId: orgId,
+      featureId: featureId,
+    });
+
+    console.log("Result:", result);
+
+    if (result.allowed) {
+      return true;
+    }
+
+    throw new RecaseError({
+      message: `You've used up your allowance for feature ${featureId}. Please upgrade your plan or contact hey@useautumn.com to get more!`,
+      code: ErrCode.InternalError,
+      data: result,
+    });
+  } catch (error: any) {
+    throw new RecaseError({
+      message: "Failed to check entitlement...",
+      code: ErrCode.InternalError,
+      data: error,
+    });
   }
 };
