5957cd28b481dcdd3724ff49463349d1f4757428
Ayush <joesj2905@gmail.com>
2025-02-04 13:24:46 +0000

    tailwind pkg

--- packages/react/src/components/PricingPage.tsx
+++ packages/react/src/components/PricingPage.tsx
@@ -1,25 +1,37 @@
 "use client";
-
+import React from 'react';
 import { useAutumnContext } from "../providers/AutumnContext";
 import { useCustomSwr } from "../hooks/useCustomSwr";
+import { cn } from "../../lib/utils";
 
 // use react swr
 
-export const PricingPage = () => {
-  const { publishableKey } = useAutumnContext();
+interface PricingPageProps {
+  className?: string;
+}
+
+export default function PricingPage({ className }: PricingPageProps) {
 
   const { data, error, isLoading } = useCustomSwr({
-    url: "http://localhost:8080/public/products",
+    url: "https://api.useautumn.com/v1/public/products",
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>Error: {error.message}</div>;
+
+  console.log(data);
 
   return (
-    <div className="bg-blue-500 p-4 rounded">
-      {data.map((product: any) => (
+    <div 
+    className={cn(
+      "border rounded-md p-4 bg-fuchsia-500",
+      className
+    )}
+    // className="border rounded p-4 bg-rose-400 border-blue-300"
+    >
+      {/* {data?.map((product: any) => (
         <div key={product.id}>{product.name}</div>
-      ))}
+      ))} */}
     </div>
   );
-};
+}
