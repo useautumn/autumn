d94c5b4aea1a55dbff7bc3d24ccd6f48318d5888
John Yeo <johnyeocx@gmail.com>
2025-02-04 13:51:54 +0000

    fixed react package

--- frontend/src/app/pricing/page.tsx
+++ frontend/src/app/pricing/page.tsx
@@ -1,13 +1,15 @@
-'use client';
+"use client";
 
 import { AutumnProvider, PricingPage } from "@useautumn/react";
 
 export default function Page() {
   return (
-    <AutumnProvider publishableKey={process.env.NEXT_PUBLIC_AUTUMN_PUBLISHABLE_KEY || ""}>
-    <div className="w-full h-full justify-center items-center flex">
-      <PricingPage className="rounded-full bg-fuchsia-500"/>
-    </div>
+    <AutumnProvider
+      publishableKey={process.env.NEXT_PUBLIC_AUTUMN_PUBLISHABLE_KEY || ""}
+    >
+      <div className="w-full h-full justify-center items-center flex">
+        <PricingPage className="rounded-full bg-fuchsia-500" />
+      </div>
     </AutumnProvider>
   );
 }
