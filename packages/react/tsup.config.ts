f67fc346afb418e78e21710afb61eb4cdb502ad1
John Yeo <johnyeocx@gmail.com>
2025-02-05 12:30:31 +0000

    uploaded react package

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  dts: true,
-  clean: true,
-  banner: {
-    js: "'use client'",
+export default defineConfig([
+  {
+    entry: {
+      "server/index": "src/server/index.ts", // Changed from "server" to "server/index"
+    },
+    dts: true,
+    clean: true,
   },
-});
+  {
+    entry: {
+      index: "src/index.ts",
+    },
+    dts: true,
+    clean: true,
+    banner: {
+      js: "'use client'",
+    },
+  },
+]);
