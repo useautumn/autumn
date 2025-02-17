2c3b934339b2c3d7f87fa39f05e5c5099f4f009e
Ayush <joesj2905@gmail.com>
2025-02-17 18:05:49 +0000

    keywords demo

--- frontend/src/views/demo/DemoView.tsx
+++ frontend/src/views/demo/DemoView.tsx
@@ -5,16 +5,15 @@ import toast from "react-hot-toast";
 import SmallSpinner from "@/components/general/SmallSpinner";
 import { useState, useEffect } from "react";
 import { CustomToaster } from "@/components/general/CustomToaster";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { LoaderCircle } from "lucide-react";
 import { AutumnProvider, PricingPage } from "@useautumn/react";
-import { useAxiosSWR, useDemoSWR } from "@/services/useAxiosSwr";
-import React from "react";
-import { keyToTitle } from "@/utils/formatUtils/formatTextUtils";
+import { useDemoSWR } from "@/services/useAxiosSwr";
 import CustomerBalances from "./CustomerBalances";
+import { Input } from "@/components/ui/input";
 
-const apiKey = "am_live_3ZavonZcha8ENdWwfHbrirU3";
+const apiKey = process.env.NEXT_PUBLIC_AUTUMN_API_KEY;
+const publishableKey = process.env.NEXT_PUBLIC_AUTUMN_PUBLISHABLE_KEY;
 const baseUrl = "https://api.useautumn.com/v1";
 const headers = {
   Authorization: `Bearer ${apiKey}`,
@@ -25,8 +24,18 @@ const axiosInstance = axios.create({
   headers,
 });
 
+const colorizeJSON = (json: any) => {
+  const jsonString = JSON.stringify(json, null, 2);
+  return jsonString.replace(/\btrue\b|\bfalse\b/g, (match) =>
+    match === "true"
+      ? `<span class="text-lime-500">true</span>`
+      : `<span class="text-red-400">false</span>`
+  );
+};
+
 export default function DemoView() {
-  const customerId = "123";
+  const customerId = "ayush";
+  const eventName = "logs";
 
   const {
     data: customer,
@@ -34,18 +43,20 @@ export default function DemoView() {
     mutate: cusMutate,
   } = useDemoSWR({
     url: `/public/customers/${customerId}`,
-    publishableKey: process.env.NEXT_PUBLIC_AUTUMN_PUBLISHABLE_KEY,
+    publishableKey: publishableKey || "",
   });
 
   const hasAccessRequest = {
-    feature_id: "emails",
+    feature_id: eventName,
     customer_id: customerId,
   };
 
   const sendEventRequest = {
-    event_name: "email",
+    event_name: eventName,
     customer_id: customerId,
-    properties: {},
+    properties: {
+      value: 2500,
+    },
   };
 
   const [hasAccessLoading, setHasAccessLoading] = useState(false);
@@ -53,13 +64,15 @@ export default function DemoView() {
   const [sendEventLoading, setSendEventLoading] = useState(false);
   const [sendEventResponse, setSendEventResponse] = useState(null);
   const [loading, setLoading] = useState(true);
-
-  const [hasProFeatures, setHasProFeatures] = useState<boolean>(false);
+  const [message, setMessage] = useState("");
+  const [hasProModels, setHasProModels] = useState<boolean>(false);
+  const [showPremiumModels, setShowPremiumModels] = useState(false);
 
   useEffect(() => {
     const init = async () => {
       setLoading(true);
-      // await checkFeatures();
+      // const hasProModels = await checkPremiumModels();
+      setHasProModels(hasProModels);
       setLoading(false);
     };
     init();
@@ -79,14 +92,12 @@ export default function DemoView() {
     const { data } = await axiosInstance.post("/events", {
       customer_id: customerId,
       event_name: featureId,
-      properties: {},
     });
 
-    toast.success("Scrape successful");
     return data;
   };
 
-  const handleClicked = async (eventName: string) => {
+  const handleClicked = async () => {
     setHasAccessLoading(true);
     const data = await checkAccess(eventName);
     setHasAccessResponse(data);
@@ -105,137 +116,156 @@ export default function DemoView() {
   };
 
   return (
-    <div className="flex gap-12 p-4">
-      <div className="flex flex-col gap-4 w-full">
-        {loading ? (
-          <div className="flex justify-center items-center h-[500px]">
-            <LoaderCircle className="animate-spin text-primary" size={30} />
-          </div>
-        ) : (
-          <>
-            <AutumnProvider
-              publishableKey={
-                process.env.NEXT_PUBLIC_AUTUMN_PUBLISHABLE_KEY || ""
-              }
-            >
-              <PricingPage customerId={customerId} />
-            </AutumnProvider>
-            <CustomToaster />
-
-            <div className="text-lg font-semibold mt-4">Balances</div>
-            <CustomerBalances customer={customer} />
-
-            {/* Use Email Credit */}
-            <div className="text-lg font-semibold mt-4">Actions</div>
-            <div className="flex gap-2">
-              <Button
-                isLoading={hasAccessLoading}
-                onClick={async () => handleClicked("email-credits")}
-                disabled={hasProFeatures === true}
-                className="bg-gradient-to-r from-red-500 via-purple-500 to-blue-500 hover:from-green-500 hover:via-yellow-500 hover:to-pink-500 transition-all duration-700 w-48 shadow-[0_0_15px_rgba(168,85,247,0.5)] hover:shadow-[0_0_20px_rgba(236,72,153,0.7)] bg-[size:200%] hover:bg-right"
-              >
-                Create Email
-              </Button>
+    <div className="w-full flex justify-start pl-12">
+      <div className="flex gap-32 p-4 w-[900px] relative">
+        <div className="flex flex-col gap-4 min-w-[700px]">
+          {loading ? (
+            <div className="flex justify-center items-center h-[500px]">
+              <LoaderCircle className="animate-spin text-primary" size={30} />
             </div>
+          ) : (
+            <>
+              {/* <AutumnProvider publishableKey={publishableKey || ""}>
+                <PricingPage customerId={customerId} />
+              </AutumnProvider> */}
+              <div className="flex gap-2">
+                <Button
+                  variant="gradientPrimary"
+                  onClick={async () => {
+                    const { data } = await axiosInstance.post("/attach", {
+                      customer_id: customerId,
+                      product_id: "pro",
+                      options: [
+                        {
+                          feature_id: "seats",
+                          quantity: 2,
+                        },
+                      ],
+                    });
 
-            {/* <Card>
-              <CardHeader className="flex justify-between p-3 px-4">
-                <div className="flex justify-between items-center gap-4">
-                  <div className="flex flex-col gap-0">
-                    <p className="text-sm font-medium text-gray-600">
-                      Email Balance:
-                    </p>
-                    <span className="text-lg font-semibold">
-                      {emailBalance || 0}
-                    </span>
-                  </div>
-                  <Button
-                    onClick={() => handleClicked("emails")}
-                    className="bg-blue-500 hover:bg-blue-600 transition-colors w-48"
-                  >
-                    Send Email
-                  </Button>
-                </div>
-                <Button onClick={() => handleClicked("ai")}>Use AI</Button>
-              </CardHeader>
-            </Card> */}
-
-            {/* PRO FEATURES */}
-            {/* {hasProFeatures ? (
-              <div>
-                <div className="space-y-4">
-                  <p className="font-semibold text-lg">Pro Analytics</p>
-
-                  <div className="grid grid-cols-2 gap-2">
-                    <Card>
-                      <CardHeader>
-                        <h4 className="text-lg">User Activity</h4>
-                        <div className="flex items-center gap-2">
-                          <div className="h-12 w-12 rounded-full bg-blue-500 flex items-center justify-center">
-                            <span className="text-xl font-bold text-white">
-                              87
-                            </span>
-                          </div>
-                          <div>
-                            <p className="text-sm text-zinc-500">
-                              Active Users
-                            </p>
-                            <p className="text-green-500 text-sm">
-                              ↑ 1% from last week
-                            </p>
-                          </div>
-                        </div>
-                      </CardHeader>
-                    </Card>
-
-                    <Card>
-                      <CardHeader>
-                        <h4 className="text-lg">Engagement</h4>
-                        <div className="flex items-center gap-2">
-                          <div className="h-12 w-12 rounded-full bg-purple-500 flex items-center justify-center">
-                            <span className="text-xl font-bold text-white">
-                              5.2
-                            </span>
-                          </div>
-                          <div>
-                            <p className="text-sm text-zinc-500">
-                              Avg Session (min)
-                            </p>
-                            <p className="text-red-500 text-sm">
-                              ↓ 3% from last week
-                            </p>
-                          </div>
-                        </div>
-                      </CardHeader>
-                    </Card>
-                  </div>
-                </div>
+                    data.checkout_url &&
+                      window.open(data.checkout_url, "_blank");
+                  }}
+                >
+                  Buy Pro
+                </Button>
+                <Button variant="gradientPrimary" onClick={() => {}}>
+                  Buy Team
+                </Button>
               </div>
-            ) : (
-              <div>
-                <p>You do not have access to pro features</p>
+              <CustomToaster />
+
+              <div className="text-lg font-semibold mt-4">Chat</div>
+              <div className="flex gap-2">
+                <Input
+                  placeholder="Enter message"
+                  className="w-full"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                <Button
+                  isLoading={hasAccessLoading}
+                  onClick={async () => handleClicked()}
+                  className="font-semibold bg-gradient-to-b border-b-2 border-red-500 from-red-500 to-red-300 hover:bg-gradient-to-r hover:from-green-500 hover:via-yellow-500 hover:to-pink-500 transition-all duration-700 w-48 shadow-[0_0_15px_rgba(168,85,247,0.5)] hover:shadow-[0_0_20px_rgba(236,72,153,0.7)] bg-[size:200%] hover:bg-right"
+                >
+                  Log
+                </Button>
               </div>
-            )} */}
-          </>
-        )}
-      </div>
-
-      <div className="w-2/4 space-y-4 bg-gray-900 p-4">
-        <APIPlayground
-          title="Check Entitlement"
-          endpoint="GET /entitled"
-          request={hasAccessRequest}
-          response={hasAccessResponse}
-          loading={hasAccessLoading}
-        />
-
-        <APIPlayground
-          title="Send Usage Event"
-          endpoint="POST /events"
-          request={sendEventRequest}
-          response={sendEventResponse}
-          loading={sendEventLoading}
-        />
+              <CustomerBalances customer={customer} />
+              {/* <Card>
+                <CardHeader className="flex justify-between p-3 px-4">
+                  <div className="flex justify-between items-center gap-4">
+                    <div className="flex flex-col gap-0">
+                      <p className="text-sm font-medium text-gray-600">
+                        Email Balance:
+                      </p>
+                      <span className="text-lg font-semibold">
+                        {emailBalance || 0}
+                      </span>
+                    </div>
+                    <Button
+                      onClick={() => handleClicked("emails")}
+                      className="bg-blue-500 hover:bg-blue-600 transition-colors w-48"
+                    >
+                      Send Email
+                    </Button>
+                  </div>
+                  <Button onClick={() => handleClicked("ai")}>Use AI</Button>
+                </CardHeader>
+              </Card> */}
+              {/* PRO FEATURES */}
+              {/* {hasProFeatures ? (
+                <div>
+                  <div className="space-y-4">
+                    <p className="font-semibold text-lg">Pro Analytics</p>
+                    <div className="grid grid-cols-2 gap-2">
+                      <Card>
+                        <CardHeader>
+                          <h4 className="text-lg">User Activity</h4>
+                          <div className="flex items-center gap-2">
+                            <div className="h-12 w-12 rounded-full bg-blue-500 flex items-center justify-center">
+                              <span className="text-xl font-bold text-white">
+                                87
+                              </span>
+                            </div>
+                            <div>
+                              <p className="text-sm text-zinc-500">
+                                Active Users
+                              </p>
+                              <p className="text-green-500 text-sm">
+                                ↑ 1% from last week
+                              </p>
+                            </div>
+                          </div>
+                        </CardHeader>
+                      </Card>
+                      <Card>
+                        <CardHeader>
+                          <h4 className="text-lg">Engagement</h4>
+                          <div className="flex items-center gap-2">
+                            <div className="h-12 w-12 rounded-full bg-purple-500 flex items-center justify-center">
+                              <span className="text-xl font-bold text-white">
+                                5.2
+                              </span>
+                            </div>
+                            <div>
+                              <p className="text-sm text-zinc-500">
+                                Avg Session (min)
+                              </p>
+                              <p className="text-red-500 text-sm">
+                                ↓ 3% from last week
+                              </p>
+                            </div>
+                          </div>
+                        </CardHeader>
+                      </Card>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <p>You do not have access to pro features</p>
+                </div>
+              )} */}
+            </>
+          )}
+        </div>
+        <div className="w-2/4 space-y-4 flex flex-col gap-8 w-[500px] bg-gray-900 p-4 rounded-sm">
+          <APIPlayground
+            title="Check Feature Access"
+            endpoint="GET /entitled"
+            request={hasAccessRequest}
+            response={hasAccessResponse}
+            loading={hasAccessLoading}
+          />
+          <APIPlayground
+            title="Send Usage Event"
+            endpoint="POST /events"
+            request={sendEventRequest}
+            response={sendEventResponse}
+            loading={sendEventLoading}
+          />
+        </div>
       </div>
     </div>
   );
@@ -255,10 +285,9 @@ const APIPlayground = ({
   loading: boolean;
 }) => {
   return (
-    <div className="border border-gray-700 flex flex-col gap-4 p-4 bg-gray-900">
-      <h3 className="font-bold text-white">{title}</h3>
+    <div className="flex flex-col gap-4 bg-gray-900 p-4 rounded-sm">
       <div className="flex flex-col gap-2">
-        <p className="text-sm text-gray-400">Endpoint</p>
+        <p className="text-md font-semibold text-white">{title}</p>
         <pre className="bg-gray-600 p-2 rounded text-sm text-gray-200">
           {endpoint}
         </pre>
@@ -279,7 +308,11 @@ const APIPlayground = ({
           ) : response === null ? (
             "No response"
           ) : (
-            JSON.stringify(response, null, 2)
+            <div
+              dangerouslySetInnerHTML={{
+                __html: colorizeJSON(response),
+              }}
+            />
           )}
         </pre>
       </div>
