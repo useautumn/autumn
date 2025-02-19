a330527e07f16187fb47cc28f860a8dc1f1aae61
John Yeo <johnyeocx@gmail.com>
2025-02-19 19:58:11 +0000

    updated pricing plan and demo view

--- frontend/src/app/demo/[slug]/page.tsx
+++ frontend/src/app/demo/[slug]/page.tsx
@@ -1,7 +1,34 @@
+import { createClient } from "@/utils/supabase/server";
 import DemoView from "@/views/demo/DemoView";
 import DynamicDemoView from "@/views/demo/DynamicDemo";
+import MintDemoView from "@/views/demo/MintDemo";
 
-export default function DemoPage({ params }: { params: { slug: string } }) {
+export default async function DemoPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   // console.log(params);
+
+  const sb = await createClient();
+  const { data, error } = await sb
+    .from("demo")
+    .select("*")
+    .eq("slug", params.slug)
+    .single();
+
+  if (error) {
+    return <div>Not Found...</div>;
+  }
+
+  if (params.slug == "mint") {
+    return (
+      <MintDemoView
+        publishableKey={data.publishable_key}
+        secretKey={data.secret_key}
+      />
+    );
+  }
+
   return <DynamicDemoView />;
 }
---
+++ frontend/src/views/demo/APIPlayground.tsx
@@ -0,0 +1,59 @@
+import SmallSpinner from "@/components/general/SmallSpinner";
+
+export const colorizeJSON = (json: any) => {
+  const jsonString = JSON.stringify(json, null, 2);
+  return jsonString?.replace(/\btrue\b|\bfalse\b/g, (match) =>
+    match === "true"
+      ? `<span class="text-lime-500">true</span>`
+      : `<span class="text-red-400">false</span>`
+  );
+};
+
+export const APIPlayground = ({
+  title,
+  endpoint,
+  request,
+  response,
+  loading,
+}: {
+  title: string;
+  endpoint: string;
+  request: any;
+  response: any;
+  loading: boolean;
+}) => {
+  return (
+    <div className="flex flex-col gap-4 bg-gray-900 p-4 rounded-sm">
+      <div className="flex flex-col gap-2">
+        <p className="text-md font-semibold text-white">{title}</p>
+        <pre className="bg-gray-600 p-2 rounded text-sm text-gray-200">
+          {endpoint}
+        </pre>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <p className="text-sm text-gray-400">Request</p>
+        <pre className="bg-gray-600 p-2 rounded text-sm text-gray-200">
+          {JSON.stringify(request, null, 2)}
+        </pre>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <p className="text-sm text-gray-400">Response</p>
+        <pre className="bg-gray-600 p-2 rounded text-sm text-gray-200">
+          {loading ? (
+            <SmallSpinner />
+          ) : response === null ? (
+            "No response"
+          ) : (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: colorizeJSON(response),
+              }}
+            />
+          )}
+        </pre>
+      </div>
+    </div>
+  );
+};
