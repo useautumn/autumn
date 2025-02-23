972618d0357b92015430fcd1f92a9c41ead634d6
Ayush <joesj2905@gmail.com>
2025-02-23 19:25:29 +0000

    add publishable key to onboarding guide

--- frontend/src/components/ui/step.tsx
+++ frontend/src/components/ui/step.tsx
@@ -14,12 +14,12 @@ function Step({ title, children, className, description }: StepProps) {
   return (
     <div
       className={cn(
-        "relative pl-8 pb-8 border-l-2 border-purple-100 gap-4 flex flex-col",
+        "relative pl-8 pb-8 border-l-2 border-zinc-200 gap-4 flex flex-col",
         className
       )}
     >
       <div className="absolute -left-[17px] -top-1 flex items-center justify-center w-8 h-8 rounded-full bg-stone-50">
-        <FontAwesomeIcon icon={faCoin} className="text-primary w-5 h-5" />
+        <FontAwesomeIcon icon={faCoin} className="text-zinc-500 w-5 h-5" />
       </div>
       <div className="flex flex-col gap-1">
         <h1 className="text-t1 text-lg font-medium">{title}</h1>
