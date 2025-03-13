130842d9bde2054e045ecc97f5cb1dd306d82c26
John Yeo <johnyeocx@gmail.com>
2025-03-13 14:04:24 +0800

    fix: ToolbarButton, forwardRef

--- vite/src/components/general/table-components/ToolbarButton.tsx
+++ vite/src/components/general/table-components/ToolbarButton.tsx
@@ -2,19 +2,23 @@ import { Button, ButtonProps } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/pro-regular-svg-icons";
+import { forwardRef } from "react";
 
-export const ToolbarButton = (props?: ButtonProps) => {
-  return (
-    <Button
-      isIcon
-      variant="ghost"
-      className={cn(
-        "rounded-md !h-4 !w-6 px-0 hover:translate-x-0.5 transition-all duration-100 !bg-transparent !cursor-pointer",
-        props?.className
-      )}
-      {...props}
-    >
-      <FontAwesomeIcon icon={faEllipsisVertical} size="sm" className="w-3" />
-    </Button>
-  );
-};
+export const ToolbarButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    return (
+      <Button
+        ref={ref}
+        isIcon
+        variant="ghost"
+        className={cn(
+          "rounded-md !h-4 !w-6 px-0 hover:translate-x-0.5 transition-all duration-100 !bg-transparent !cursor-pointer",
+          props?.className
+        )}
+        {...props}
+      >
+        <FontAwesomeIcon icon={faEllipsisVertical} size="sm" className="w-3" />
+      </Button>
+    );
+  }
+);
