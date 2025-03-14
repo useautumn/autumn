bd3f36ecb589cbec1621d268af3542be272b0ec8
John Yeo <johnyeocx@gmail.com>
2025-03-14 14:32:21 +0800

    fix: posthog

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -4,20 +4,30 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-posthog.init("phc_L763VL2WY6TExI5xG5xWOua6E5LYkZN496hBeSpEdAn", {
-  api_host: "https://us.i.posthog.com",
-});
+// posthog.init("phc_L763VL2WY6TExI5xG5xWOua6E5LYkZN496hBeSpEdAn", {
+//   api_host: "https://us.i.posthog.com",
+// });
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={options}
+      >
+        <App />
+      </PostHogProvider>
     </ClerkProvider>
   </StrictMode>
 );
