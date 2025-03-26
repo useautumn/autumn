69bd48ebd9d4292b778f4c713b405e8f0a41bfd2
John Yeo <johnyeocx@gmail.com>
2025-03-26 11:39:58 -0700

    working on create entity

--- server/src/internal/api/entities/handleCreateEntity.ts
+++ server/src/internal/api/entities/handleCreateEntity.ts
@@ -29,6 +29,116 @@ export const handleCreateEntity = async (req: any, res: any) => {
     ]);
 
     // 2. For each entity
+    for (const entity of parsedEntities) {
+      // 1. Get feature and customer
+      let feature = features.find((f) => f.id === entity.feature_id);
+      let customer = customers.find((c) => c.id === entity.customer_id);
+
+      // 2. Create entitlement for entity, customer and feature?
+    }
+    // for (const linkedFeature of linkedFeatures) {
+    //   const linkedCusEnt = getLinkedCusEnt({
+    //     linkedFeature,
+    //     cusEnts,
+    //   });
+
+    //   const groupVal = getGroupValFromProperties({
+    //     properties: event.add_groups || event.remove_groups,
+    //     feature: linkedFeature,
+    //   });
+
+    //   // console.log("Group val:", groupVal);
+    //   // console.log("Linked feature:", linkedFeature);
+
+    //   if (!groupVal) {
+    //     continue;
+    //   }
+
+    //   let isAdding = notNullish(event.add_groups);
+    //   event.value = isAdding ? groupVal.length : -groupVal.length;
+    //   const allowance = linkedCusEnt?.entitlement.allowance;
+
+    //   if (isAdding) {
+    //     let curBalances = linkedCusEnt?.balances || {};
+
+    //     for (const group of groupVal) {
+    //       // Check if group already exists & is not deleted
+    //       if (curBalances[group] && !curBalances[group].deleted) {
+    //         logger.warn(
+    //           `   - Group ${group} already exists & is not deleted, skipping`
+    //         );
+    //         replacedCount++;
+    //         event.value! -= 1;
+    //         continue;
+    //       }
+
+    //       if (curBalances[group] && curBalances[group].deleted) {
+    //         curBalances[group].deleted = false;
+    //         console.log(`   - Undeleting group ${group}`);
+    //         // replacedCount++;
+    //         event.value! -= 1;
+    //         continue;
+    //       }
+
+    //       // Check if there's any deleted balance to activate
+    //       let replaced = false;
+    //       for (const id in curBalances) {
+    //         let balance = curBalances[id];
+    //         if (balance.deleted) {
+    //           curBalances[group] = {
+    //             ...balance,
+    //             deleted: false,
+    //           };
+
+    //           delete curBalances[id];
+    //           // replacedCount++;
+    //           event.value = (event.value || 1) - 1;
+    //           break;
+    //         }
+    //       }
+
+    //       if (!replaced) {
+    //         curBalances[group] = {
+    //           balance: allowance!,
+    //           adjustment: 0,
+    //         };
+    //       }
+    //     }
+
+    //     await CustomerEntitlementService.update({
+    //       sb,
+    //       id: linkedCusEnt!.id,
+    //       updates: { balances: curBalances },
+    //     });
+    //   } else {
+    //     const curBalances = linkedCusEnt?.balances || {};
+    //     event.value = 0;
+    //     for (const group of groupVal) {
+    //       if (curBalances[group]) {
+    //         curBalances[group].deleted = true;
+    //         replacedCount++;
+    //       } else {
+    //         logger.warn(
+    //           `   - Group ${group} not found for linked feature ${linkedFeature.id}, can't delete`
+    //         );
+    //         throw new RecaseError({
+    //           message: `Group ${group} not found for linked feature ${linkedFeature.id}, can't delete`,
+    //           code: "GROUP_NOT_FOUND",
+    //           data: {
+    //             group,
+    //             linkedFeature,
+    //           },
+    //         });
+    //       }
+    //     }
+
+    //     await CustomerEntitlementService.update({
+    //       sb,
+    //       id: linkedCusEnt!.id,
+    //       updates: { balances: curBalances },
+    //     });
+    //   }
+    // }
 
     res.status(200).json({
       success: true,
