cb2f4a289b7cb3eaef90d566ab4676341b69479e
Ayush Rodrigues <joesj2905@gmail.com>
2025-04-09 20:45:05 +0100

    styling dfix

--- vite/src/views/products/ProductsView.tsx
+++ vite/src/views/products/ProductsView.tsx
@@ -57,48 +57,50 @@ function ProductsView({ env }: { env: AppEnv }) {
         mutateCounts,
       }}
     >
-      <div className="flex justify-between items-center">
-        <div>
-          <h1 className="text-xl font-medium">Products</h1>
-          <p className="text-sm text-t2">
-            Create the products your users can purchase.
-          </p>
+      <div className="p-6 flex flex-col gap-4 max-w-[1048px]">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-xl font-medium">Products</h1>
+            <p className="text-sm text-t2">
+              Create the products your users can purchase.
+            </p>
+          </div>
+          <ToggleDisplayButton
+            show={showRewards}
+            disabled={data?.rewards.length > 0}
+            onClick={() => setShowRewards((prev) => !prev)}
+          >
+            <Ticket size={12} className="mr-2" />
+            Coupons
+          </ToggleDisplayButton>
         </div>
-        <ToggleDisplayButton
-          show={showRewards}
-          disabled={data?.rewards.length > 0}
-          onClick={() => setShowRewards((prev) => !prev)}
-        >
-          <Ticket size={12} className="mr-2" />
-          Coupons
-        </ToggleDisplayButton>
+        <ProductsTable products={data?.products} />
+        <CreateProduct />
+        {showRewards && (
+          <React.Fragment>
+            <div className="flex flex-col gap-4 h-fit mt-6">
+              <div>
+                <h2 className="text-lg font-medium">Coupons</h2>
+                <p className="text-sm text-t2">
+                  Create a coupon to give users credits or a discount on one or
+                  more products.{" "}
+                  {/* <span className="text-t3">(eg, 10% off all products).</span> */}
+                </p>
+              </div>
+              <RewardsTable />
+              <CreateReward />
+            </div>
+            <div className="flex flex-col gap-4 h-fit mt-6">
+              <div>
+                <h2 className="text-lg font-medium">Referral Programs</h2>
+                <p className="text-sm text-t2">Create a referral program. </p>
+              </div>
+              <RewardProgramsTable />
+              <CreateRewardProgramModal />
+            </div>
+          </React.Fragment>
+        )}
       </div>
-      <ProductsTable products={data?.products} />
-      <CreateProduct />
-      {showRewards && (
-        <React.Fragment>
-          <div className="flex flex-col gap-4 h-fit mt-6">
-            <div>
-              <h2 className="text-lg font-medium">Coupons</h2>
-              <p className="text-sm text-t2">
-                Create a coupon to give users credits or a discount on one or
-                more products.{" "}
-                {/* <span className="text-t3">(eg, 10% off all products).</span> */}
-              </p>
-            </div>
-            <RewardsTable />
-            <CreateReward />
-          </div>
-          <div className="flex flex-col gap-4 h-fit mt-6">
-            <div>
-              <h2 className="text-lg font-medium">Referral Programs</h2>
-              <p className="text-sm text-t2">Create a referral program. </p>
-            </div>
-            <RewardProgramsTable />
-            <CreateRewardProgramModal />
-          </div>
-        </React.Fragment>
-      )}
     </ProductsContext.Provider>
   );
 }
