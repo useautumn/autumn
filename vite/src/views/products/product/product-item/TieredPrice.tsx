cbc89f6fd23b8f45f037066242a803551da0ab97
Ayush Rodrigues <joesj2905@gmail.com>
2025-04-14 02:22:36 +0100

    modal styling

--- vite/src/views/products/product/product-item/TieredPrice.tsx
+++ vite/src/views/products/product/product-item/TieredPrice.tsx
@@ -6,11 +6,15 @@ import { useState } from "react";
 import { useProductContext } from "../ProductContext";
 import { useProductItemContext } from "./ProductItemContext";
 import { Feature, TierInfinite } from "@autumn/shared";
+import { SelectCycle } from "./components/SelectCycle";
+import FieldLabel from "@/components/general/modal-components/FieldLabel";
 
 export default function TieredPrice({
-  setShowPrice,
+  show,
+  setShow,
 }: {
-  setShowPrice: (showPrice: boolean) => void;
+  show: any;
+  setShow: (show: any) => void;
 }) {
   let { features } = useProductContext();
   let { item, setItem } = useProductItemContext();
@@ -47,7 +51,7 @@ export default function TieredPrice({
   const handleRemoveTier = (index: number) => {
     const newTiers = [...item.tiers];
     if (newTiers.length == 1) {
-      setShowPrice(false);
+      setShow({ ...show, price: false });
       setItem({
         ...item,
         tiers: null,
@@ -61,115 +65,122 @@ export default function TieredPrice({
   };
 
   return (
-    <div className="flex flex-col gap-1 max-h-64 overflow-auto">
-      {item.tiers?.map((tier: any, index: number) => (
-        <div key={index} className="flex gap-1 w-full items-center">
-          <div className="w-full gap-2 flex items-center">
-            {item.tiers?.length > 1 && ( // First tier is just a price and billing units. No from or to tiers.
-              <div className="flex w-full items-center">
-                <div className="flex w-full text-sm">
-                  <UsageTierInput
-                    value={index == 0 ? 0 : item.tiers[index - 1].to}
-                    onChange={(e) => null}
-                    type="from"
-                  />
-                </div>
-                <span className="px-2 text-t3 text-xs">to</span>
-                <div
-                  className={cn(
-                    "flex w-full text-sm",
-                    tier.to == -1 && "bg-transparent"
-                  )}
-                >
-                  <UsageTierInput
-                    value={tier.to}
-                    onChange={(e) => {
-                      setUsageTier(index, "to", Number(e.target.value));
-                    }}
-                    type="to"
-                  />
-                </div>
-              </div>
-            )}
-            <div className="flex w-full gap-2 text-sm items-center">
-              <div className="flex w-full">
-                <UsageTierInput
-                  value={tier.amount}
-                  onChange={(e) =>
-                    setUsageTier(index, "amount", Number(e.target.value))
-                  }
-                  type="amount"
-                />
-              </div>
-              {editBillingUnits && index == 0 ? (
-                <>
-                  <span className="pr-2 text-t3 text-xs">per</span>
-                  <div
-                    className="w-full flex items-center relative"
-                    onBlur={() => setEditBillingUnits(false)}
-                  >
-                    <Input
-                      autoFocus
-                      value={item.billing_units}
-                      className="pr-14 !text-xs"
-                      onChange={(e) =>
-                        setItem({
-                          ...item,
-                          billing_units: Number(e.target.value),
-                        })
-                      }
+    <div className="flex flex-col w-full">
+      <FieldLabel className="flex items-center gap-2">Pricing</FieldLabel>
+
+      <div className="flex flex-col gap-1 max-h-64 overflow-auto">
+        {item.tiers?.map((tier: any, index: number) => (
+          <div key={index} className="flex gap-1 w-full items-center">
+            <div className="w-full gap-2 flex items-center">
+              {item.tiers?.length > 1 && ( // First tier is just a price and billing units. No from or to tiers.
+                <div className="flex w-full items-center">
+                  <div className="flex w-full text-sm">
+                    <UsageTierInput
+                      value={index == 0 ? 0 : item.tiers[index - 1].to}
+                      onChange={(e) => null}
+                      type="from"
                     />
-                    <span className="absolute right-2 top-1/2 -translate-y-1/2 text-t3 text-[10px] whitespace-nowrap truncate overflow-hidden max-w-12">
-                      {featureName ?? "units"}
-                    </span>
                   </div>
-                </>
-              ) : (
-                <div className="flex w-full">
-                  <Button
-                    size="sm"
-                    variant="ghost"
-                    disabled={index > 0}
-                    className="w-fit text-t3 w-30 overflow-hidden hover:bg-transparent justify-start"
-                    onClick={() => setEditBillingUnits(true)}
+                  <span className="px-2 text-t3 text-xs">to</span>
+                  <div
+                    className={cn(
+                      "flex w-full text-sm",
+                      tier.to == -1 && "bg-transparent"
+                    )}
                   >
-                    <span
-                      className={cn(
-                        "truncate",
-                        index == 0 && "border-b border-dotted border-t3"
-                      )}
-                    >
-                      {item.billing_units == 1
-                        ? `per ${featureName ?? "units"}`
-                        : `per ${item.billing_units} ${featureName ?? "units"}`}
-                    </span>
-                  </Button>
+                    <UsageTierInput
+                      value={tier.to}
+                      onChange={(e) => {
+                        setUsageTier(index, "to", Number(e.target.value));
+                      }}
+                      type="to"
+                    />
+                  </div>
                 </div>
               )}
+              <div className="flex w-full gap-2 text-sm items-center">
+                <div className="flex w-full">
+                  <UsageTierInput
+                    value={tier.amount}
+                    onChange={(e) =>
+                      setUsageTier(index, "amount", Number(e.target.value))
+                    }
+                    type="amount"
+                  />
+                </div>
+                {editBillingUnits && index == 0 ? (
+                  <>
+                    <span className="text-t3 text-xs">per</span>
+                    <div
+                      className="w-full flex items-center relative"
+                      onBlur={() => setEditBillingUnits(false)}
+                    >
+                      <Input
+                        autoFocus
+                        value={item.billing_units}
+                        className="pr-14 !text-xs"
+                        onChange={(e) =>
+                          setItem({
+                            ...item,
+                            billing_units: Number(e.target.value),
+                          })
+                        }
+                      />
+                      <span className="absolute right-2 top-1/2 -translate-y-1/2 text-t3 text-[10px] whitespace-nowrap truncate overflow-hidden max-w-12">
+                        {featureName ?? "units"}
+                      </span>
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex w-fit">
+                    <Button
+                      size="sm"
+                      variant="ghost"
+                      disabled={index > 0}
+                      className="w-fit max-w-24 text-t3 overflow-hidden hover:bg-transparent justify-start"
+                      onClick={() => setEditBillingUnits(true)}
+                    >
+                      <span
+                        className={cn(
+                          "truncate",
+                          index == 0 && "border-b border-dotted border-t3"
+                        )}
+                      >
+                        {item.billing_units == 1
+                          ? `per ${featureName ?? "units"}`
+                          : `per ${item.billing_units} ${
+                              featureName ?? "units"
+                            }`}
+                      </span>
+                    </Button>
+                  </div>
+                )}
+              </div>
             </div>
+            <Button
+              isIcon
+              size="sm"
+              variant="ghost"
+              className="w-fit text-t3"
+              onClick={() => handleAddTier()}
+              dim={6}
+            >
+              <Plus size={12} className="text-t3" />
+            </Button>
+
+            <Button
+              isIcon
+              size="sm"
+              variant="ghost"
+              className="w-fit text-t3 mr-1"
+              onClick={() => handleRemoveTier(index)}
+              dim={6}
+            >
+              <X size={12} className="text-t3" />
+            </Button>
           </div>
-          <Button
-            isIcon
-            size="sm"
-            variant="ghost"
-            className="w-fit text-t3"
-            onClick={() => handleAddTier()}
-            dim={6}
-          >
-            <Plus size={12} className="text-t3" />
-          </Button>
-          <Button
-            isIcon
-            size="sm"
-            variant="ghost"
-            className="w-fit text-t3 mr-1"
-            onClick={() => handleRemoveTier(index)}
-            dim={6}
-          >
-            <X size={12} className="text-t3" />
-          </Button>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
