4389e5e369fbef5566134b4b40cc495faaef6814
John Yeo <johnyeocx@gmail.com>
2025-05-06 16:20:59 +0100

    updated get customer schema, features and products are now objects

--- server/src/internal/api/customers/getCustomerDetails.ts
+++ server/src/internal/api/customers/getCustomerDetails.ts
@@ -6,7 +6,6 @@ import {
   fullCusProductToCusEnts,
   fullCusProductToCusPrices,
 } from "@/internal/customers/products/cusProductUtils.js";
-import { OrgService } from "@/internal/orgs/OrgService.js";
 import { BREAK_API_VERSION } from "@/utils/constants.js";
 import {
   Customer,
@@ -21,10 +20,97 @@ import {
   FeatureType,
   Feature,
   Organization,
+  CusEntResponse,
+  CusEntResponseV2,
+  CusProductResponse,
 } from "@autumn/shared";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { EntityService } from "../entities/EntityService.js";
 import { getCusInvoices, processFullCusProducts } from "./cusUtils.js";
+
+export const sumValues = (
+  entList: CusEntResponse[],
+  key: keyof CusEntResponse
+) => {
+  return entList.reduce((acc, curr) => {
+    if (curr[key]) {
+      return acc + Number(curr[key]);
+    }
+
+    return acc;
+  }, 0);
+};
+
+export const getEarliestNextResetAt = (entList: CusEntResponse[]) => {
+  let earliest = entList.reduce((acc, curr) => {
+    if (curr.next_reset_at && curr.next_reset_at < acc) {
+      return curr.next_reset_at;
+    }
+
+    return acc;
+  }, Infinity);
+
+  return earliest == Infinity ? null : earliest;
+};
+
+export const featuresToObject = ({
+  features,
+  entList,
+}: {
+  features: Feature[];
+  entList: CusEntResponse[];
+}) => {
+  let featureObject: Record<string, CusEntResponseV2> = {};
+  for (let entRes of entList) {
+    let feature = features.find((f) => f.id == entRes.feature_id)!;
+    if (feature.type == FeatureType.Boolean) {
+      featureObject[feature.id] = {
+        id: feature.id,
+        name: feature.name,
+      };
+      continue;
+    } else if (entRes.unlimited) {
+      featureObject[feature.id] = {
+        id: feature.id,
+        name: feature.name,
+        unlimited: true,
+      };
+      continue;
+    }
+
+    let featureId = feature.id;
+    let unlimited = entRes.unlimited;
+    let relatedEnts = entList.filter((e) => e.feature_id == featureId);
+
+    if (featureObject[featureId]) {
+      continue;
+    }
+
+    featureObject[featureId] = {
+      id: featureId,
+      name: feature.name,
+      unlimited,
+      balance: unlimited ? null : sumValues(relatedEnts, "balance"),
+      usage: sumValues(relatedEnts, "usage"),
+      included_usage: sumValues(relatedEnts, "included_usage"),
+
+      next_reset_at: getEarliestNextResetAt(relatedEnts),
+      interval: relatedEnts.length == 1 ? relatedEnts[0].interval : "multiple",
+      breakdown:
+        relatedEnts.length > 1
+          ? relatedEnts.map((e) => ({
+              interval: e.interval!,
+              balance: e.balance,
+              usage: e.usage,
+              included_usage: e.included_usage,
+              next_reset_at: e.next_reset_at,
+            }))
+          : undefined,
+    };
+  }
+
+  return featureObject;
+};
 
 export const getCustomerDetails = async ({
   customer,
@@ -113,28 +199,44 @@ export const getCustomerDetails = async ({
     (cusEnt: FullCustomerEntitlement) => cusEnt.entitlement.feature
   );
 
-  if (org.api_version == APIVersion.v1_1) {
+  let apiVersion = org.api_version || APIVersion.v1;
+
+  if (apiVersion >= APIVersion.v1_1) {
+    let entList: any = balances.map((b) => {
+      let isBoolean =
+        features.find((f: Feature) => f.id == b.feature_id)?.type ==
+        FeatureType.Boolean;
+      if (b.unlimited || isBoolean) {
+        return b;
+      }
+
+      return CusEntResponseSchema.parse({
+        ...b,
+        usage: b.used,
+        included_usage: b.allowance,
+      });
+    });
+
+    let products: any = [...main, ...addOns];
+
+    if (apiVersion >= APIVersion.v1_2) {
+      entList = featuresToObject({
+        features,
+        entList,
+      });
+      let productObject: Record<string, CusProductResponse> = {};
+      for (let product of products) {
+        productObject[product.id] = product as any;
+      }
+      products = productObject;
+    }
+
     let cusResponse = {
       ...CusResponseSchema.parse({
         ...customer,
-        // autumn_id: customer.internal_id,
         stripe_id: customer.processor?.id,
-        products: [...main, ...addOns],
-        // add_ons: addOns,
-        features: balances.map((b) => {
-          let isBoolean =
-            features.find((f: Feature) => f.id == b.feature_id)?.type ==
-            FeatureType.Boolean;
-          if (b.unlimited || isBoolean) {
-            return b;
-          }
-
-          return CusEntResponseSchema.parse({
-            ...b,
-            usage: b.used,
-            included_usage: b.allowance,
-          });
-        }),
+        products,
+        features: entList,
       }),
     };
 
