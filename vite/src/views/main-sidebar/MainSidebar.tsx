5a7ee8e318fbc779f1f41ee482982294b65d7594
Misbah Ansari <misbaansari444@gmail.com>
2025-06-21 14:26:10 +0530

    added image in empty state, fixed searching msg, added sidebar toggle(open/close)

--- vite/src/views/main-sidebar/MainSidebar.tsx
+++ vite/src/views/main-sidebar/MainSidebar.tsx
@@ -1,9 +1,8 @@
 import SidebarBottom from "./SidebarBottom";
 import { NavButton } from "./NavButton";
 import { useEnv } from "@/utils/envUtils";
-import { useState } from "react";
 import { cn } from "@/lib/utils";
-import { SidebarContext } from "./SidebarContext";
+import { useSidebarContext } from "./SidebarContext";
 import { useHotkeys } from "react-hotkeys-hook";
 import { Code, Package, Shield, User } from "lucide-react";
 import { EnvDropdown } from "./EnvDropdown";
@@ -12,50 +11,48 @@ import { AdminOnly } from "../admin/components/AdminOnly";
 
 export const MainSidebar = () => {
   const env = useEnv();
-  const [state, setState] = useState<"expanded" | "collapsed">("expanded");
+  const { state, setState } = useSidebarContext();
 
   useHotkeys(["meta+b", "ctrl+b"], () => {
-    setState((prev) => (prev == "expanded" ? "collapsed" : "expanded"));
+    setState((prev: "expanded" | "collapsed") => (prev == "expanded" ? "collapsed" : "expanded"));
   });
 
   return (
-    <SidebarContext.Provider value={{ state, setState }}>
-      <div
-        className={cn(
-          `h-full bg-stone-100 py-4 flex flex-col justify-between transition-all duration-150`,
-          state == "expanded"
-            ? "min-w-[200px] max-w-[200px]"
-            : "min-w-[50px] max-w-[50px]",
-        )}
-      >
-        <div className="flex flex-col gap-6">
-          <OrgDropdown />
-          {/* <SidebarTop /> */}
-          <EnvDropdown env={env} />
-          <div className="flex flex-col px-4">
-            <NavButton
-              value="products"
-              icon={<Package size={14} />}
-              title="Products"
-              env={env}
-            />
-            <NavButton
-              value="customers"
-              icon={<User size={20} />}
-              title="Customers"
-              env={env}
-            />
-            <NavButton
-              value="dev"
-              icon={<Code size={15} />}
-              title="Developer"
-              env={env}
-            />
-          </div>
+    <div
+      className={cn(
+        `h-full bg-stone-100 py-4 flex flex-col justify-between transition-all duration-150`,
+        state == "expanded"
+          ? "min-w-[200px] max-w-[200px]"
+          : "min-w-[50px] max-w-[50px]",
+      )}
+    >
+      <div className="flex flex-col gap-6">
+        <OrgDropdown />
+        {/* <SidebarTop /> */}
+        <EnvDropdown env={env} />
+        <div className="flex flex-col px-4">
+          <NavButton
+            value="products"
+            icon={<Package size={14} />}
+            title="Products"
+            env={env}
+          />
+          <NavButton
+            value="customers"
+            icon={<User size={20} />}
+            title="Customers"
+            env={env}
+          />
+          <NavButton
+            value="dev"
+            icon={<Code size={15} />}
+            title="Developer"
+            env={env}
+          />
         </div>
-        {/* Sidebar bottom */}
-        <SidebarBottom />
       </div>
-    </SidebarContext.Provider>
+      {/* Sidebar bottom */}
+      <SidebarBottom />
+    </div>
   );
 };
--- vite/src/views/main-sidebar/NavButton.tsx
+++ vite/src/views/main-sidebar/NavButton.tsx
@@ -32,44 +32,64 @@ export const NavButton = ({
   const isActive = tab == value;
 
   const [isHovered, setIsHovered] = useState(false);
+  const showTooltip = state === "collapsed" && isHovered;
+
   return (
-    <Link
-      to={href ? href : getRedirectUrl(`/${value}`, env)}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className={cn(
-        `cursor-pointer font-medium transition-all duration-100 
-         text-sm flex h-9 items-center text-t2 hover:text-primary`,
-        isActive && "font-semibold text-primary hover:text-primary/80",
-        className,
-      )}
-      target={href ? "_blank" : undefined}
-    >
-      <div
+    <div className="relative">
+      <Link
+        to={href ? href : getRedirectUrl(`/${value}`, env)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
         className={cn(
-          "flex justify-center w-4 h-4 items-center rounded-sm transition-all duration-100",
-          state == "expanded" && "mr-2",
-          isHovered && "translate-x-[-1px]",
+          `cursor-pointer font-medium transition-all duration-100 
+           text-sm flex h-9 items-center text-t2 hover:text-primary`,
+          isActive && "font-semibold text-primary hover:text-primary/80",
+          className,
         )}
+        target={href ? "_blank" : undefined}
       >
-        {icon}
-      </div>
-      <span
-        className={cn(
-          "transition-all duration-200 whitespace-nowrap",
-          state === "expanded"
-            ? "opacity-100 translate-x-0"
-            : "opacity-0 -translate-x-2 pointer-events-none w-0 m-0 p-0",
-        )}
-      >
-        {title}
-      </span>
-      {online && (
-        <span className="relative flex h-2 w-2 ml-2">
-          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-lime-400 opacity-75"></span>
-          <span className="relative inline-flex rounded-full h-2 w-2 bg-lime-500"></span>
+        <div
+          className={cn(
+            "flex justify-center w-4 h-4 items-center rounded-sm transition-all duration-100",
+            state == "expanded" && "mr-2",
+            isHovered && "translate-x-[-1px]",
+          )}
+        >
+          {icon}
+        </div>
+        <span
+          className={cn(
+            "transition-all duration-200 whitespace-nowrap",
+            state === "expanded"
+              ? "opacity-100 translate-x-0"
+              : "opacity-0 -translate-x-2 pointer-events-none w-0 m-0 p-0",
+          )}
+        >
+          {title}
         </span>
+        {online && (
+          <span className="relative flex h-2 w-2 ml-2">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-lime-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-2 w-2 bg-lime-500"></span>
+          </span>
+        )}
+      </Link>
+      
+      {/* Custom Tooltip */}
+      {showTooltip && (
+        <div className="absolute left-full top-1/2 transform -translate-y-1/2 ml-2 z-50">
+          <div className="relative">
+            {/* Arrow */}
+            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 -translate-x-full">
+              <div className="w-0 h-0 border-t-[6px] border-t-transparent border-b-[6px] border-b-transparent border-r-[6px] border-r-gray-900"></div>
+            </div>
+            {/* Tooltip content */}
+            <div className="bg-gray-900 text-white px-2 py-1 rounded text-sm font-medium whitespace-nowrap">
+              {title}
+            </div>
+          </div>
+        </div>
       )}
-    </Link>
+    </div>
   );
 };
