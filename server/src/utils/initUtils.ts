daf78a821951dae252588b4691a2438190b60c60
SirTenzin <49116958+SirTenzin@users.noreply.github.com>
2025-06-24 00:47:04 +0100

    fix: 🐛 missing supabase checks in initUtils
    This just makes sure users know earlier on that some actions will be
    skipped if they choose to not add SUPABASE_URL or SUPABASE_SERVICE_KEY.
    Logger will warn on startup

--- server/src/utils/initUtils.ts
+++ server/src/utils/initUtils.ts
@@ -36,4 +36,10 @@ export const checkEnvVars = () => {
   ) {
     logger.warn("LOGTAIL ENV VARs not found, skipping logtail");
   }
+
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_KEY) {
+    logger.warn(
+      `SUPABASE_URL or SUPABASE_SERVICE_KEY is not set, some actions will be skipped`,
+    );
+  }
 };
