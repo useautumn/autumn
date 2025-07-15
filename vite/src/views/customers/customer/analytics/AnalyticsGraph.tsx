a851320ad15b7946a8a9769bcbb53e9446905504
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-07-15 12:15:54 +0100

    feat: 🎸 properly format axes text and add font styling

--- vite/src/views/customers/customer/analytics/AnalyticsGraph.tsx
+++ vite/src/views/customers/customer/analytics/AnalyticsGraph.tsx
@@ -11,6 +11,7 @@ import {
   AgChartOptions,
   AgFlowProportionChartOptions,
   AgStandaloneChartOptions,
+  FormatterParams,
 } from "ag-charts-community";
 import { AgCharts } from "ag-charts-react";
 import {
@@ -26,28 +27,26 @@ import { AgGridReact } from "ag-grid-react";
 import { useEffect, useState } from "react";
 import { Copy } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { CopyablePre } from "@/components/general/CopyablePre";
+import { IRow, Row, autumnTheme } from "./components/AGGrid";
+import { useAnalyticsContext } from "./AnalyticsContext";
+import { Feature } from "@autumn/shared";
 
-export const description = "An interactive area chart";
+const dateFormatter = new Intl.DateTimeFormat(navigator.language || "en-US", {
+  month: "short",
+  day: "numeric",
+});
 
-export type Row =
-  | {
-      interval_start: string;
-    }
-  | {
-      [key: string]: number;
-    };
-
-export interface IRow {
-  timestamp: string;
-  event_name: string;
-  value: number;
-  properties: any;
-  idempotency_key: string;
-  entity_id: string;
-  customer_id: string;
-}
+const hourFormatter = new Intl.DateTimeFormat(navigator.language || "en-US", {
+  hour: "numeric",
+  minute: "numeric",
+});
 
 export function EventsBarChart({
   data,
@@ -60,10 +59,16 @@ export function EventsBarChart({
   };
   chartConfig: any;
 }) {
+  const { selectedInterval } = useAnalyticsContext();
   const [options, setOptions] = useState<AgChartOptions>({
     data: data.data,
     series: chartConfig,
     theme: {
+      params: {
+        fontFamily: {
+          googleFont: "Inter",
+        },
+      },
       palette: {
         fills: [
           "#9c5aff",
@@ -78,6 +83,17 @@ export function EventsBarChart({
           "#27a7ff",
         ],
       },
+    },
+    background: {
+      fill: "#fafaf9",
+    },
+    formatter: {
+      x: (params: FormatterParams<any, unknown>) => {
+        if (params.type !== "category") return;
+        return selectedInterval === "24h"
+          ? hourFormatter.format(new Date(params.value as string))
+          : dateFormatter.format(new Date(params.value as string));
+      }
     },
   });
 
@@ -100,7 +116,7 @@ export function EventsAGGrid({ data }: { data: any }) {
   const [rowData, setRowData] = useState<IRow[]>([]);
   const [isOpen, setIsOpen] = useState(false);
   const [event, setEvent] = useState<IRow | null>(null);
-  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
+  const [colDefs] = useState<ColDef<IRow>[]>([
     { field: "timestamp", flex: 1 },
     { field: "event_name", flex: 1 },
     { field: "value", flex: 1 },
@@ -124,7 +140,7 @@ export function EventsAGGrid({ data }: { data: any }) {
         paginationPageSize={500}
         paginationPageSizeSelector={[10, 100, 500, 1000]}
         className="w-full h-full"
-        theme={themeQuartz}
+        theme={autumnTheme}
         defaultColDef={{
           flex: 1,
           resizable: true,
@@ -162,7 +178,10 @@ export function RowClickDialog({
         </DialogTitle>
       </DialogHeader>
 
-      <DialogContent className="sm:max-w-[600px]">
+      <DialogContent
+        className="sm:max-w-[600px]"
+        aria-describedby="Event Details"
+      >
         <div className="flex flex-col gap-4">
           <div className="grid grid-cols-2 gap-4">
             <div className="space-y-4">
@@ -182,7 +201,9 @@ export function RowClickDialog({
                 <p className="text-sm font-semibold text-muted-foreground">
                   Event Properties
                 </p>
-                <CopyablePre text={JSON.stringify(JSON.parse(event.properties), null, 2)} />
+                <CopyablePre
+                  text={JSON.stringify(JSON.parse(event.properties), null, 2)}
+                />
               </div>
             </div>
 
@@ -216,18 +237,20 @@ export function RowClickDialog({
             </div>
           </div>
 
-          <Accordion
-            type="single"
-            collapsible
-            className="w-full"
-          >
+          <Accordion type="single" collapsible className="w-full">
             <AccordionItem value="item-1">
               <AccordionTrigger>Raw Event</AccordionTrigger>
               <AccordionContent>
-                <CopyablePre text={JSON.stringify({
-                  ...event,
-                  properties: JSON.parse(event.properties),
-                }, null, 4)} />
+                <CopyablePre
+                  text={JSON.stringify(
+                    {
+                      ...event,
+                      properties: JSON.parse(event.properties),
+                    },
+                    null,
+                    4,
+                  )}
+                />
               </AccordionContent>
             </AccordionItem>
           </Accordion>
