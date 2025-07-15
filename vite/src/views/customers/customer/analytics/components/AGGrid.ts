43bc2e3144eeaa28c63690d87701f929faa4341c
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-07-15 14:25:59 +0100

    feat: 🎸 set up table theme to match other tables

--- vite/src/views/customers/customer/analytics/components/AGGrid.ts
+++ vite/src/views/customers/customer/analytics/components/AGGrid.ts
@@ -19,14 +19,34 @@ export interface IRow {
 }
 
 export const autumnTheme = themeQuartz.withParams({
+  accentColor: "#8838FF70",
+  backgroundColor: "#FAFAF9",
+  borderColor: "#00000000",
+  browserColorScheme: "light",
   fontFamily: {
     googleFont: "Inter",
   },
-  headerFontFamily: {
-    googleFont: "Inter",
-  },
-  cellFontFamily: {
-    googleFont: "Inter",
-  },
-  rowHoverColor: "#eabfff"
+  fontSize: 13,
+  textColor: "#52525B",
+  foregroundColor: "#52525B",
+  headerBackgroundColor: "#00000000",
+  headerFontSize: 11,
+  headerTextColor: "#A1A1AA",
+  iconSize: 13,
+  spacing: "5.25px",
 });
+
+export const colors = [
+  "#9c5aff",
+  "#a97eff",
+  "#8268ff",
+  "#7571ff",
+  "#687aff",
+  "#5b83ff",
+  "#4e8cff",
+  "#4195ff",
+  "#349eff",
+  "#27a7ff",
+];
+
+export const paginationOptions = [10, 100, 500, 1000];
