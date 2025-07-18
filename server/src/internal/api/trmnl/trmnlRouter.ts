162d8ac1502df3ad3cca287171a9d4afd53bb2a5
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-07-18 11:27:25 +0100

    feat: 🎸 TRMNL screen for analytics

--- server/src/internal/api/trmnl/trmnlRouter.ts
+++ server/src/internal/api/trmnl/trmnlRouter.ts
@@ -1,139 +1,89 @@
-import { AnalyticsService } from '@/internal/analytics/AnalyticsService.js';
-import { ExtendedRequest } from '@/utils/models/Request.js';
-import { routeHandler } from '@/utils/routerUtils.js';
-import { Router } from 'express';
+import { AnalyticsService } from "@/internal/analytics/AnalyticsService.js";
+import { RevenueService } from "@/internal/analytics/RevenueService.js";
+import { ExtendedRequest } from "@/utils/models/Request.js";
+import { routeHandler } from "@/utils/routerUtils.js";
+import { Router } from "express";
 
 const trmnlRouter = Router();
 
-trmnlRouter.post('/screen', async (req: any, res: any) =>
+function numberWithCommas(x: number | string) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+trmnlRouter.post("/screen", async (req: any, res: any) =>
 	routeHandler({
 		req,
 		res,
-		action: 'generate trmnl screen',
+		action: "generate trmnl screen",
 		handler: async () => {
-			const { db, org, env, features } = req;
+			let topEvent: string[] = await AnalyticsService.getTopEventNames({
+				req,
+				limit: 1,
+			});
 
-			console.log('req.body', req.body);
-            console.log('req.body.event_name', req.body.event_name);
-            console.log('req.body.interval', req.body.interval);
+			if (!topEvent) {
+				topEvent = ["Unknown"];
+			}
 
-            let topUser = await AnalyticsService.getTopUser({ req });
-            let totalEvents = await AnalyticsService.getTotalEvents({ req });
-            let dailyActiveCustomers = await AnalyticsService.dailyActiveCustomers({ req });
+			let totalEvents: number | string =
+				await AnalyticsService.getTotalEvents({
+					req,
+					eventName: topEvent[0],
+				});
+
+			if (!totalEvents) {
+				totalEvents = "Unknown";
+			}
+
+			let monthlyRevenue:
+				| { total_payment_volume: number; label: string }
+				| string = await RevenueService.getMonthlyRevenue({
+				req,
+			});
+
+			if (!monthlyRevenue) {
+				monthlyRevenue = {
+					total_payment_volume: 0,
+					label: "Unknown",
+				};
+			}
 
 			let results = await AnalyticsService.getTimeseriesEvents({
 				req,
 				params: {
-					event_names: [req.body.event_name],
-					interval: req.body.interval,
+					event_names: [topEvent[0]],
+					interval: "30d",
 				},
-                aggregateAll: true,
+				aggregateAll: true,
 			});
 
-			console.log('results', results);
+			let totalCustomers: number | string =
+				await AnalyticsService.getTotalCustomers({ req });
 
-			let template = (data: any) => `
-<div class='view view--full'>
-  <div class='layout layout--col gap--space-between'>
-    <div class='grid grid--cols-3'>
-      <div class='item'>
-        <div class='meta'></div>
-        <div class='content'>
-          <span class='value value--tnums' data-value-fit='true'>${totalEvents || 0}</span>
-          <span class='label'>Total Events Today</span>
-        </div>
-      </div>
-      <div class='item'>
-        <div class='meta'></div>
-        <div class='content'>
-          <span class='value value--tnums' data-value-fit='true'>${topUser.name || "Unknown"}</span>
-          <span class='label'>Most Active User</span>
-        </div>
-      </div>
-      <div class='item'>
-        <div class='meta'></div>
-        <div class='content'>
-          <span class='value value--tnums' data-value-fit='true'>${dailyActiveCustomers || 0}</span>
-          <span class='label'>Customers Active Today</span>
-        </div>
-      </div>
-    </div>
+			if (!totalCustomers) {
+				totalCustomers = "Unknown";
+			}
 
-    <div id='chart-123' style='width: 100%'></div>
-  </div>
+			if (!results?.data) {
+				results = {
+					data: [],
+				};
+			}
 
-  <div class='title_bar'>
-    <img class='image' src='https://cdn.discordapp.com/emojis/1387133701393223680.webp?size=96' />
-    <span class='title'>Autumn</span>
-    <span class='instance'>atmn.sh</span>
-  </div>
-
-  <script type='text/javascript'>
-  var data = [${data.data.map((row: any) => `['${row.period}', ${row[req.body.event_name + '_count']}]`).join(',')}];
-  
-  var createChart = function() {
-    new Chartkick['LineChart'](
-    'chart-123',
-    data,
-    {
-      adapter: 'highcharts', // chartjs, google, etc available
-      prefix: '',
-      thousands: ',',
-      points: false,
-      colors: ['black'],
-      curve: true,
-      library: {
-        chart: {
-          height: 260
-        },
-        plotOptions: {
-          series: {
-            animation: false,
-            lineWidth: 4
-          }
-        },
-        yAxis: {
-          labels: {
-            style: {
-              fontSize: '16px',
-              color:'#000000'
-            }
-          },
-          gridLineDashStyle: 'shortdot',
-          gridLineWidth: 1,
-          gridLineColor: '#000000',
-          tickAmount: 5
-        },
-        xAxis: {
-          type: 'datetime',
-          labels: {
-            style: {
-              fontSize: '16px',
-              color: '#000000'
-            }
-          },
-          lineWidth: 0,
-          gridLineDashStyle: 'dot',
-          tickWidth: 1,
-          tickLength: 0,
-          gridLineWidth: 1,
-          gridLineColor: '#000000',
-          tickPixelInterval: 120
-        }
-      }
-    });
-  };
-
-  if ('Chartkick' in window) {
-    createChart();
-  } else {
-    window.addEventListener('chartkick:load', createChart, true);
-  }
-</script>
-  </div>
-`;
-            console.log("template", template(results));
-			res.status(200).json({ markup: template(results).replace(/\n/g, '') });
+			console.log({
+				rowData: `[${results.data.map((row: any) => `['${row.period}', ${row[topEvent[0] + "_count"]}]`).join(",")}]`,
+				revenue: numberWithCommas(monthlyRevenue.total_payment_volume),
+				totalEvent: numberWithCommas(totalEvents),
+				totalCustomers: numberWithCommas(totalCustomers),
+				topEvent: topEvent[0],
+			});
+			res.status(200).json({
+				rowData: `[${results.data.map((row: any) => `['${row.period}', ${row[topEvent[0] + "_count"]}]`).join(",")}]`,
+				revenue: numberWithCommas(monthlyRevenue.total_payment_volume),
+				totalEvents: numberWithCommas(totalEvents),
+				totalCustomers: numberWithCommas(totalCustomers),
+				topEvent: topEvent[0],
+			});
 		},
 	})
 );
