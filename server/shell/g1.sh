91ccad3a32df56c61fbf26e1364a92b994b52cb5
John Yeo <johnyeocx@gmail.com>
2025-07-19 20:01:35 +0100

    fix: local tunnel using wrong env var

--- server/shell/g1.sh
+++ server/shell/g1.sh
@@ -5,6 +5,7 @@ source "$(dirname "$0")/config.sh"
 
 MOCHA_PARALLEL=true $MOCHA_SETUP \
 && $MOCHA_CMD \
-'tests/attach/basic/*.ts' \
-'tests/attach/upgrade/*.ts' \
-'tests/attach/downgrade/*.ts'
+'tests/attach/basic/basic2.ts' \
+# 'tests/attach/basic/*.ts' \
+# 'tests/attach/upgrade/*.ts' \
+# 'tests/attach/downgrade/*.ts'
