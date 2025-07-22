709fc765673965cdffd6aba4cdef5d32f0fe3281
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-07-22 12:20:17 +0100

    feat: 🎸 validate item is able to be configured for rollovers

--- vite/src/utils/product/entitlementUtils.ts
+++ vite/src/utils/product/entitlementUtils.ts
@@ -1,24 +1,37 @@
-import { Feature, ProductItem } from "@autumn/shared";
+import { Feature, FeatureType, ProductItem } from "@autumn/shared";
 
 export const getFeature = (
-  featureId: string | undefined,
-  features: Feature[],
+	featureId: string | undefined,
+	features: Feature[]
 ) => {
-  const foundFeature = features?.find(
-    (feature: Feature) => feature.id === featureId,
-  );
-  return foundFeature || null;
+	const foundFeature = features?.find(
+		(feature: Feature) => feature.id === featureId
+	);
+	return foundFeature || null;
 };
 
 export const getFeatureUsageType = ({
-  item,
-  features,
+	item,
+	features,
 }: {
-  item: ProductItem;
-  features: Feature[];
+	item: ProductItem;
+	features: Feature[];
 }) => {
-  if (!item.feature_id) return null;
-  const feature = getFeature(item.feature_id, features);
+	if (!item.feature_id) return null;
+	const feature = getFeature(item.feature_id, features);
 
-  return feature?.config?.usage_type;
+	return feature?.config?.usage_type;
+};
+
+export const getFeatureCreditSystem = ({
+  item,
+  features
+}: {
+	item: ProductItem;
+	features: Feature[];
+}) => {
+	if (!item.feature_id) return null;
+	const feature = getFeature(item.feature_id, features);
+
+	return feature?.type === FeatureType.CreditSystem;
 };
