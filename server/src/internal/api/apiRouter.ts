2ec440354aefe5f4d97f124335734733560d61df
John Yeo <johnyeocx@gmail.com>
2025-07-22 12:51:05 +0100

    fix: updated analytics code

--- server/src/internal/api/apiRouter.ts
+++ server/src/internal/api/apiRouter.ts
@@ -22,7 +22,7 @@ import rewardRouter from "./rewards/rewardRouter.js";
 import expireRouter from "../customers/expire/expireRouter.js";
 import { handleSetupPayment } from "../customers/attach/handleSetupPayment.js";
 import { internalFeatureRouter } from "../features/internalFeatureRouter.js";
-import { analyticsRouter } from "./analytics/analyticsRouter.js";
+import { analyticsRouter } from "../analytics/analyticsRouter.js";
 
 const apiRouter: Router = Router();
 
@@ -56,7 +56,6 @@ apiRouter.use("/check", checkRouter);
 apiRouter.use("/events", eventsRouter);
 apiRouter.use("/track", eventsRouter);
 apiRouter.post("/setup_payment", handleSetupPayment);
-
 
 // Analytics
 apiRouter.use("/query", analyticsRouter);
