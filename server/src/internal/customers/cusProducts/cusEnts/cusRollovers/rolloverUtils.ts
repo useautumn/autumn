3188a8a63d1d31ef476ab598f4dd911d78783c09
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-07-23 15:24:29 +0100

    feat: 🎸 rolloverUtils prioritises entities and then balances

--- server/src/internal/customers/cusProducts/cusEnts/cusRollovers/rolloverUtils.ts
+++ server/src/internal/customers/cusProducts/cusEnts/cusRollovers/rolloverUtils.ts
@@ -11,11 +11,9 @@ import { randomUUID } from "crypto";
 
 export const getRolloverUpdates = ({
 	cusEnt,
-	allowance,
-	nextResetAt,
+	nextResetAt
 }: {
 	cusEnt: FullCustomerEntitlement;
-	allowance?: number;
 	nextResetAt: number;
 }) => {
 	let update: {
@@ -27,7 +25,6 @@ export const getRolloverUpdates = ({
 		toInsert: [],
 		toUpdate: [],
 	};
-
 	if (nullish(cusEnt.entitlement.rollover) || !cusEnt.entitlement.rollover) {
 		return update;
 	}
@@ -36,52 +33,74 @@ export const getRolloverUpdates = ({
 		nextResetAt,
 		cusEnt.entitlement.rollover
 	);
+
 	if (nullish(nextExpiry) || !nextExpiry) {
 		return update;
 	}
 
-	let entitlement = cusEnt.entitlement.allowance;
+	let entitlement = cusEnt.entitlement.allowance ?? 0;
 
-	if (nullish(entitlement) || !entitlement) {
+	if (entitlement < 0) {
 		return update;
 	}
 
-	let balance = cusEnt.balance || 0;
-
-	let rollover = entitlement! - balance;
+	let rollover = cusEnt.balance || 0;
 	console.log(
-		`Rollover: ${rollover} | Entitlement: ${entitlement} | Balance: ${balance}`
+		`🔥 Unused balance (rollover): ${rollover} | Entitlement: ${entitlement}`
 	);
 
 	let newEntitlement = {
 		cus_ent_id: cusEnt.id,
-		balance: rollover,
+		balance: 0,
 		expires_at: nextExpiry,
 		entities: [] as EntityRolloverBalance[],
 		id: randomUUID() as string,
 	};
 
-	console.log("🏢 entities:", cusEnt.entities);
-	console.log("📋 entitlement:", cusEnt.entitlement);
-	console.log("🆔 entity feature id:", cusEnt.entitlement.entity_feature_id);
+	if (cusEnt.entities != null)
+		console.log(
+			"🏢 entities:",
+			Object.values(cusEnt.entities).map((x: any) => `${x.id}: ${x.balance}`)
+		);
+	else console.log("🏢 entities: none");
+	console.log(
+		"📋 entitlement:",
+		cusEnt.entitlement.feature_id,
+		"| 🆔 entity_feature_id:",
+		cusEnt.entitlement.entity_feature_id,
+		"| allowance:",
+		cusEnt.entitlement.allowance
+	);
 
 	if (notNullish(cusEnt.entitlement.entity_feature_id)) {
 		console.log("🔍 newEntities:", cusEnt.entities);
 		for (const entityId in cusEnt.entities) {
-			let entRollover = entitlement! - cusEnt.entities[entityId].balance;
+			let entRollover = cusEnt.entities[entityId].balance;
 			if (entRollover > 0) {
 				newEntitlement.entities.push({
 					id: entityId,
 					balance: entRollover,
 				});
-			}
+				console.log("🔍 entityId:", entityId, "entRollover:", entRollover);
+			} else console.log("🔍 no rollover for entityId:", entityId, " | entitlement:", entitlement, " | balance:", cusEnt.entities[entityId].balance);
 		}
 		update.toInsert.push(newEntitlement);
 	} else {
-		update.toInsert.push(newEntitlement);
+		if (rollover > 0) {
+			newEntitlement.balance = rollover;
+			update.toInsert.push(newEntitlement);
+		} else console.log("🔍 no rollover for entitlement: ", cusEnt.id, " | rollable balance:", rollover);
 	}
 
-	console.log("Rollover update", update);
+	console.log(
+		"Rollover update sending from rolloverUtils:",
+		update.toInsert.map((rollover) => ({
+			id: rollover.id,
+			balance: rollover.balance,
+			entities: rollover.entities.map((entity) => `${entity.id}: ${entity.balance}`).join(", "),
+			expires_at: rollover.expires_at ? new Date(rollover.expires_at).toISOString() : null,
+		}))
+	);
 
 	return update;
 };
