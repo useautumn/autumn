691bb96fef6c83c51bb83da9e4892421565ce5a5
John Yeo <johnyeocx@gmail.com>
2025-07-24 17:45:04 +0100

    chore: added psuedocode for getNewProductRollovers

--- server/src/internal/customers/cusProducts/cusEnts/cusRollovers/getNewProductRollovers.ts
+++ server/src/internal/customers/cusProducts/cusEnts/cusRollovers/getNewProductRollovers.ts
@@ -1,23 +1,40 @@
-// import {
-//   CustomerEntitlement,
-//   EntitlementWithFeature,
-//   FullCusProduct,
-// } from "@autumn/shared";
+import {
+  CustomerEntitlement,
+  EntitlementWithFeature,
+  FullCusProduct,
+} from "@autumn/shared";
 
-// export const getNewProductRollovers = ({
-//   curCusProduct,
-//   cusEnts,
-//   entitlements,
-// }: {
-//   curCusProduct: FullCusProduct;
-//   cusEnts: CustomerEntitlement[];
-//   entitlements: EntitlementWithFeature[];
-// }) => {
-//   let newRollovers = [];
+export const getNewProductRollovers = async ({
+  curCusProduct,
+  cusEnts,
+  entitlements,
+  logger,
+}: {
+  curCusProduct: FullCusProduct;
+  cusEnts: CustomerEntitlement[];
+  entitlements: EntitlementWithFeature[];
+  logger: any;
+}) => {
+  try {
+    let newRollovers = [];
 
-//   for (const cusEnt of cusEnts) {
-//     let ent = entitlements.find((e) => e.id === cusEnt.entitlement_id);
-//     if (!ent?.rollover) continue;
+    for (const cusEnt of cusEnts) {
+      let ent = entitlements.find((e) => e.id === cusEnt.entitlement_id);
+      if (!ent?.rollover) continue;
 
-//   }
-// };
+      // 1. Get rollovers from current cus product (Look at feature ID)
+
+      // 2. Cases
+      // - Bring over current balance (if greater > 0), and any existing rollover
+      // - Perform max clearing according to new entitlement's rollover config (so cusEnt.entitlement.rollover)
+      // - To test: entity mode and non-entity mode, upgrade and downgrade
+      // - Don't need to handle no entity -> entity or entity -> no entity
+
+      // 3. Perform db operations AFTER insertFullCusProduct later on
+    }
+  } catch (error) {
+    logger.error(`Failed to handle new product rollovers:`, {
+      error,
+    });
+  }
+};
