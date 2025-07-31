be9a50b910360c0cabaefde2de24e55d666dd14a
John Yeo <johnyeocx@gmail.com>
2025-07-31 10:31:49 -0700

    fix: added delete feature modal to credit systems

--- vite/src/views/credits/CreditSystemRowToolbar.tsx
+++ vite/src/views/credits/CreditSystemRowToolbar.tsx
@@ -15,6 +15,7 @@ import { getBackendErr } from "@/utils/genUtils";
 import { useFeaturesContext } from "../features/FeaturesContext";
 import { ToolbarButton } from "@/components/general/table-components/ToolbarButton";
 import { Delete } from "lucide-react";
+import { DeleteFeatureDialog } from "../features/components/DeleteFeatureDialog";
 
 export const CreditSystemRowToolbar = ({
   creditSystem,
@@ -24,45 +25,55 @@ export const CreditSystemRowToolbar = ({
   const { env, mutate } = useFeaturesContext();
   const axiosInstance = useAxiosInstance({ env });
   const [deleteLoading, setDeleteLoading] = useState(false);
-  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
 
-  const handleDelete = async () => {
-    setDeleteLoading(true);
+  // const handleDelete = async () => {
+  //   setDeleteLoading(true);
 
-    try {
-      await FeatureService.deleteFeature(axiosInstance, creditSystem.id);
-      await mutate();
-    } catch (error) {
-      toast.error(getBackendErr(error, "Failed to delete feature"));
-    }
+  //   try {
+  //     await FeatureService.deleteFeature(axiosInstance, creditSystem.id);
+  //     await mutate();
+  //   } catch (error) {
+  //     toast.error(getBackendErr(error, "Failed to delete feature"));
+  //   }
 
-    setDeleteLoading(false);
-    setDeleteOpen(false);
-  };
+  //   setDeleteLoading(false);
+  //   setDeleteOpen(false);
+  // };
   return (
-    <DropdownMenu open={deleteOpen} onOpenChange={setDeleteOpen}>
-      <DropdownMenuTrigger asChild>
-        <ToolbarButton />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="text-t2">
-        <DropdownMenuItem
-          className="flex items-center"
-          onClick={async (e) => {
-            e.stopPropagation();
-            e.preventDefault();
-            await handleDelete();
-          }}
-        >
-          <div className="flex items-center justify-between w-full gap-2">
-            Delete
-            {deleteLoading ? (
-              <SmallSpinner />
-            ) : (
-              <Delete size={14} className="text-t3" />
-            )}
-          </div>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DeleteFeatureDialog
+        feature={creditSystem}
+        open={deleteDialogOpen}
+        setOpen={setDeleteDialogOpen}
+      />
+
+      <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
+        <DropdownMenuTrigger asChild>
+          <ToolbarButton />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="text-t2" align="end">
+          <DropdownMenuItem
+            className="flex items-center"
+            onClick={async (e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              setDeleteDialogOpen(true);
+              setDropdownOpen(false);
+            }}
+          >
+            <div className="flex items-center justify-between w-full gap-2">
+              Delete
+              {deleteLoading ? (
+                <SmallSpinner />
+              ) : (
+                <Delete size={14} className="text-t3" />
+              )}
+            </div>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
