42a5cdf35785324e1a1c3f503532620451babe06
John Yeo <johnyeocx@gmail.com>
2025-08-10 22:18:51 +0100

    fix: billing cycle one off formatting

--- server/tests/attach/basic/basic2.ts
+++ server/tests/attach/basic/basic2.ts
@@ -45,7 +45,7 @@ describe(`${chalk.yellowBright("basic2: Testing attach pro")}`, () => {
     });
 
     await completeCheckoutForm(checkout_url);
-    await timeout(10000);
+    await timeout(12000);
   });
 
   it("should have correct product & entitlements", async function () {
