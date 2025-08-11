8f85ece7a190c0ab783c8d6ae5fe30ba10367126
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-08-11 18:35:45 +0100

    feat: 🎸 default trial products

--- server/src/internal/products/free-trials/FreeTrialService.ts
+++ server/src/internal/products/free-trials/FreeTrialService.ts
@@ -1,45 +1,101 @@
 import { buildConflictUpdateColumns } from "@/db/dbUtils.js";
 import { DrizzleCli } from "@/db/initDrizzle.js";
-import { FreeTrial, freeTrials } from "@autumn/shared";
+import { AppEnv, FreeTrial, freeTrials, products } from "@autumn/shared";
 import { SupabaseClient } from "@supabase/supabase-js";
-import { eq } from "drizzle-orm";
+import { and, count, eq, inArray } from "drizzle-orm";
 
 export class FreeTrialService {
-  static async insert({ db, data }: { db: DrizzleCli; data: FreeTrial }) {
-    await db.insert(freeTrials).values(data as any);
-  }
+	static async insert({ db, data }: { db: DrizzleCli; data: FreeTrial }) {
+		await db.insert(freeTrials).values(data as any);
+	}
 
-  static async upsert({ db, data }: { db: DrizzleCli; data: FreeTrial }) {
-    let updateCols = buildConflictUpdateColumns(freeTrials, [
-      "id",
-      "internal_product_id",
-    ]);
+	static async upsert({ db, data }: { db: DrizzleCli; data: FreeTrial }) {
+		let updateCols = buildConflictUpdateColumns(freeTrials, [
+			"id",
+			"internal_product_id",
+		]);
 
-    await db
-      .insert(freeTrials)
-      .values(data as any)
-      .onConflictDoUpdate({
-        target: [freeTrials.id],
-        set: updateCols,
-      });
-  }
+		await db
+			.insert(freeTrials)
+			.values(data as any)
+			.onConflictDoUpdate({
+				target: [freeTrials.id],
+				set: updateCols,
+			});
+	}
 
-  static async update({
+	static async update({
+		db,
+		freeTrialId,
+		update,
+	}: {
+		db: DrizzleCli;
+		freeTrialId: string;
+		update: Partial<FreeTrial>;
+	}) {
+		await db
+			.update(freeTrials)
+			.set(update)
+			.where(eq(freeTrials.id, freeTrialId));
+	}
+
+	static async delete({ db, id }: { db: DrizzleCli; id: string }) {
+		await db.delete(freeTrials).where(eq(freeTrials.id, id));
+	}
+
+	static async getByProductId({
+		db,
+		productId,
+	}: {
+		db: DrizzleCli;
+		productId: string;
+	}) {
+		return await db.query.freeTrials.findFirst({
+			where: eq(freeTrials.internal_product_id, productId),
+		});
+	}
+
+  static async list({
     db,
-    freeTrialId,
-    update,
+    productIds,
+    isDefaultTrial,
   }: {
     db: DrizzleCli;
-    freeTrialId: string;
-    update: Partial<FreeTrial>;
+    productIds: string[];
+    isDefaultTrial?: boolean;
   }) {
-    await db
-      .update(freeTrials)
-      .set(update)
-      .where(eq(freeTrials.id, freeTrialId));
+    const result = await db.select({ id: freeTrials.internal_product_id }).from(freeTrials).where(
+      and(
+        inArray(freeTrials.internal_product_id, productIds),
+        isDefaultTrial ? eq(freeTrials.is_default_trial, true) : undefined
+      )
+    );
+    return { count: result.length, ids: result.map((r) => r.id) };
   }
 
-  static async delete({ db, id }: { db: DrizzleCli; id: string }) {
-    await db.delete(freeTrials).where(eq(freeTrials.id, id));
+  static async listByOrgId({
+    db,
+    orgId,
+    env,
+  }: {
+    db: DrizzleCli;
+    orgId: string;
+    env: AppEnv;
+  }) {
+    const result = await db
+      .select({
+        product: products,
+        trial: freeTrials,
+      })
+      .from(freeTrials)
+      .innerJoin(products, eq(freeTrials.internal_product_id, products.internal_id))
+      .where(
+        and(
+          eq(products.org_id, orgId),
+          eq(products.env, env)
+        )
+      );
+
+    return result[0];
   }
 }
