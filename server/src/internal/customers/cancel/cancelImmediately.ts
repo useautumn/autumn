b066ef4a7e8752d25dbe07a21634e6be533757a7
John Yeo <johnyeocx@gmail.com>
2025-08-22 10:44:16 -0700

    fix: webhooks when cancel product thru dashboard

--- server/src/internal/customers/cancel/cancelImmediately.ts
+++ server/src/internal/customers/cancel/cancelImmediately.ts
@@ -1,11 +1,17 @@
 import { createStripeCli } from "@/external/stripe/utils.js";
 import { ExtendedRequest } from "@/utils/models/Request.js";
-import { CusProductStatus, FullCusProduct, FullCustomer } from "@autumn/shared";
+import {
+  AttachScenario,
+  CusProductStatus,
+  FullCusProduct,
+  FullCustomer,
+} from "@autumn/shared";
 import { cusProductToSub } from "../cusProducts/cusProductUtils/convertCusProduct.js";
 import { getLatestPeriodEnd } from "@/external/stripe/stripeSubUtils/convertSubUtils.js";
 import { CusProductService } from "../cusProducts/CusProductService.js";
 import { activateDefaultProduct } from "../cusProducts/cusProductUtils.js";
 import { getExistingCusProducts } from "../cusProducts/cusProductUtils/getExistingCusProducts.js";
+import { addProductsUpdatedWebhookTask } from "@/internal/analytics/handlers/handleProductsUpdated.js";
 
 export const cancelImmediately = async ({
   req,
@@ -30,9 +36,12 @@ export const cancelImmediately = async ({
   const sub = await cusProductToSub({ cusProduct, stripeCli });
 
   if (sub) {
-    // if sub latest invoice not paid, don't prorate
-
-    await stripeCli.subscriptions.cancel(sub.id, { prorate: prorate });
+    await stripeCli.subscriptions.cancel(sub.id, {
+      prorate: prorate,
+      cancellation_details: {
+        comment: "autumn_cancel",
+      },
+    });
   }
 
   const isMain = !cusProduct.product.is_add_on;
@@ -60,5 +69,17 @@ export const cancelImmediately = async ({
       status: CusProductStatus.Expired,
       ended_at: Date.now(),
     },
+  });
+
+  console.log("Sending webhook for expired product");
+  await addProductsUpdatedWebhookTask({
+    req,
+    internalCustomerId: fullCus.internal_id,
+    org,
+    env,
+    customerId: fullCus.id || null,
+    cusProduct,
+    scenario: AttachScenario.Expired,
+    logger,
   });
 };
