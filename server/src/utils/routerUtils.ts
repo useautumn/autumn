044551d280c004fd76087b2bb446589e45d36d07
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-08-27 17:58:01 +0100

    feat: 🎸 include span in router utils

--- server/src/utils/routerUtils.ts
+++ server/src/utils/routerUtils.ts
@@ -8,6 +8,7 @@ import { StatusCodes } from "http-status-codes";
 import Stripe from "stripe";
 import { DrizzleCli } from "@/db/initDrizzle.js";
 import { ExtendedRequest } from "./models/Request.js";
+import { withSpan as withSpanTracer } from "@/internal/analytics/tracer/spanUtils.js";
 
 /**
  * Type-safe route handler with optional loader function.
@@ -39,35 +40,67 @@ export const routeHandler = async <TLoad = undefined>({
   handler,
   validator,
   loader,
+  withSpan = false,
 }: {
   req: any;
   res: any;
   action: string;
   handler: (req: any, res: any, load: TLoad) => Promise<void>;
   validator?: ((req: any, res: any) => Promise<void>) | ZodAny | ZodObject<any, any, any, any, any>;
+  withSpan?: boolean;
 } & (TLoad extends undefined
   ? { loader?: never }
   : { loader: (org: Organization, env: AppEnv, db: DrizzleCli, body: any, query: any, req: ExtendedRequest) => Promise<TLoad> }
 )) => {
   try {
     let load: TLoad | undefined;
-    if (typeof validator === 'function') {
-      await validator(req, res);
-    } else if(validator instanceof ZodAny || validator instanceof ZodObject) {
-      const parseResult = validator.safeParse(req.body);
-      if (!parseResult.success) {
-        const errorMsg =
-          parseResult.error.errors[0]?.message || "Invalid request body";
-        throw new RecaseError({
-          message: errorMsg,
-          code: ErrCode.InvalidRequest,
-        });
+
+    if(!withSpan) {
+      if (typeof validator === 'function') {
+        await validator(req, res);
+      } else if(validator instanceof ZodAny || validator instanceof ZodObject) {
+        const parseResult = validator.safeParse(req.body);
+        if (!parseResult.success) {
+          const errorMsg =
+            parseResult.error.errors[0]?.message || "Invalid request body";
+          throw new RecaseError({
+            message: errorMsg,
+            code: ErrCode.InvalidRequest,
+          });
+        }
       }
+      if (loader) {
+        load = await loader((req as ExtendedRequest).org, (req as ExtendedRequest).env, (req as ExtendedRequest).db, req.body, req.query, req);
+      }
+      await handler(req, res, load as TLoad);
+    } else {
+      await withSpanTracer({
+        name: action ?? "unknown",
+        attributes: {
+          org: req.org?.id,
+          env: req.env,
+        },
+        fn: async () => {
+          if (typeof validator === 'function') {
+            await validator(req, res);
+          } else if(validator instanceof ZodAny || validator instanceof ZodObject) {
+            const parseResult = validator.safeParse(req.body);
+            if (!parseResult.success) {
+              const errorMsg =
+                parseResult.error.errors[0]?.message || "Invalid request body";
+              throw new RecaseError({
+                message: errorMsg,
+                code: ErrCode.InvalidRequest,
+              });
+            }
+          }
+          if (loader) {
+            load = await loader((req as ExtendedRequest).org, (req as ExtendedRequest).env, (req as ExtendedRequest).db, req.body, req.query, req);
+          }
+          await handler(req, res, load as TLoad);
+        }
+      });
     }
-    if (loader) {
-      load = await loader((req as ExtendedRequest).org, (req as ExtendedRequest).env, (req as ExtendedRequest).db, req.body, req.query, req);
-    }
-    await handler(req, res, load as TLoad);
   } catch (error) {
     try {
       if (error instanceof RecaseError) {
