d0a3b1eb005d8c653527d12758f2a7a92a436d3b
Supriyo <supriyomalstartup@gmail.com>
2025-08-31 16:31:08 +0530

    Update orgRelations.ts

--- shared/models/orgModels/orgRelations.ts
+++ shared/models/orgModels/orgRelations.ts
@@ -3,8 +3,10 @@ import { apiKeys } from "../devModels/apiKeyTable.js";
 import { features } from "../featureModels/featureTable.js";
 import { organizations } from "./orgTable.js";
 import { user } from "../../db/auth-schema.js";
+import { member } from "../../db/auth-schema.js";
 
 export const organizationsRelations = relations(organizations, ({ many }) => ({
   api_keys: many(apiKeys),
   features: many(features),
+  members: many(member),
 }));
