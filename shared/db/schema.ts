625fe2c17ffe60725409676eb04ce106ff7decdd
Supriyo <supriyomalstartup@gmail.com>
2025-09-07 00:51:53 +0530

    refactor: remove orgJoinRequests from schema exports

--- shared/db/schema.ts
+++ shared/db/schema.ts
@@ -26,7 +26,6 @@ import { metadata } from "../models/otherModels/metadataTable.js";
 import { subscriptions } from "../models/subModels/subTable.js";
 import { invoices } from "../models/cusModels/invoiceModels/invoiceTable.js";
 import { invoiceRelations } from "../models/cusModels/invoiceModels/invoiceRelations.js";
-import { orgJoinRequests } from "../models/orgModels/orgJoinRequestTable.js";
 
 // Reward Tables
 import { rewards } from "../models/rewardModels/rewardModels/rewardTable.js";
@@ -40,7 +39,6 @@ import { migrationErrors } from "../models/migrationModels/migrationErrorTable.j
 
 /* RELATIONS */
 import { organizationsRelations } from "../models/orgModels/orgRelations.js";
-import { orgJoinRequestRelations } from "../models/orgModels/orgJoinRequestRelations.js";
 import { featureRelations } from "../models/featureModels/featureRelations.js";
 
 // Customer Relations
@@ -86,7 +84,6 @@ import { userRelations, memberRelations } from "./auth-relations.js";
 export {
   // Tables
   organizations,
-  orgJoinRequests,
   chatResults,
   freeTrials,
   entitlements,
@@ -123,7 +120,6 @@ export {
 
   // Relations
   organizationsRelations,
-  orgJoinRequestRelations,
   entitlementsRelations,
   featureRelations,
   priceRelations,
