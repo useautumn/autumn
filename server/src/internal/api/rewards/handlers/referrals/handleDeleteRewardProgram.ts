5c8b00d29cd9d79e67b17e28c4cf10f71ed73f23
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-08 11:54:27 +0100

    refactor: 💡 clean up before changes

---
+++ server/src/internal/api/rewards/handlers/referrals/handleDeleteRewardProgram.ts
@@ -0,0 +1,22 @@
+import { RewardProgramService } from "@/internal/rewards/RewardProgramService.js";
+import { routeHandler } from "@/utils/routerUtils.js";
+
+export default async (req: any, res: any) =>
+	routeHandler({
+		req,
+		res,
+		action: "delete reward scheme",
+		handler: async (req, res) => {
+			const { orgId, env, db } = req;
+			const { id } = req.params;
+
+			const rewardProgram = await RewardProgramService.delete({
+				db,
+				id,
+				orgId,
+				env,
+			});
+
+			return res.status(200).json(rewardProgram);
+		},
+	});
---
+++ server/src/internal/api/rewards/handlers/rewards/handleCreateCoupon.ts
@@ -0,0 +1,68 @@
+import { CreateRewardSchema, RewardCategory } from "@autumn/shared";
+import { createStripeCoupon } from "@/external/stripe/stripeCouponUtils/stripeCouponUtils.js";
+import { OrgService } from "@/internal/orgs/OrgService.js";
+import { PriceService } from "@/internal/products/prices/PriceService.js";
+import { RewardService } from "@/internal/rewards/RewardService.js";
+import {
+    constructReward,
+    getRewardCat,
+    initRewardStripePrices,
+} from "@/internal/rewards/rewardUtils.js";
+import { routeHandler } from "@/utils/routerUtils.js";
+
+
+export default async (req: any, res: any) => routeHandler({
+    req,
+    res,
+    action: "create coupon",
+    handler: async (req, res) => {
+        const { db, orgId, env, logtail: logger } = req;
+        const rewardBody = req.body;
+        const rewardData = CreateRewardSchema.parse(rewardBody);
+
+        const org = await OrgService.getFromReq(req);
+
+        const newReward = constructReward({
+            reward: rewardData,
+            orgId,
+            env,
+            // internalId: rewardBody.internal_id,
+        });
+
+        if (getRewardCat(newReward) === RewardCategory.Discount) {
+            const discountConfig = newReward.discount_config;
+
+            // Get prices for coupon
+            const [prices] = await Promise.all([
+                PriceService.getInIds({
+                    db,
+                    ids: discountConfig!.price_ids || [],
+                })
+            ]);
+
+            await initRewardStripePrices({
+                db,
+                prices,
+                org,
+                env,
+                logger,
+            });
+
+            await createStripeCoupon({
+                reward: newReward,
+                org,
+                env,
+                prices,
+                logger,
+                legacyVersion: req.query.legacyStripe === "true",
+            });
+        }
+
+        const insertedCoupon = await RewardService.insert({
+            db,
+            data: newReward,
+        });
+
+        res.status(200).json(insertedCoupon);
+    },
+})
