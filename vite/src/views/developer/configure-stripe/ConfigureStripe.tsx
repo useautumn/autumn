0324100dbe3111d619acfa6054544b8e57930354
John Yeo <johnyeocx@gmail.com>
2025-09-08 21:45:20 -0700

    final touches

--- vite/src/views/developer/configure-stripe/ConfigureStripe.tsx
+++ vite/src/views/developer/configure-stripe/ConfigureStripe.tsx
@@ -13,8 +13,11 @@ import { Check } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { DisconnectStripePopover } from "./DisconnectStripePopover";
+import { AppEnv } from "@autumn/shared";
+import { useEnv } from "@/utils/envUtils";
 
 export const ConfigureStripe = () => {
+  const env = useEnv();
   const { org, isLoading, mutate } = useOrg();
   const axiosInstance = useAxiosInstance();
 
@@ -122,22 +125,25 @@ export const ConfigureStripe = () => {
             </a>
             .
           </p>
-          <div className="flex items-center gap-2 mb-2">
-            <span className="text-t3 text-sm">If you want to use a restricted key</span>
-            <InfoTooltip>
-              <div className="max-w-xs">
-                <p className="font-medium mb-2">The following information</p>
-                <p className="text-sm mb-1">Scopes needed:</p>
-                <ul className="text-sm list-disc list-inside space-y-0.5">
-                  <li>Core</li>
-                  <li>Checkout</li>
-                  <li>Billing</li>
-                  <li>All webhook</li>
-                  <li>Accounts</li>
-                </ul>
-              </div>
-            </InfoTooltip>
-          </div>
+          {env == AppEnv.Live && (
+            <div className="flex items-center gap-2 mb-2">
+              <span className="text-t3 text-sm">
+                If you want to use a restricted key
+              </span>
+              <InfoTooltip>
+                <div className="max-w-xs">
+                  <p className="mb-2">The following scopes are needed:</p>
+                  <ul className="list-disc list-inside space-y-0.5">
+                    <li>Core</li>
+                    <li>Checkout</li>
+                    <li>Billing</li>
+                    <li>All webhook</li>
+                    <li>Accounts</li>
+                  </ul>
+                </div>
+              </InfoTooltip>
+            </div>
+          )}
 
           {org.stripe_connected ? (
             <Input
