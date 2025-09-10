5c8c076dc46994138e830c1a067b1470e5f14337
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-10 17:44:33 +0100

    feat: 🎸 paid products for N months in reward programs

--- server/src/internal/rewards/referralUtils.ts
+++ server/src/internal/rewards/referralUtils.ts
@@ -1,260 +1,386 @@
 import {
-  AppEnv,
-  CusProductStatus,
-  Customer,
-  ErrCode,
-  FullRewardProgram,
-  ReferralCode,
-  Reward,
-  RewardCategory,
-  RewardReceivedBy,
-  RewardRedemption,
+	type AppEnv,
+	AttachBranch,
+	type Customer,
+	ErrCode,
+	type FullRewardProgram,
+	type ReferralCode,
+	type Reward,
+	RewardReceivedBy,
+	type RewardRedemption,
 } from "@autumn/shared";
-
-import { CusService } from "../customers/CusService.js";
+import { StatusCodes } from "http-status-codes";
+import type Stripe from "stripe";
+import type { DrizzleCli } from "@/db/initDrizzle.js";
 import { createStripeCusIfNotExists } from "@/external/stripe/stripeCusUtils.js";
 import { createStripeCli } from "@/external/stripe/utils.js";
-import Stripe from "stripe";
-import { RewardRedemptionService } from "./RewardRedemptionService.js";
-import { ProductService } from "../products/ProductService.js";
-import { createFullCusProduct } from "../customers/add-product/createFullCusProduct.js";
-import { InsertCusProductParams } from "../customers/cusProducts/AttachParams.js";
-import { DrizzleCli } from "@/db/initDrizzle.js";
-import { CusProductService } from "../customers/cusProducts/CusProductService.js";
 import RecaseError from "@/utils/errorUtils.js";
-import { StatusCodes } from "http-status-codes";
-import { getRewardCat } from "./rewardUtils.js";
-import { ExtendedRequest } from "@/utils/models/Request.js";
+import type { ExtendedRequest } from "@/utils/models/Request.js";
+import { createCusInStripe } from "@/utils/scriptUtils/initCustomer.js";
+import { createFullCusProduct } from "../customers/add-product/createFullCusProduct.js";
+import { handleAddProduct } from "../customers/attach/attachFunctions/addProductFlow/handleAddProduct.js";
+import { rewardProgramToAttachParams } from "../customers/attach/attachUtils/attachParams/convertToParams.js";
+import { CusService } from "../customers/CusService.js";
 import { deleteCusCache } from "../customers/cusCache/updateCachedCus.js";
+import type { InsertCusProductParams } from "../customers/cusProducts/AttachParams.js";
+import { ProductService } from "../products/ProductService.js";
+import {
+	isFreeProduct,
+	isOneOff,
+	itemsAreOneOff,
+} from "../products/productUtils.js";
+import { RewardRedemptionService } from "./RewardRedemptionService.js";
 
 export const generateReferralCode = () => {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const codeLength = 6;
+	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const codeLength = 6;
 
-  let code = "";
+	let code = "";
 
-  for (let i = 0; i < codeLength; i++) {
-    code += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
+	for (let i = 0; i < codeLength; i++) {
+		code += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
 
-  return code;
+	return code;
 };
 
 // Trigger reward
 export const triggerRedemption = async ({
-  db,
-  referralCode,
-  org,
-  env,
-  logger,
-  reward,
-  redemption,
+	db,
+	referralCode,
+	org,
+	env,
+	logger,
+	reward,
+	redemption,
 }: {
-  db: DrizzleCli;
-  org: any;
-  env: AppEnv;
-  logger: any;
-  referralCode: ReferralCode;
-  reward: Reward;
-  redemption: RewardRedemption;
+	db: DrizzleCli;
+	org: any;
+	env: AppEnv;
+	logger: any;
+	referralCode: ReferralCode;
+	reward: Reward;
+	redemption: RewardRedemption;
 }) => {
-  logger.info(
-    `Triggering redemption ${redemption.id} for referral code ${referralCode.code}`
-  );
+	logger.info(
+		`Triggering redemption ${redemption.id} for referral code ${referralCode.code}`,
+	);
 
-  let applyToCustomer = await CusService.getByInternalId({
-    db,
-    internalId: referralCode.internal_customer_id,
-  });
+	const applyToCustomer = await CusService.getByInternalId({
+		db,
+		internalId: referralCode.internal_customer_id,
+	});
 
-  if (!applyToCustomer) {
-    throw new RecaseError({
-      message: `Customer ${referralCode.internal_customer_id} not found`,
-      code: ErrCode.CustomerNotFound,
-      statusCode: StatusCodes.NOT_FOUND,
-    });
-  }
+	if (!applyToCustomer) {
+		throw new RecaseError({
+			message: `Customer ${referralCode.internal_customer_id} not found`,
+			code: ErrCode.CustomerNotFound,
+			statusCode: StatusCodes.NOT_FOUND,
+		});
+	}
 
-  let stripeCli = createStripeCli({
-    org,
-    env,
-    legacyVersion: true,
-  });
+	const stripeCli = createStripeCli({
+		org,
+		env,
+		legacyVersion: true,
+	});
 
-  await createStripeCusIfNotExists({
-    db,
-    customer: applyToCustomer,
-    org,
-    env,
-    logger,
-  });
+	await createStripeCusIfNotExists({
+		db,
+		customer: applyToCustomer,
+		org,
+		env,
+		logger,
+	});
 
-  let stripeCusId = applyToCustomer.processor.id;
-  let stripeCus = (await stripeCli.customers.retrieve(
-    stripeCusId
-  )) as Stripe.Customer;
+	const stripeCusId = applyToCustomer.processor.id;
+	const stripeCus = (await stripeCli.customers.retrieve(
+		stripeCusId,
+	)) as Stripe.Customer;
 
-  let applied = false;
-  if (!stripeCus.discount) {
-    await stripeCli.customers.update(stripeCusId, {
-      // @ts-ignore
-      coupon: reward.id,
-    });
+	let applied = false;
+	if (!stripeCus.discount) {
+		await stripeCli.customers.update(stripeCusId, {
+			// @ts-expect-error
+			coupon: reward.id,
+		});
 
-    applied = true;
-    logger.info(`Applied coupon to customer in Stripe`);
-  }
+		applied = true;
+		logger.info(`Applied coupon to customer in Stripe`);
+	}
 
-  let updatedRedemption = await RewardRedemptionService.update({
-    db,
-    id: redemption.id,
-    updates: {
-      applied,
-      triggered: true,
-    },
-  });
+	const updatedRedemption = await RewardRedemptionService.update({
+		db,
+		id: redemption.id,
+		updates: {
+			applied,
+			triggered: true,
+		},
+	});
 
-  logger.info(`Successfully triggered redemption, applied: ${applied}`);
+	logger.info(`Successfully triggered redemption, applied: ${applied}`);
 
-  return updatedRedemption;
+	return updatedRedemption;
 };
 
 export const triggerFreeProduct = async ({
-  req,
-  db,
-  referralCode,
-  redeemer,
-  redemption,
-  rewardProgram,
-  org,
-  env,
-  logger,
+	req,
+	db,
+	referralCode,
+	redeemer,
+	redemption,
+	rewardProgram,
+	org,
+	env,
+	logger,
 }: {
-  req?: ExtendedRequest;
-  db: DrizzleCli;
-  referralCode: ReferralCode;
-  redeemer: Customer;
-  redemption: RewardRedemption;
-  rewardProgram: FullRewardProgram;
-  org: any;
-  env: AppEnv;
-  logger: any;
+	req?: ExtendedRequest;
+	db: DrizzleCli;
+	referralCode: ReferralCode;
+	redeemer: Customer;
+	redemption: RewardRedemption;
+	rewardProgram: FullRewardProgram & { reward: Reward };
+	org: any;
+	env: AppEnv;
+	logger: any;
 }) => {
-  logger.info(`Triggering free product reward`);
-  let { received_by } = rewardProgram;
+	logger.info(`Triggering free product reward`);
+	const { received_by } = rewardProgram;
 
-  let addToRedeemer = received_by === RewardReceivedBy.All;
-  let addToReferrer =
-    received_by === RewardReceivedBy.Referrer ||
-    received_by === RewardReceivedBy.All;
+	let addToRedeemer = received_by === RewardReceivedBy.All;
+	let addToReferrer =
+		received_by === RewardReceivedBy.Referrer ||
+		received_by === RewardReceivedBy.All;
 
-  let productId = rewardProgram.reward.free_product_id!;
+	const productId = rewardProgram.reward.free_product_id!;
 
-  let fullProduct = await ProductService.getFull({
-    db,
-    idOrInternalId: productId,
-    orgId: org.id,
-    env,
-  });
+	const fullProduct = await ProductService.getFull({
+		db,
+		idOrInternalId: productId,
+		orgId: org.id,
+		env,
+	});
 
-  if (!fullProduct) {
-    throw new RecaseError({
-      message: `Product ${productId} not found`,
-      code: ErrCode.ProductNotFound,
-      statusCode: StatusCodes.NOT_FOUND,
-    });
-  }
+	const isPaidProduct = !isFreeProduct(fullProduct.prices);
+	const isRecurring =
+		!isOneOff(fullProduct.prices) && !itemsAreOneOff(fullProduct.entitlements);
 
-  let referrer = await CusService.getByInternalId({
-    db,
-    internalId: referralCode.internal_customer_id,
-  });
+	if (!fullProduct) {
+		throw new RecaseError({
+			message: `Product ${productId} not found`,
+			code: ErrCode.ProductNotFound,
+			statusCode: StatusCodes.NOT_FOUND,
+		});
+	}
 
-  if (!referrer) {
-    throw new RecaseError({
-      message: `Referrer ${referralCode.internal_customer_id} not found`,
-      code: ErrCode.CustomerNotFound,
-      statusCode: StatusCodes.NOT_FOUND,
-    });
-  }
+	const referrer = await CusService.getByInternalId({
+		db,
+		internalId: referralCode.internal_customer_id,
+	});
 
-  let [fullReferrer, fullRedeemer] = await Promise.all([
-    CusService.getFull({
-      db,
-      idOrInternalId: referrer.id!,
-      orgId: org.id,
-      env,
-    }),
-    CusService.getFull({
-      db,
-      idOrInternalId: redeemer.id!,
-      orgId: org.id,
-      env,
-    }),
-  ]);
+	if (!referrer) {
+		throw new RecaseError({
+			message: `Referrer ${referralCode.internal_customer_id} not found`,
+			code: ErrCode.CustomerNotFound,
+			statusCode: StatusCodes.NOT_FOUND,
+		});
+	}
 
-  let attachParams: InsertCusProductParams = {
-    req,
-    org,
-    product: fullProduct,
-    prices: fullProduct.prices,
-    entitlements: fullProduct.entitlements,
-    optionsList: [],
-    entities: [],
-    freeTrial: null,
-    features: [],
-    customer: fullReferrer,
-    cusProducts: fullReferrer.customer_products,
-    replaceables: [],
-  };
+	const [fullReferrer, fullRedeemer] = await Promise.all([
+		CusService.getFull({
+			db,
+			idOrInternalId: referrer.id!,
+			orgId: org.id,
+			env,
+		}),
+		CusService.getFull({
+			db,
+			idOrInternalId: redeemer.id!,
+			orgId: org.id,
+			env,
+		}),
+	]);
 
-  if (addToRedeemer) {
-    let redeemerAttachParams = structuredClone({
-      ...attachParams,
-      customer: fullRedeemer,
-      cusProducts: fullRedeemer.customer_products,
-    });
+	if (fullReferrer.customer_products.find((cp) => cp.product.id === productId))
+		addToReferrer = false;
 
-    await createFullCusProduct({
-      db,
-      attachParams: redeemerAttachParams,
-      logger,
-    });
-    logger.info(`✅ Added ${fullProduct.name} to redeemer`);
+	if (fullRedeemer.customer_products.find((cp) => cp.product.id === productId))
+		addToRedeemer = false;
 
-    await deleteCusCache({
-      db,
-      customerId: fullRedeemer.id!,
-      org,
-      env,
-    });
-  }
+	function seedReq(req: ExtendedRequest) {
+		// Seed in properties that aren't usually present dependent on the trigger type
+		return {
+			...req,
+			db: req?.db ? undefined : db,
+			org: req?.org ? undefined : org,
+			env: req?.env ? undefined : env,
+			logger: req?.logger ? undefined : logger,
+			logtail: req?.logtail ? undefined : logger,
+		} as ExtendedRequest;
+	}
 
-  if (addToReferrer) {
-    await createFullCusProduct({
-      db,
-      attachParams: {
-        ...attachParams,
-        customer: fullReferrer,
-        cusProducts: fullReferrer.customer_products,
-      },
-      logger,
-    });
-    await deleteCusCache({
-      db,
-      customerId: fullReferrer.id!,
-      org,
-      env,
-    });
-    logger.info(`✅ Added ${fullProduct.name} to referrer`);
-  }
+	// Branch 1: Free add-on product or non-recurring product
+	if (!isPaidProduct || !isRecurring) {
+		logger.info(`Branch 1: Free add-on product`);
+		const attachParams: InsertCusProductParams = {
+			req,
+			org,
+			product: fullProduct,
+			prices: fullProduct.prices,
+			entitlements: fullProduct.entitlements,
+			optionsList: [],
+			entities: [],
+			freeTrial: null,
+			features: [],
+			customer: fullReferrer,
+			cusProducts: fullReferrer.customer_products,
+			replaceables: [],
+		};
 
-  await RewardRedemptionService.update({
-    db,
-    id: redemption.id,
-    updates: {
-      triggered: true,
-      applied: true,
-    },
-  });
+		if (addToRedeemer) {
+			const redeemerAttachParams = {
+				...structuredClone(attachParams),
+				customer: fullRedeemer,
+				cusProducts: fullRedeemer.customer_products,
+			};
+
+			await createFullCusProduct({
+				db,
+				attachParams: redeemerAttachParams,
+				logger,
+			});
+			logger.info(`✅ Added ${fullProduct.name} to redeemer`);
+
+			await deleteCusCache({
+				db,
+				customerId: fullRedeemer.id!,
+				org,
+				env,
+			});
+		}
+
+		if (addToReferrer) {
+			await createFullCusProduct({
+				db,
+				attachParams: {
+					...structuredClone(attachParams),
+					customer: fullReferrer,
+					cusProducts: fullReferrer.customer_products,
+				},
+				logger,
+			});
+			await deleteCusCache({
+				db,
+				customerId: fullReferrer.id!,
+				org,
+				env,
+			});
+			logger.info(`✅ Added ${fullProduct.name} to referrer`);
+		}
+
+		await RewardRedemptionService.update({
+			db,
+			id: redemption.id,
+			updates: {
+				triggered: true,
+				applied: true,
+			},
+		});
+	}
+	// Branch 2: Paid product from Customer Redemption
+	else if (req && isPaidProduct) {
+		logger.info(`Branch 2: Paid product from Customer Redemption`);
+		req = seedReq(req);
+		const ensureStripeIDs = [
+			!fullRedeemer.processor?.id &&
+				(await createCusInStripe({
+					customer: fullRedeemer,
+					org,
+					env,
+					db,
+					testClockId: req.body.testClockId,
+				})),
+			!fullReferrer.processor?.id &&
+				(await createCusInStripe({
+					customer: fullReferrer,
+					org,
+					env,
+					db,
+					testClockId: req.body.testClockId,
+				})),
+		];
+
+		const updatedIDs = await Promise.all(ensureStripeIDs);
+
+		// Update customers with new Stripe IDs if they were created
+		const updatedRedeemer = updatedIDs[0]
+			? {
+					...fullRedeemer,
+					processor: {
+						type: "stripe" as const,
+						id: updatedIDs[0].id,
+					},
+				}
+			: fullRedeemer;
+
+		const updatedReferrer = updatedIDs[1]
+			? {
+					...fullReferrer,
+					processor: {
+						type: "stripe" as const,
+						id: updatedIDs[1].id,
+					},
+				}
+			: fullReferrer;
+
+		const executions = [
+			addToRedeemer &&
+				(await handleAddProduct({
+					req,
+					attachParams: rewardProgramToAttachParams({
+						req,
+						rewardProgram: rewardProgram,
+						customer: updatedRedeemer,
+						product: fullProduct,
+						org,
+					}),
+					branch: AttachBranch.New,
+				})),
+			addToReferrer &&
+				(await handleAddProduct({
+					req,
+					attachParams: rewardProgramToAttachParams({
+						req,
+						rewardProgram: rewardProgram,
+						customer: updatedReferrer,
+						product: fullProduct,
+						org,
+					}),
+					branch: AttachBranch.New,
+				})),
+		];
+
+		await Promise.allSettled(executions)
+			.then(async (x) => {
+				if (x.every((y) => y.status === "fulfilled")) {
+					await RewardRedemptionService.update({
+						db,
+						id: redemption.id,
+						updates: {
+							triggered: true,
+							applied: true,
+						},
+					});
+				} else {
+					logger.error(`Error in executions: ${x}`);
+				}
+			})
+			.catch((error) => {
+				logger.error(`Error in executions: ${error}`);
+			});
+	}
+	// Branch 3: Paid product from Checkout
+	else if (!req && isPaidProduct) {
+		logger.info(`Branch 3: Paid product from Checkout`);
+	}
 };
