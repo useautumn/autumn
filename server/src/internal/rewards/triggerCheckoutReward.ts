b07f3280570a209343da5f513970cf79e048853b
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-12 17:40:09 +0100

    feat: 🎸 checkout trigger, cleaner response on redemption

--- server/src/internal/rewards/triggerCheckoutReward.ts
+++ server/src/internal/rewards/triggerCheckoutReward.ts
@@ -1,115 +1,140 @@
-import { RewardRedemptionService } from "./RewardRedemptionService.js";
-import { RewardCategory, RewardTriggerEvent } from "@autumn/shared";
-import { triggerFreeProduct, triggerRedemption } from "./referralUtils.js";
-import { RewardProgramService } from "../rewards/RewardProgramService.js";
-import { getRewardCat } from "./rewardUtils.js";
+import {
+	type ReferralCode,
+	type Reward,
+	RewardCategory,
+	type RewardProgram,
+	type RewardRedemption,
+	RewardTriggerEvent,
+} from "@autumn/shared";
+import type { DrizzleCli } from "@/db/initDrizzle.js";
 import { createStripeCli } from "@/external/stripe/utils.js";
-import { DrizzleCli } from "@/db/initDrizzle.js";
+import { RewardProgramService } from "../rewards/RewardProgramService.js";
+import { RewardRedemptionService } from "./RewardRedemptionService.js";
+import { triggerFreeProduct, triggerRedemption } from "./referralUtils.js";
+import { getRewardCat } from "./rewardUtils.js";
 export const runTriggerCheckoutReward = async ({
-  db,
-  payload,
-  logger,
+	db,
+	payload,
+	logger,
 }: {
-  db: DrizzleCli;
-  payload: any;
-  logger: any;
+	db: DrizzleCli;
+	payload: any;
+	logger: any;
 }) => {
-  try {
-    // Customer redeeming code, product they're buying
-    let { customer, product, org, env, subId } = payload;
+	try {
+		console.log("Triggering checkout reward in worker");
+		console.info("Triggering checkout reward in worker");
+		// Customer redeeming code, product they're buying
+		const { customer, product, org, env, subId } = payload;
+		console.info(`Customer: ${customer.name} (${customer.id})`);
+		const stripeCli = createStripeCli({
+			org,
+			env,
+		});
 
-    let stripeCli = createStripeCli({
-      org,
-      env,
-    });
+		// 1. Check if redemption exists
+		const redemptions = await RewardRedemptionService.getByCustomer({
+			db,
+			internalCustomerId: customer.internal_id, // customer that redeemed code
+			withRewardProgram: true,
+			triggered: false,
+			withReferralCode: true,
+			triggerWhen: RewardTriggerEvent.Checkout,
+		});
 
-    // 1. Check if redemption exists
-    let redemptions = await RewardRedemptionService.getByCustomer({
-      db,
-      internalCustomerId: customer.internal_id, // customer that redeemed code
-      withRewardProgram: true,
-      triggered: false,
-      withReferralCode: true,
-      triggerWhen: RewardTriggerEvent.Checkout,
-    });
+		console.info(`Found ${redemptions.length} redemptions`);
 
-    for (let redemption of redemptions) {
-      if (
-        !redemption ||
-        redemption.reward_program.when !== RewardTriggerEvent.Checkout
-      ) {
-        return;
-      }
+		for (const redemption of redemptions) {
+			if (
+				!redemption ||
+				redemption.reward_program.when !== RewardTriggerEvent.Checkout
+			) {
+				console.info(
+					"No redemption found or reward program not set to checkout, skipping",
+				);
+				return;
+			}
 
-      let { reward_program, referral_code: referralCode } = redemption;
-      let { reward } = reward_program;
+			const { reward_program, referral_code: referralCode } =
+				redemption as RewardRedemption & {
+					reward_program: RewardProgram & { reward: Reward };
+					referral_code: ReferralCode;
+				};
+			const { reward } = reward_program;
 
-      logger.info(`--------------------------------`);
-      logger.info(`CHECKING FOR CHECKOUT REWARD, ORG: ${org.slug}`);
-      logger.info(
-        `Redeemed by: ${customer.name} (${customer.id}) for referral program: ${reward_program.id}`
-      );
-      logger.info(`Referral code: ${referralCode.code} (${referralCode.id})`);
+			console.info(`--------------------------------`);
+			console.info(`CHECKING FOR CHECKOUT REWARD, ORG: ${org.slug}`);
+			console.info(
+				`Redeemed by: ${customer.name} (${customer.id}) for referral program: ${reward_program.id}`,
+			);
+			console.info(`Referral code: ${referralCode.code} (${referralCode.id})`);
+			console.info(
+				`Products: ${reward_program.product_ids?.join(", ")}, ${reward_program.reward.free_product_id}`,
+			);
 
-      if (!reward_program.product_ids.includes(product.id)) {
-        logger.info(
-          `Product ${product.name} (${product.id}) not included in referral program, skipping`
-        );
-        return;
-      }
+			if (!reward_program.product_ids?.includes(product.id)) {
+				console.info(
+					`Product ${product.name} (${product.id}) not included in referral program, skipping`,
+				);
+				if (reward_program.reward.free_product_id !== product.id) {
+					return;
+				}
+			}
 
-      // Check for trial
-      let hasTrial = false;
-      if (subId) {
-        let sub = await stripeCli.subscriptions.retrieve(subId);
-        // hasTrial = Boolean(sub.trial_end && sub.trial_end > Date.now());
-        hasTrial = sub.status === "trialing";
-      }
+			// Check for trial
+			let hasTrial = false;
+			if (subId) {
+				const sub = await stripeCli.subscriptions.retrieve(subId);
+				// hasTrial = Boolean(sub.trial_end && sub.trial_end > Date.now());
+				hasTrial = sub.status === "trialing";
+			}
 
-      if (hasTrial) {
-        logger.info(`Subscription is on trial, not triggering reward`);
-        return;
-      }
+			if (hasTrial) {
+				console.info(`Subscription is on trial, not triggering reward`);
+				return;
+			}
 
-      // Get redemption count
-      let redemptionCount = await RewardProgramService.getCodeRedemptionCount({
-        db,
-        referralCodeId: referralCode.id,
-      });
+			// Get redemption count
+			const redemptionCount = await RewardProgramService.getCodeRedemptionCount(
+				{
+					db,
+					referralCodeId: referralCode.id,
+				},
+			);
 
-      if (redemptionCount >= reward_program.max_redemptions) {
-        logger.info(
-          `Max redemptions reached, not triggering latest redemption`
-        );
-        return;
-      }
+			if (redemptionCount >= reward_program.max_redemptions!) {
+				console.info(
+					`Max redemptions reached, not triggering latest redemption`,
+				);
+				return;
+			}
 
-      let rewardCat = getRewardCat(reward);
-      if (rewardCat === RewardCategory.FreeProduct) {
-        await triggerFreeProduct({
-          db,
-          referralCode,
-          redeemer: customer,
-          rewardProgram: reward_program,
-          org,
-          env,
-          logger,
-          redemption,
-        });
-      } else {
-        await triggerRedemption({
-          db,
-          referralCode,
-          org,
-          env,
-          logger,
-          reward,
-          redemption,
-        });
-      }
-    }
-  } catch (error) {
-    logger.error("Failed to trigger checkout reward");
-    logger.error(error);
-  }
+			const rewardCat = getRewardCat(reward);
+			if (rewardCat === RewardCategory.FreeProduct) {
+				await triggerFreeProduct({
+					db,
+					referralCode,
+					redeemer: customer,
+					rewardProgram: reward_program,
+					org,
+					env,
+					logger,
+					redemption,
+				});
+			} else {
+				await triggerRedemption({
+					db,
+					referralCode,
+					org,
+					env,
+					logger,
+					reward,
+					redemption,
+				});
+			}
+		}
+	} catch (error) {
+		console.error("Failed to trigger checkout reward");
+		console.error(error);
+	}
 };
