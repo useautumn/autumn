9e7835bbfb6e2939fe0bdf986aa8cf0eb38bb025
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-15 15:25:47 +0100

    refactor: 💡 type sugar :)

--- server/src/queue/queueUtils.ts
+++ server/src/queue/queueUtils.ts
@@ -1,30 +1,39 @@
+import type { Price } from "@autumn/shared";
 import RecaseError from "@/utils/errorUtils.js";
+import { JobName } from "./JobName.js";
 import { QueueManager } from "./QueueManager.js";
 
-export const addTaskToQueue = async ({
-  jobName,
-  payload,
+export interface JobNameToPayloadType {
+	[JobName.RewardMigration]: {
+		oldPrices: Price[];
+		newPrices: Price[];
+	};
+}
+
+export const addTaskToQueue = async <T extends keyof JobNameToPayloadType>({
+	jobName,
+	payload,
 }: {
-  jobName: string;
-  payload: any;
+	jobName: T;
+	payload: JobNameToPayloadType[T];
 }) => {
-  try {
-    const queue = await QueueManager.getQueue({ useBackup: false });
-    await queue.add(jobName, payload);
-  } catch (error: any) {
-    try {
-      console.log(`Adding ${jobName} to backup queue`);
-      const backupQueue = await QueueManager.getQueue({ useBackup: true });
-      await backupQueue.add(jobName, payload);
-    } catch (error: any) {
-      throw new RecaseError({
-        message: `Failed to add ${jobName} to queue (backup)`,
-        code: "EVENT_QUEUE_ERROR",
-        statusCode: 500,
-        data: {
-          message: error.message,
-        },
-      });
-    }
-  }
+	try {
+		const queue = await QueueManager.getQueue({ useBackup: false });
+		await queue.add(jobName, payload);
+	} catch (error: any) {
+		try {
+			console.log(`Adding ${jobName} to backup queue`);
+			const backupQueue = await QueueManager.getQueue({ useBackup: true });
+			await backupQueue.add(jobName, payload);
+		} catch (error: any) {
+			throw new RecaseError({
+				message: `Failed to add ${jobName} to queue (backup)`,
+				code: "EVENT_QUEUE_ERROR",
+				statusCode: 500,
+				data: {
+					message: error.message,
+				},
+			});
+		}
+	}
 };
