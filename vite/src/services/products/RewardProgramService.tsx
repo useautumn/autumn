46ec3eb4b8f44b45caec7a592ba6b9c0a00e797e
sidgaikwad <gaikwadsiddharth039@gmail.com>
2025-09-19 23:21:05 +0530

    changes to have view for the updaterewardprogram and also the service for it

---
+++ vite/src/services/products/RewardProgramService.tsx
@@ -0,0 +1,37 @@
+import { RewardProgram, CreateRewardProgram } from "@autumn/shared";
+
+import { AxiosInstance } from "axios";
+
+export class RewardProgramService {
+  static async createReward({
+    axiosInstance,
+    data,
+  }: {
+    axiosInstance: AxiosInstance;
+    data: CreateRewardProgram;
+  }) {
+    await axiosInstance.post("/v1/reward_programs", data);
+  }
+
+  static async deleteReward({
+    axiosInstance,
+    internalId,
+  }: {
+    axiosInstance: AxiosInstance;
+    internalId: string;
+  }) {
+    await axiosInstance.delete(`/v1/reward_programs/${internalId}`);
+  }
+
+  static async updateReward({
+    axiosInstance,
+    internalId,
+    data,
+  }: {
+    axiosInstance: AxiosInstance;
+    internalId: string;
+    data: RewardProgram;
+  }) {
+    await axiosInstance.post(`/v1/reward_programs/${internalId}`, data);
+  }
+}
---
+++ vite/src/views/products/rewards/reward-programs/UpdateRewardPrograms.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useAxiosInstance } from "@/services/useAxiosInstance";
+import { toast } from "sonner";
+import { Reward } from "@autumn/shared";
+import { useEnv } from "@/utils/envUtils";
+import { RewardService } from "@/services/products/RewardService";
+import { getBackendErr } from "@/utils/genUtils";
+import { WarningBox } from "@/components/general/modal-components/WarningBox";
+import { useRewardsQuery } from "@/hooks/queries/useRewardsQuery";
+import { RewardProgramConfig } from "./RewardProgramConfig";
+import { RewardProgramService } from "@/services/products/RewardProgramService";
+
+function UpdateRewardProgram({
+  open,
+  setOpen,
+  selectedReward,
+  setSelectedReward,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  selectedReward: Reward | null;
+  setSelectedReward: (reward: Reward) => void;
+}) {
+  const [updateLoading, setUpdateLoading] = useState(false);
+  const { refetch } = useRewardsQuery();
+
+  const env = useEnv();
+  const axiosInstance = useAxiosInstance({ env });
+
+  const handleUpdate = async () => {
+    setUpdateLoading(true);
+    try {
+      await RewardProgramService.updateReward({
+        axiosInstance,
+        internalId: selectedReward!.internal_id,
+        data: selectedReward!,
+      });
+      toast.success("Reward updated successfully");
+      await refetch();
+      setOpen(false);
+    } catch (error) {
+      toast.error(getBackendErr(error, "Failed to update coupon"));
+    }
+    setUpdateLoading(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="w-[500px]">
+        <DialogTitle>Update Reward Programs</DialogTitle>
+        <WarningBox>
+          Existing customers with this coupon will not be affected
+        </WarningBox>
+
+        {selectedReward && (
+          <RewardProgramConfig
+            rewardProgram={selectedReward}
+            setRewardProgram={setSelectedReward}
+          />
+        )}
+
+        <DialogFooter>
+          <Button
+            isLoading={updateLoading}
+            onClick={() => handleUpdate()}
+            variant="gradientPrimary"
+          >
+            Update
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default UpdateRewardProgram;
