4a1e06b16505ba18bb005e2c132ab486b884f5c5
John Yeo <johnyeocx@gmail.com>
2025-09-22 12:05:35 +0100

    chore: starting edit plan redesign

---
+++ shared/models/productV3Models/productV3Response.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { AppEnv } from "../genModels/genEnums.js";
+import { AttachScenario } from "../checkModels/checkPreviewModels.js";
+import { ProductPropertiesSchema } from "../productV2Models/productResponseModels.js";
+import { ProductItemInterval } from "../productV2Models/productItemModels/productItemModels.js";
+
+export const PlanResponseSchema = z.object({
+  id: z.string(),
+  name: z.string().nullable(),
+  group: z.string().nullable(),
+  env: z.nativeEnum(AppEnv),
+  is_add_on: z.boolean(),
+  is_default: z.boolean(),
+  archived: z.boolean().optional(),
+  version: z.number(),
+  created_at: z.number(),
+
+  price: z
+    .object({
+      amount: z.number(),
+      interval: z.nativeEnum(ProductItemInterval).nullable(),
+    })
+    .nullable(),
+
+  // features:
+
+  // items: z.array(ProductItemResponseSchema),
+  // free_trial: FreeTrialResponseSchema.nullable(),
+  // base_variant_id: z.string().nullable(),
+
+  scenario: z.nativeEnum(AttachScenario).optional(),
+  properties: ProductPropertiesSchema.optional(),
+});
