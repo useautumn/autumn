fa422c16b69b9618c31fae1aa303832066ba0fbf
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-25 13:19:41 +0100

    feat: 🎸 working plan type badge

--- vite/src/views/products/product/product-item/ProductItemContext.tsx
+++ vite/src/views/products/product/product-item/ProductItemContext.tsx
@@ -1,9 +1,18 @@
+import type { ProductItem } from "@autumn/shared";
 import { createContext, useContext } from "react";
 
-export const ProductItemContext = createContext<any>(null);
+export interface ProductItemContextType {
+	item: ProductItem;
+	setItem: (item: ProductItem) => void;
+}
+
+export const ProductItemContext = createContext<ProductItemContextType>({
+	item: {},
+	setItem: () => {},
+});
 
 export const useProductItemContext = () => {
-	const context = useContext(ProductItemContext);
+	const context = useContext<ProductItemContextType>(ProductItemContext);
 
 	if (context === undefined) {
 		throw new Error(
