101b54c39e340fea7b9fe0a999484b2afdf4a9bf
John Yeo <johnyeocx@gmail.com>
2025-09-25 16:29:10 +0100

    feat: created dialog

--- vite/src/components/v2/dialogs/Dialog.tsx
+++ vite/src/components/v2/dialogs/Dialog.tsx
@@ -60,7 +60,10 @@ function DialogContent({
 			<DialogPrimitive.Content
 				data-slot="dialog-content"
 				className={cn(
-					"bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
+					"data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[40%] left-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] rounded-lg border shadow-lg duration-200",
+
+					"w-full max-w-md gap-3 bg-input-background",
+					"p-4",
 					className,
 				)}
 				{...props}
