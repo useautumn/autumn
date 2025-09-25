f67d8ad1ad50cd14080acdcda44ce4f0ffe770ff
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-25 16:13:51 +0100

    feat: 🎸 types for ProductItemContext

--- vite/src/views/products/product/product-item/ProductItemContext.tsx
+++ vite/src/views/products/product/product-item/ProductItemContext.tsx
@@ -2,13 +2,23 @@ import type { ProductItem } from "@autumn/shared";
 import { createContext, useContext } from "react";
 
 export interface ProductItemContextType {
-	item: ProductItem;
+	item: ProductItem | null;
 	setItem: (item: ProductItem) => void;
+	selectedIndex: number;
+	showCreateFeature: boolean;
+	setShowCreateFeature: (show: boolean) => void;
+	isUpdate: boolean;
+	handleUpdateProductItem: () => Promise<any>;
 }
 
 export const ProductItemContext = createContext<ProductItemContextType>({
-	item: {},
+	item: null,
 	setItem: () => {},
+	selectedIndex: 0,
+	showCreateFeature: false,
+	setShowCreateFeature: () => {},
+	isUpdate: false,
+	handleUpdateProductItem: async () => null,
 });
 
 export const useProductItemContext = () => {
