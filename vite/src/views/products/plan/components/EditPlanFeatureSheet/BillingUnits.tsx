f70442465e9b83084317f90b07f93bbd5b32b3ab
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-25 13:17:23 +0100

    feat: 🎸 billing units works

--- vite/src/views/products/plan/components/EditPlanFeatureSheet/BillingUnits.tsx
+++ vite/src/views/products/plan/components/EditPlanFeatureSheet/BillingUnits.tsx
@@ -8,29 +8,16 @@ import { Button } from "@/components/v2/buttons/Button";
 import { Input } from "@/components/v2/inputs/Input";
 import { useOrg } from "@/hooks/common/useOrg";
 import { cn } from "@/lib/utils";
+import { useProductItemContext } from "@/views/products/product/product-item/ProductItemContext";
 
-interface BillingUnitsProps {
-	unitsPerTier: number;
-	onUnitsChange: (units: number) => void;
-	disabled?: boolean;
-}
-
-export function BillingUnits({
-	unitsPerTier,
-	onUnitsChange,
-	disabled = false,
-}: BillingUnitsProps) {
+export function BillingUnits() {
 	const { org } = useOrg();
 	const [popoverOpen, setPopoverOpen] = useState(false);
-	const [inputValue, setInputValue] = useState(unitsPerTier);
+	const { item, setItem } = useProductItemContext();
 	const triggerRef = useRef<HTMLButtonElement>(null);
 
 	const handleEnterClick = () => {
-		let num = Number(inputValue);
-		if (Number.isNaN(num) || num <= 0) {
-			num = 1;
-		}
-		onUnitsChange(num);
+		setItem({ ...item, billing_units: Number(item.billing_units) });
 		setPopoverOpen(false);
 	};
 
@@ -44,27 +31,23 @@ export function BillingUnits({
 						ref={triggerRef}
 						size="default"
 						variant="skeleton"
-						disabled={disabled}
 						className="w-fit max-w-32 text-body-secondary overflow-hidden hover:bg-transparent justify-start p-1 h-auto
 							[&:focus]:outline-none [&:focus-visible]:outline-none [&:focus]:ring-0 [&:focus-visible]:ring-0"
 					>
-						<span
-							className={cn(
-								"truncate text-xs",
-								!disabled && "border-b border-dotted border-body-secondary",
-							)}
-						>
-							{unitsPerTier === 1
+						<span className={cn("truncate text-xs")}>
+							{item.billing_units === 1
 								? `${currency} per unit`
-								: `${currency} per ${unitsPerTier} units`}
+								: `${currency} per ${item.billing_units} units`}
 						</span>
 					</Button>
 				</PopoverTrigger>
 				<PopoverContent className="max-w-md p-1" align="start">
 					<Input
 						type="number"
-						value={inputValue}
-						onChange={(e) => setInputValue(Number(e.target.value))}
+						value={item.billing_units === 0 ? "" : (item.billing_units ?? "")}
+						onChange={(e) =>
+							setItem({ ...item, billing_units: Number(e.target.value) })
+						}
 						placeholder="e.g. 100 units"
 						onKeyDown={(e) => {
 							if (e.key === "Enter") {
