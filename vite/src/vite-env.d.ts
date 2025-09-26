2a4005fc2ecf48c5e7f634c8d0abb0031b1e4aa3
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-26 12:47:59 +0100

    fix: 🐛 remove viewtransition api

--- vite/src/vite-env.d.ts
+++ vite/src/vite-env.d.ts
@@ -1,13 +1,2 @@
 /// <reference types="vite/client" />
 
-// View Transitions API
-interface Document {
-	startViewTransition?: (callback: () => void) => ViewTransition;
-}
-
-interface ViewTransition {
-	finished: Promise<void>;
-	ready: Promise<void>;
-	updateCallbackDone: Promise<void>;
-	skipTransition(): void;
-}
