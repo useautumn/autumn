33961b09e873336bde7eca7699f37a4b85ae6a3a
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-10-01 12:18:00 +0100

    fix: 🐛 exports

--- shared/index.ts
+++ shared/index.ts
@@ -131,6 +131,8 @@ export * from "./utils/productDisplayUtils/sortProductItems.js";
 export * from "./utils/productDisplayUtils.js";
 export * from "./utils/productUtils/priceToInvoiceAmount.js";
 export * from "./utils/productUtils.js";
+export * from "./utils/productV2Utils/compareProductUtils.ts/compareProductUtils.js";
 export * from "./utils/productV2Utils/productItemUtils/getProductItemRes.js";
+export * from "./utils/productV2Utils/productItemUtils/itemIntervalUtils.js";
 export * from "./utils/productV3Utils/mapToProductV3.js";
 export * from "./utils/rewardUtils/rewardMigrationUtils.js";
