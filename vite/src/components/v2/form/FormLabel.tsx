b029e85bf659add48c759b496bccaf1c73d57d9c
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-10-09 11:09:25 +0100

    feat: 🎸 various

--- vite/src/components/v2/form/FormLabel.tsx
+++ vite/src/components/v2/form/FormLabel.tsx
@@ -3,12 +3,20 @@ import { cn } from "@/lib/utils";
 export const FormLabel = ({
 	children,
 	disabled,
+	className,
 }: {
 	children: React.ReactNode;
 	disabled?: boolean;
+	className?: string;
 }) => {
 	return (
-		<div className={cn("text-form-label block mb-1", disabled && "opacity-50")}>
+		<div
+			className={cn(
+				"text-form-label block mb-1",
+				disabled && "opacity-50",
+				className,
+			)}
+		>
 			{children}
 		</div>
 	);
--- vite/src/components/v2/icons/AutumnIcons.tsx
+++ vite/src/components/v2/icons/AutumnIcons.tsx
@@ -34,23 +34,23 @@ export const IncludedUsageIcon = ({
 			<path
 				d="M13.5 13.5H3.5C3.36739 13.5 3.24021 13.4473 3.14645 13.3536C3.05268 13.2598 3 13.1326 3 13V4.5L4 2.5H13L14 4.5V13C14 13.1326 13.9473 13.2598 13.8536 13.3536C13.7598 13.4473 13.6326 13.5 13.5 13.5Z"
 				stroke={color}
-				stroke-width="1.1"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.1"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M3 4.5H14"
 				stroke={color}
-				stroke-width="1.1"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.1"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M6.35742 9.25579L7.64314 10.5415L10.6431 7.5415"
 				stroke={color}
-				stroke-width="1.1"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.1"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	);
@@ -230,16 +230,16 @@ export const BooleanIcon = ({
 			<path
 				d="M8 2.84253V7.99253"
 				stroke={color}
-				stroke-width="1.2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M11.0909 3.35742C12.6411 4.36811 13.6659 6.00388 13.6659 7.99242C13.6659 9.49487 13.0691 10.9358 12.0067 11.9982C10.9443 13.0606 9.50339 13.6574 8.00094 13.6574C6.49849 13.6574 5.05757 13.0606 3.99518 11.9982C2.93278 10.9358 2.33594 9.49487 2.33594 7.99242C2.33594 6.00388 3.36079 4.36811 4.91094 3.35742"
 				stroke={color}
-				stroke-width="1.2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	);
@@ -258,16 +258,16 @@ export const ContinuousUseIcon = () => {
 			<path
 				d="M6 3.5V12.5"
 				stroke="#9210B9"
-				stroke-width="1.2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M1.5 10C2.03043 10 2.53914 9.78929 2.91421 9.41421C3.28929 9.03914 3.5 8.53043 3.5 8C3.5 7.46957 3.28929 6.96086 2.91421 6.58579C2.53914 6.21071 2.03043 6 1.5 6V4C1.5 3.86739 1.55268 3.74021 1.64645 3.64645C1.74021 3.55268 1.86739 3.5 2 3.5H14C14.1326 3.5 14.2598 3.55268 14.3536 3.64645C14.4473 3.74021 14.5 3.86739 14.5 4V6C13.9696 6 13.4609 6.21071 13.0858 6.58579C12.7107 6.96086 12.5 7.46957 12.5 8C12.5 8.53043 12.7107 9.03914 13.0858 9.41421C13.4609 9.78929 13.9696 10 14.5 10V12C14.5 12.1326 14.4473 12.2598 14.3536 12.3536C14.2598 12.4473 14.1326 12.5 14 12.5H2C1.86739 12.5 1.74021 12.4473 1.64645 12.3536C1.55268 12.2598 1.5 12.1326 1.5 12V10Z"
 				stroke="#9210B9"
-				stroke-width="1.2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	);
@@ -292,9 +292,9 @@ export const DefaultIcon = ({
 			<path
 				d="M11.875 2.6875H3.125C2.88338 2.6875 2.6875 2.88338 2.6875 3.125V11.875C2.6875 12.1166 2.88338 12.3125 3.125 12.3125H11.875C12.1166 12.3125 12.3125 12.1166 12.3125 11.875V3.125C12.3125 2.88338 12.1166 2.6875 11.875 2.6875Z"
 				stroke={color}
-				stroke-width="1.1"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.1"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<circle cx="7.50039" cy="7.50015" r="1.225" fill={color} />
 		</svg>
@@ -320,16 +320,16 @@ export const FreeTrialIcon = ({
 			<path
 				d="M11.875 2.1875H3.125C2.88338 2.1875 2.6875 2.38338 2.6875 2.625V11.375C2.6875 11.6166 2.88338 11.8125 3.125 11.8125H11.875C12.1166 11.8125 12.3125 11.6166 12.3125 11.375V2.625C12.3125 2.38338 12.1166 2.1875 11.875 2.1875Z"
 				stroke={color}
-				stroke-width="1.1"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.1"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M7.22852 4.5V7.5H10.2285"
 				stroke={color}
-				stroke-width="1.1"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.1"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	);
