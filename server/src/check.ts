c63248d0f5dcf20c3af0cdce296d75ccfe337427
John Yeo <johnyeocx@gmail.com>
2025-10-14 19:44:05 +0100

    fix: scan script

--- server/src/check.ts
+++ server/src/check.ts
@@ -36,14 +36,14 @@ import { checkCusSubCorrect } from "./utils/checkUtils/checkCustomerCorrect.js";
 
 const { db } = initDrizzle({ maxConnections: 5 });
 
-let orgSlugs = process.env.ORG_SLUGS!.split(",");
+const orgSlugs = process.env.ORG_SLUGS!.split(",");
 const skipEmails = process.env.SKIP_EMAILS!.split(",");
 const skipIds = [
 	"cus_2tXCCwC6iyiftgA6ndSo1Ubb2dx",
 	"DxG668K7uDd0Vahk54YWjvCGVgf2",
 ];
 
-orgSlugs = ["lumenary"];
+// orgSlugs = ["lumenary"];
 const customerId = null;
 // customerId = "EBbxiRv9QJKXFy5WiAKeHi";
 
