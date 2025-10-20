13d13dcc056b66722e2e16f0ecdc3ed0e9a48ea0
John Yeo <johnyeocx@gmail.com>
2025-10-20 13:44:18 +0100

    fix: update product, group optional

--- shared/api/products/productOpModels.ts
+++ shared/api/products/productOpModels.ts
@@ -35,7 +35,7 @@ export const UpdateProductV2ParamsSchema = z.object({
 	is_add_on: z.boolean().optional(),
 	is_default: z.boolean().optional(),
 	version: z.number().optional(),
-	group: z.string().nonempty().nullable(),
+	group: z.string().nonempty().nullable().optional(),
 	archived: z.boolean().optional(),
 
 	items: z.array(CreateProductItemParamsSchema).optional(),
