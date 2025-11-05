6334c1bfd3541d595fec34936bd25054118654e7
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-11-05 13:11:45 +0000

    feat: 🎸 processor object

--- shared/models/cusModels/cusTable.ts
+++ shared/models/cusModels/cusTable.ts
@@ -1,18 +1,27 @@
+import { sql } from "drizzle-orm";
 import {
+	foreignKey,
+	jsonb,
+	numeric,
 	pgTable,
 	text,
-	numeric,
-	jsonb,
 	unique,
-	foreignKey,
 } from "drizzle-orm/pg-core";
 import { collatePgColumn } from "../../db/utils.js";
 import { organizations } from "../orgModels/orgTable.js";
-import { sql } from "drizzle-orm";
 
 export type CustomerProcessor = {
 	type: "stripe";
 	id: string;
+};
+
+export type ExternalProcessors = {
+	vercel?: VercelProcessor;
+};
+
+export type VercelProcessor = {
+	installation_id: string;
+	access_token: string;
 };
 
 export const customers = pgTable(
@@ -26,9 +35,11 @@ export const customers = pgTable(
 		email: text(),
 		fingerprint: text().default(sql`null`),
 		metadata: jsonb().$type<Record<string, unknown>>(),
-
 		env: text().notNull(),
 		processor: jsonb().$type<CustomerProcessor>(),
+		processors: jsonb()
+			.$type<ExternalProcessors>()
+			.default({} as ExternalProcessors),
 	},
 	(table) => [
 		unique("cus_id_constraint").on(table.org_id, table.id, table.env),
