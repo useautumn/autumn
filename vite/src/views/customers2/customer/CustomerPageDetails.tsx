9ce0568cc476705f15668d89d82e583960b66530
Ayush Rodrigues <joesj2905@gmail.com>
2025-11-10 10:03:13 +0000

    fix biome format

--- vite/src/views/customers2/customer/CustomerPageDetails.tsx
+++ vite/src/views/customers2/customer/CustomerPageDetails.tsx
@@ -1,7 +1,6 @@
 import { faStripe } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ArrowSquareOutIcon, FingerprintIcon } from "@phosphor-icons/react";
-import { ArrowUpRightFromSquare } from "lucide-react";
 import { Button } from "@/components/v2/buttons/Button";
 import { CopyButton } from "@/components/v2/buttons/CopyButton";
 import { useOrgStripeQuery } from "@/hooks/queries/useOrgStripeQuery";
@@ -10,52 +9,52 @@ import { getStripeCusLink } from "@/utils/linkUtils";
 import { useCustomerContext } from "./CustomerContext";
 
 const mutedDivClassName =
-  "py-0.5 px-1.5 bg-muted rounded-lg text-t3 text-sm flex items-center gap-1 h-6 max-w-48 truncate ";
+	"py-0.5 px-1.5 bg-muted rounded-lg text-t3 text-sm flex items-center gap-1 h-6 max-w-48 truncate ";
 
 const placeholderText = "NULL";
 
 export const CustomerPageDetails = () => {
-  const { customer } = useCustomerContext();
-  const env = useEnv();
-  const { stripeAccount } = useOrgStripeQuery();
+	const { customer } = useCustomerContext();
+	const env = useEnv();
+	const { stripeAccount } = useOrgStripeQuery();
 
-  return (
-    <div className="flex gap-2">
-      <CopyButton
-        text={customer.id ?? placeholderText}
-        size="sm"
-        innerClassName="!text-sm !font-sans max-w-48 truncate"
-      />
-      {customer.email && (
-        <div className={mutedDivClassName}>
-          <span className="truncate">{customer.email ?? placeholderText}</span>
-        </div>
-      )}
-      {customer.fingerprint && (
-        <div className={mutedDivClassName}>
-          <FingerprintIcon size={16} />
-          {customer.fingerprint ?? placeholderText}
-        </div>
-      )}
-      {customer.processor?.id && (
-        <Button
-          variant="muted"
-          size="sm"
-          onClick={() => {
-            window.open(
-              getStripeCusLink({
-                customerId: customer.processor.id,
-                env,
-                accountId: stripeAccount?.id,
-              }),
-              "_blank"
-            );
-          }}
-        >
-          <FontAwesomeIcon icon={faStripe} className="!h-6 !w-6 text-t3" />
-          <ArrowSquareOutIcon size={12} />
-        </Button>
-      )}
-    </div>
-  );
+	return (
+		<div className="flex gap-2">
+			<CopyButton
+				text={customer.id ?? placeholderText}
+				size="sm"
+				innerClassName="!text-sm !font-sans max-w-48 truncate"
+			/>
+			{customer.email && (
+				<div className={mutedDivClassName}>
+					<span className="truncate">{customer.email ?? placeholderText}</span>
+				</div>
+			)}
+			{customer.fingerprint && (
+				<div className={mutedDivClassName}>
+					<FingerprintIcon size={16} />
+					{customer.fingerprint ?? placeholderText}
+				</div>
+			)}
+			{customer.processor?.id && (
+				<Button
+					variant="muted"
+					size="sm"
+					onClick={() => {
+						window.open(
+							getStripeCusLink({
+								customerId: customer.processor.id,
+								env,
+								accountId: stripeAccount?.id,
+							}),
+							"_blank",
+						);
+					}}
+				>
+					<FontAwesomeIcon icon={faStripe} className="!h-6 !w-6 text-t3" />
+					<ArrowSquareOutIcon size={12} />
+				</Button>
+			)}
+		</div>
+	);
 };
--- vite/src/views/customers2/customer/CustomerView2.tsx
+++ vite/src/views/customers2/customer/CustomerView2.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { PencilIcon, TrashIcon } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/v2/buttons/Button";
 import { pushPage } from "@/utils/genUtils";
 import ErrorScreen from "@/views/general/ErrorScreen";
 import LoadingScreen from "@/views/general/LoadingScreen";
@@ -20,62 +17,62 @@ import { CustomerContext } from "./CustomerContext";
 import { CustomerPageDetails } from "./CustomerPageDetails";
 
 export default function CustomerView2() {
-  const [searchParams] = useSearchParams();
-  const entityIdParam = searchParams.get("entity_id");
+	const [searchParams] = useSearchParams();
+	const entityIdParam = searchParams.get("entity_id");
 
-  const { customer, isLoading: cusLoading } = useCusQuery();
+	const { customer, isLoading: cusLoading } = useCusQuery();
 
-  useCusReferralQuery();
+	useCusReferralQuery();
 
-  const [entityId, setEntityId] = useState(entityIdParam);
+	const [entityId, setEntityId] = useState(entityIdParam);
 
-  useEffect(() => {
-    if (entityIdParam) {
-      setEntityId(entityIdParam);
-    } else {
-      setEntityId(null);
-    }
-  }, [entityIdParam]);
+	useEffect(() => {
+		if (entityIdParam) {
+			setEntityId(entityIdParam);
+		} else {
+			setEntityId(null);
+		}
+	}, [entityIdParam]);
 
-  if (cusLoading) return <LoadingScreen />;
+	if (cusLoading) return <LoadingScreen />;
 
-  if (!customer) {
-    return (
-      <ErrorScreen>
-        <div className="text-t2 text-sm">Customer not found</div>
-        <Link
-          className="text-t3 text-xs hover:underline"
-          to={pushPage({ path: "/customers" })}
-        >
-          Return
-        </Link>
-      </ErrorScreen>
-    );
-  }
+	if (!customer) {
+		return (
+			<ErrorScreen>
+				<div className="text-t2 text-sm">Customer not found</div>
+				<Link
+					className="text-t3 text-xs hover:underline"
+					to={pushPage({ path: "/customers" })}
+				>
+					Return
+				</Link>
+			</ErrorScreen>
+		);
+	}
 
-  return (
-    <CustomerContext.Provider
-      value={{ customer, entityId: entityId, setEntityId }}
-    >
-      <div className="flex flex-col [&>*:not([data-slot=separator-root])]:px-4 [&>*:not([data-slot=separator-root])]:py-8 [&>*:not([data-slot=separator-root])]:max-w-4xl [&>*:not([data-slot=separator-root])]:mx-auto">
-        <div className="flex items-end justify-between w-full gap-4">
-          <div className="flex flex-col w-full">
-            <CustomerBreadcrumbs />
-            <h3 className="text-md font-semibold text-t2 pt-2.5 pb-2">
-              {customer.name}
-            </h3>
-            <CustomerPageDetails />
-          </div>
-          <CustomerActions />
-        </div>
-        {/* <Separator className="my-2" /> */}
-        <div className="flex flex-col gap-20 mt-4">
-          <CustomerProductsTable />
-          <CustomerFeatureUsageTable />
-          <CustomerUsageAnalyticsTable />
-          <CustomerInvoicesTable />
-        </div>
-      </div>
-    </CustomerContext.Provider>
-  );
+	return (
+		<CustomerContext.Provider
+			value={{ customer, entityId: entityId, setEntityId }}
+		>
+			<div className="flex flex-col [&>*:not([data-slot=separator-root])]:px-4 [&>*:not([data-slot=separator-root])]:py-8 [&>*:not([data-slot=separator-root])]:max-w-4xl [&>*:not([data-slot=separator-root])]:mx-auto">
+				<div className="flex items-end justify-between w-full gap-4">
+					<div className="flex flex-col w-full">
+						<CustomerBreadcrumbs />
+						<h3 className="text-md font-semibold text-t2 pt-2.5 pb-2">
+							{customer.name}
+						</h3>
+						<CustomerPageDetails />
+					</div>
+					<CustomerActions />
+				</div>
+				{/* <Separator className="my-2" /> */}
+				<div className="flex flex-col gap-20 mt-4">
+					<CustomerProductsTable />
+					<CustomerFeatureUsageTable />
+					<CustomerUsageAnalyticsTable />
+					<CustomerInvoicesTable />
+				</div>
+			</div>
+		</CustomerContext.Provider>
+	);
 }
