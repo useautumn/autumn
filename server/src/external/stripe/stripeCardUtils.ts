b6dea99c3c19f08d9ee70ca35bf478ff9f59da1d
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-11-10 19:04:11 +0000

    feat: 🎸 custom payment methods

--- server/src/external/stripe/stripeCardUtils.ts
+++ server/src/external/stripe/stripeCardUtils.ts
@@ -1,3 +1,8 @@
+import type { AppEnv, ProcessorConfigs } from "@autumn/shared";
+import type { Customer } from "@shared/models/cusModels/cusModels.js";
+import type { Organization } from "@shared/models/orgModels/orgTable.js";
+import { createStripeCli } from "../connect/createStripeCli.js";
+
 export const isStripeCardDeclined = (error: any) => {
 	return (
 		error.code === "card_declined" ||
@@ -5,7 +10,53 @@ export const isStripeCardDeclined = (error: any) => {
 		error.code === "incorrect_cvc" ||
 		error.code === "processing_error" ||
 		error.code === "incorrect_number" ||
-		error.code == "subscription_payment_intent_requires_action" ||
-		error.code == "payment_intent_payment_attempt_failed" // Stripe link
+		error.code === "subscription_payment_intent_requires_action" ||
+		error.code === "payment_intent_payment_attempt_failed" // Stripe link
 	);
+};
+
+export const createCustomStripeCard = async ({
+	org,
+	env,
+	customer,
+	processor = "vercel",
+	processorData,
+	defaultPaymentMethod = false,
+}: {
+	org: Organization;
+	env: AppEnv;
+	customer: Customer;
+	processor?: keyof ProcessorConfigs;
+	processorData?: {
+		name: string;
+		email: string;
+	};
+	defaultPaymentMethod?: boolean;
+}) => {
+	const stripeCli = createStripeCli({ org, env });
+	const customPaymentMethodId =
+		org.processor_configs?.[processor]?.custom_payment_method?.[env];
+	if (customPaymentMethodId?.trim()) {
+		const pm = await stripeCli.paymentMethods.create({
+			type: "custom",
+			custom: {
+				type: customPaymentMethodId.trim(),
+			},
+			billing_details: {
+				name: processorData?.name,
+				email: processorData?.email,
+			},
+		});
+		await stripeCli.paymentMethods.attach(pm.id, {
+			customer: customer.processor.id,
+		});
+		if (defaultPaymentMethod) {
+			await stripeCli.customers.update(customer.processor.id, {
+				invoice_settings: {
+					default_payment_method: pm.id,
+				},
+			});
+		}
+		return pm;
+	} else return null;
 };
