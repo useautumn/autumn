0946d472a0da321da2a54089342d7da1467f5374
Ayush Rodrigues <joesj2905@gmail.com>
2025-11-10 10:42:22 +0000

    biome 5

--- vite/src/components/ui/checkbox.tsx
+++ vite/src/components/ui/checkbox.tsx
@@ -5,27 +5,27 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & {
-    iconClassName?: string;
-  }
+	React.ElementRef<typeof CheckboxPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & {
+		iconClassName?: string;
+	}
 >(({ className, iconClassName, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
-    data-slot="checkbox"
-    className={cn(
-      "peer shrink-0 rounded border border-gray-300 size-4 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gray-900 data-[state=checked]:border-gray-900 transition-none",
-      className
-    )}
-    {...props}
-  >
-    <CheckboxPrimitive.Indicator
-      data-slot="checkbox-indicator"
-      className="flex items-center justify-center text-white transition-none"
-    >
-      <CheckIcon className={cn("size-3", iconClassName)} color="white" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
+	<CheckboxPrimitive.Root
+		ref={ref}
+		data-slot="checkbox"
+		className={cn(
+			"peer shrink-0 rounded border border-gray-300 size-4 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gray-900 data-[state=checked]:border-gray-900 transition-none",
+			className,
+		)}
+		{...props}
+	>
+		<CheckboxPrimitive.Indicator
+			data-slot="checkbox-indicator"
+			className="flex items-center justify-center text-white transition-none"
+		>
+			<CheckIcon className={cn("size-3", iconClassName)} color="white" />
+		</CheckboxPrimitive.Indicator>
+	</CheckboxPrimitive.Root>
 ));
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
