e13e68fc2d9a46f3961a96546aa9f9881f59ecb4
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-12-01 16:08:58 +0000

    fix: 🐛 analytics, no merge

--- vite/src/views/customers/hooks/useCusSearchQuery.tsx
+++ vite/src/views/customers/hooks/useCusSearchQuery.tsx
@@ -61,3 +61,74 @@ export const useCusSearchQuery = () => {
 		isFetchingUncached,
 	};
 };
+
+export const useCusSearchQueryV2 = ({
+	search,
+	filters = {},
+	page,
+	page_size,
+}: {
+	search: string;
+	filters?: {
+		status?: string;
+		version?: string;
+		none?: string;
+	};
+	page?: number;
+	page_size: number;
+}) => {
+	const axiosInstance = useAxiosInstance();
+	const fetcher = async () => {
+		const { data } = await axiosInstance.post(`/customers/all/search`, {
+			search: search || "",
+			filters: {
+				status: filters.status,
+				version: filters.version,
+				none: filters.none,
+			},
+			page: page || 1,
+			page_size: page_size || 30,
+		});
+		return { customers: data.customers, totalCount: data.totalCount };
+	};
+
+	const {
+		data,
+		isLoading,
+		error,
+		refetch,
+		isRefetching,
+		isFetching,
+		isPending,
+		isPlaceholderData,
+	} = useQuery<{
+		customers: CustomerWithProducts[];
+		totalCount: number;
+	}>({
+		queryKey: [
+			"customers",
+			page,
+			page_size,
+			filters?.status,
+			filters?.version,
+			filters?.none,
+			search,
+		],
+		queryFn: fetcher,
+		placeholderData: keepPreviousData,
+	});
+
+	const isFetchingUncached = Boolean(
+		isPending || (isFetching && isPlaceholderData),
+	);
+
+	return {
+		customers: data?.customers || [],
+		totalCount: data?.totalCount || 0,
+		isLoading,
+		error,
+		refetch,
+		isRefetching,
+		isFetchingUncached,
+	};
+};
