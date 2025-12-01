20b1b6bdeaa8c44cf5e992c7a8c2cb19a5298742
Ayush Rodrigues <joesj2905@gmail.com>
2025-12-01 20:46:52 +0000

    enable full analytics button

--- vite/src/views/customers2/components/table/customer-usage-analytics/CustomerUsageAnalyticsFullButton.tsx
+++ vite/src/views/customers2/components/table/customer-usage-analytics/CustomerUsageAnalyticsFullButton.tsx
@@ -2,8 +2,10 @@ import { ChartBarIcon } from "@phosphor-icons/react";
 import { useNavigate } from "react-router";
 import { Button } from "@/components/v2/buttons/Button";
 import { pushPage } from "@/utils/genUtils";
+import { useCusQuery } from "@/views/customers/customer/hooks/useCusQuery";
 
 export function CustomerUsageAnalyticsFullButton() {
+	const { customer } = useCusQuery();
 	const navigate = useNavigate();
 
 	return (
@@ -12,7 +14,7 @@ export function CustomerUsageAnalyticsFullButton() {
 			size="mini"
 			className="flex items-center gap-1"
 			onClick={() => {
-				pushPage({ path: "/analytics", navigate });
+				pushPage({ path: `/analytics?customer_id=${customer.id}`, navigate });
 			}}
 		>
 			<ChartBarIcon className="text-t3" />
