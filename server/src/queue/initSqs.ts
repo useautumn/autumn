da81eb7a90eb3da66807109b62cb21c63d72c27b
John Yeo <johnyeocx@gmail.com>
2025-12-03 09:06:23 +0000

    fix: sqs region

--- server/src/queue/initSqs.ts
+++ server/src/queue/initSqs.ts
@@ -1,12 +1,11 @@
 import { SQSClient } from "@aws-sdk/client-sqs";
 
 export const sqs = new SQSClient({
-	region: process.env.AWS_REGION || "eu-west-2",
+	region: process.env.NODE_ENV === "production" ? "us-west-2" : "eu-west-2",
 	credentials: {
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
 	},
 });
-
 // SQS Queue URL - you'll need to create this queue in AWS console or via terraform
 export const QUEUE_URL = process.env.SQS_QUEUE_URL || "";
