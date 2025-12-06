139ee97baadedbc3136836b0fa49a2b9fbcf4507
Kyle <kgrahammatzen@gmail.com>
2025-12-05 21:45:17 -0500

    feat(frontend): add user profile editing to settings modal

--- vite/src/views/main-sidebar/components/ManageOrg.tsx
+++ vite/src/views/main-sidebar/components/ManageOrg.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
 	Dialog,
@@ -10,6 +11,7 @@ import { InvitePopover } from "../org-dropdown/manage-org/InvitePopover";
 import { OrgInvitesList } from "../org-dropdown/manage-org/OrgInvitesList";
 import { OrgMembersList } from "../org-dropdown/manage-org/OrgMembersList";
 import { OrgDetails } from "./OrgDetails";
+import { UserDetails } from "./UserDetails";
 
 export const ManageOrg = ({
 	open,
@@ -18,20 +20,29 @@ export const ManageOrg = ({
 	open: boolean;
 	setOpen: (open: boolean) => void;
 }) => {
+	const [currentTab, setCurrentTab] = useState("user");
+
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogTrigger asChild></DialogTrigger>
 			<DialogContent className="gap-0 p-0 rounded-xs w-[90%] max-w-[650px] h-[450px] flex flex-col justify-between">
 				<div className="flex flex-col gap-6 overflow-hidden h-full">
 					<DialogHeader className="px-6 pt-6">
-						<DialogTitle>Manage Organization</DialogTitle>
+						<DialogTitle>Settings</DialogTitle>
 					</DialogHeader>
 					<div className="flex flex-col gap-6 h-full overflow-hidden">
-						<Tabs className="flex flex-col h-full" defaultValue="details">
+						<Tabs
+							className="flex flex-col h-full"
+							value={currentTab}
+							onValueChange={setCurrentTab}
+						>
 							<div className="flex justify-between items-center px-6">
 								<TabsList className="p-0 flex gap-4 justify-start w-fit !bg-transparent">
-									<TabsTrigger className="" value="details">
-										Details
+									<TabsTrigger className="" value="user">
+										User
+									</TabsTrigger>
+									<TabsTrigger className="" value="organization">
+										Organization
 									</TabsTrigger>
 									<TabsTrigger className="" value="members">
 										Members
@@ -40,10 +51,16 @@ export const ManageOrg = ({
 										Invites
 									</TabsTrigger>
 								</TabsList>
-								<InvitePopover />
+								{(currentTab === "members" || currentTab === "invites") && (
+									<InvitePopover />
+								)}
 							</div>
 
-							<TabsContent value="details" className="h-full overflow-y-auto">
+							<TabsContent value="user" className="h-full overflow-y-auto">
+								<UserDetails />
+							</TabsContent>
+
+							<TabsContent value="organization" className="h-full overflow-y-auto">
 								<OrgDetails />
 							</TabsContent>
 
@@ -56,11 +73,6 @@ export const ManageOrg = ({
 						</Tabs>
 					</div>
 				</div>
-				{/* <DialogFooter variant="new">
-          <Button variant="add" onClick={handleCreate} isLoading={isLoading}>
-            Create
-          </Button>
-        </DialogFooter> */}
 			</DialogContent>
 		</Dialog>
 	);
