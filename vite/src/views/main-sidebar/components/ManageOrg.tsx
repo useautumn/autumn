20574995f6d4c31c67678f2bdcc2b9a92deafe6c
Scofield <autumnenjoyer0@gmail.com>
2025-12-07 14:54:09 +0000

    chore: invite member nitpicks/cleanup

--- vite/src/views/main-sidebar/components/ManageOrg.tsx
+++ vite/src/views/main-sidebar/components/ManageOrg.tsx
@@ -13,6 +13,10 @@ import { OrgMembersList } from "../org-dropdown/manage-org/OrgMembersList";
 import { OrgDetails } from "./OrgDetails";
 import { UserDetails } from "./UserDetails";
 
+type ManageOrgTab = "user" | "members" | "invites";
+
+const tabsContentClassName = "h-full overflow-y-auto focus-visible:ring-0";
+
 export const ManageOrg = ({
 	open,
 	setOpen,
@@ -20,7 +24,7 @@ export const ManageOrg = ({
 	open: boolean;
 	setOpen: (open: boolean) => void;
 }) => {
-	const [currentTab, setCurrentTab] = useState("user");
+	const [currentTab, setCurrentTab] = useState<ManageOrgTab>("user");
 
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
@@ -32,42 +36,37 @@ export const ManageOrg = ({
 					</DialogHeader>
 					<div className="flex flex-col gap-6 h-full overflow-hidden">
 						<Tabs
-							className="flex flex-col h-full"
+							className="flex flex-col h-full focus-visible:ring-none"
 							value={currentTab}
-							onValueChange={setCurrentTab}
+							onValueChange={setCurrentTab as (val: string) => void}
 						>
 							<div className="flex justify-between items-center px-6">
-								<TabsList className="p-0 flex gap-4 justify-start w-fit !bg-transparent">
-									<TabsTrigger className="" value="user">
-										User
-									</TabsTrigger>
-									<TabsTrigger className="" value="organization">
-										Organization
-									</TabsTrigger>
-									<TabsTrigger className="" value="members">
-										Members
-									</TabsTrigger>
-									<TabsTrigger className="" value="invites">
-										Invites
-									</TabsTrigger>
+								<TabsList className="p-0 flex gap-4 justify-start w-fit bg-transparent!">
+									<TabsTrigger value="user">User</TabsTrigger>
+									<TabsTrigger value="organization">Organization</TabsTrigger>
+									<TabsTrigger value="members">Members</TabsTrigger>
+									<TabsTrigger value="invites">Invites</TabsTrigger>
 								</TabsList>
 								{(currentTab === "members" || currentTab === "invites") && (
 									<InvitePopover />
 								)}
 							</div>
 
-							<TabsContent value="user" className="h-full overflow-y-auto">
+							<TabsContent value="user" className={tabsContentClassName}>
 								<UserDetails />
 							</TabsContent>
 
-							<TabsContent value="organization" className="h-full overflow-y-auto">
+							<TabsContent
+								value="organization"
+								className={tabsContentClassName}
+							>
 								<OrgDetails />
 							</TabsContent>
 
-							<TabsContent value="members" className="h-full overflow-y-auto">
+							<TabsContent value="members" className={tabsContentClassName}>
 								<OrgMembersList />
 							</TabsContent>
-							<TabsContent value="invites" className="h-full overflow-y-auto">
+							<TabsContent value="invites" className={tabsContentClassName}>
 								<OrgInvitesList />
 							</TabsContent>
 						</Tabs>
