f1da51663fc636940f32f06bf4bfe3736b56cad4
John Yeo <johnyeocx@gmail.com>
2025-12-08 13:55:05 +0000

    fix: default trial upgrade, free trial false

--- server/tests/_temp/temp1.test.ts
+++ server/tests/_temp/temp1.test.ts
@@ -1,54 +1,73 @@
 import { beforeAll, describe } from "bun:test";
-import { ApiVersion } from "@autumn/shared";
+import { ApiVersion, FreeTrialDuration } from "@autumn/shared";
 import { TestFeature } from "@tests/setup/v2Features.js";
 import ctx from "@tests/utils/testInitUtils/createTestContext.js";
 import chalk from "chalk";
 import { AutumnInt } from "@/external/autumn/autumnCli.js";
 import { constructProduct } from "@/utils/scriptUtils/createTestProducts.js";
-import { attachAuthenticatePaymentMethod } from "../../src/external/stripe/stripeCusUtils.js";
 import { constructFeatureItem } from "../../src/utils/scriptUtils/constructItem.js";
-import { initCustomerV3 } from "../../src/utils/scriptUtils/testUtils/initCustomerV3.js";
 import { initProductsV0 } from "../../src/utils/scriptUtils/testUtils/initProductsV0.js";
 
 const pro = constructProduct({
 	type: "pro",
+	forcePaidDefault: true,
+
+	items: [
+		constructFeatureItem({
+			featureId: TestFeature.Messages,
+			includedUsage: 1000,
+		}),
+	],
+	freeTrial: {
+		card_required: false,
+		duration: FreeTrialDuration.Day,
+		length: 7,
+		unique_fingerprint: false,
+	},
+});
+
+const premium = constructProduct({
+	type: "premium",
 	isDefault: false,
 
 	items: [
 		constructFeatureItem({
-			featureId: TestFeature.Credits,
+			featureId: TestFeature.Messages,
 			includedUsage: 1000,
 		}),
 	],
+	freeTrial: {
+		card_required: true,
+		duration: FreeTrialDuration.Day,
+		length: 7,
+		unique_fingerprint: false,
+	},
 });
+
+console.log("Pro is default", pro.is_default);
 
 describe(`${chalk.yellowBright("temp: Testing entity prorated")}`, () => {
 	const customerId = "temp";
 	const autumn: AutumnInt = new AutumnInt({ version: ApiVersion.V1_2 });
 
 	beforeAll(async () => {
-		await initCustomerV3({
-			ctx,
-			customerId,
-			customerData: {},
-			attachPm: "success",
-			withTestClock: true,
-		});
-
 		await initProductsV0({
 			ctx,
-			products: [pro],
+			products: [pro, premium],
 			prefix: customerId,
-		});
-
-		await autumn.attach({
-			customer_id: customerId,
-			product_id: pro.id,
-		});
-
-		await attachAuthenticatePaymentMethod({
-			ctx,
 			customerId,
+		});
+
+		// await initCustomerV3({
+		// 	ctx,
+		// 	customerId,
+		// 	customerData: {},
+		// 	attachPm: "success",
+		// 	withTestClock: true,
+		// });
+		await autumn.customers.create({
+			id: customerId,
+			name: customerId,
 		});
 	});
 
