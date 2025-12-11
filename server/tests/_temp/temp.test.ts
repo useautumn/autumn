d79c063fa5a7169ed1a853648d773dbb8facab53
John Yeo <johnyeocx@gmail.com>
2025-12-11 19:23:53 +0000

    fix: upgrading canceled downgrade

--- server/tests/_temp/temp.test.ts
+++ server/tests/_temp/temp.test.ts
@@ -1,8 +1,10 @@
 import { beforeAll, describe, test } from "bun:test";
 import { ApiVersion, BillingInterval, FreeTrialDuration } from "@autumn/shared";
 import { TestFeature } from "@tests/setup/v2Features.js";
+import { advanceTestClock } from "@tests/utils/stripeUtils";
 import ctx from "@tests/utils/testInitUtils/createTestContext.js";
 import chalk from "chalk";
+import { addWeeks } from "date-fns";
 import { AutumnInt } from "@/external/autumn/autumnCli.js";
 import { constructPriceItem } from "@/internal/products/product-items/productItemUtils";
 import {
@@ -18,7 +20,7 @@ import { initCustomerV3 } from "@/utils/scriptUtils/testUtils/initCustomerV3.js"
 import { initProductsV0 } from "@/utils/scriptUtils/testUtils/initProductsV0.js";
 import { CusService } from "../../src/internal/customers/CusService";
 
-const freeProd = constructRawProduct({
+const paidAddOn = constructRawProduct({
 	id: "addOn",
 	isAddOn: true,
 	items: [
@@ -33,29 +35,16 @@ const freeProd = constructRawProduct({
 	],
 });
 
-export const defaultTrialProduct = constructProduct({
+const pro = constructProduct({
 	type: "pro",
-	forcePaidDefault: true,
+
 	items: [
 		constructFeatureItem({
 			featureId: TestFeature.Messages,
 			includedUsage: 300,
 		}),
 	],
-
-	freeTrial: {
-		length: 7,
-		duration: FreeTrialDuration.Day,
-		unique_fingerprint: false,
-		card_required: false,
-	},
 });
-
-const entity = {
-	id: "entity",
-	name: "Entity",
-	feature_id: TestFeature.Users,
-};
 
 const testCase = "temp";
 
@@ -71,17 +60,29 @@ describe(`${chalk.yellowBright("temp: temporary script for testing")}`, () => {
 			env: ctx.env,
 		});
 
-		await initCustomerV3({
+		const result = await initCustomerV3({
 			ctx,
 			customerId,
-			withTestClock: false,
+			withTestClock: true,
 			attachPm: "success",
 		});
 
 		await initProductsV0({
 			ctx,
-			products: [freeProd, defaultTrialProduct],
+			products: [pro, paidAddOn],
 			prefix: testCase,
+		});
+
+		await autumnV1.attach({
+			customer_id: customerId,
+			product_id: pro.id,
+		});
+
+		await advanceTestClock({
+			stripeCli: ctx.stripeCli,
+			testClockId: result.testClockId,
+			advanceTo: addWeeks(new Date(), 3).getTime(),
+			waitForSeconds: 10,
 		});
 	});
 });
