74dd708af2c2b92a949e8bad1a73b98dab68b13a
Ayush Rodrigues <joesj2905@gmail.com>
2025-12-20 18:30:21 +0000

    style normal sheets

--- vite/src/components/v2/sheets/Sheet.tsx
+++ vite/src/components/v2/sheets/Sheet.tsx
@@ -60,13 +60,13 @@ function SheetContent({
 				className={cn(
 					"bg-card data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-0 shadow-sm transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-300",
 					side === "right" &&
-						"data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-full min-w-xs max-w-md border-l",
+						"data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right top-3 bottom-3 right-3 w-full min-w-xs max-w-md border-l border-border/40",
 					side === "left" &&
-						"data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm",
+						"data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left top-3 bottom-3 left-3 w-3/4 border-r border-border/40 sm:max-w-sm",
 					side === "top" &&
-						"data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top inset-x-0 top-0 h-auto border-b",
+						"data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top left-3 right-3 top-3 h-auto border-b border-border/40 ",
 					side === "bottom" &&
-						"data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom inset-x-0 bottom-0 h-auto border-t",
+						"data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom left-3 right-3 bottom-3 h-auto border-t border-border/40",
 					className,
 				)}
 				{...props}
