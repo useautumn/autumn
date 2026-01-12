f0cb4c08aee57b34abc4b6536ae4ad44a5297340
John Yeo <johnyeocx@gmail.com>
2026-01-12 11:08:41 +0000

    fix: removed redis warmup

--- server/src/cron.ts
+++ server/src/cron.ts
@@ -1,7 +1,5 @@
 import { initInfisical } from "./external/infisical/initInfisical.js";
 
 await initInfisical();
-const { warmupRegionalRedis } = await import("./external/redis/initRedis.js");
-await warmupRegionalRedis();
 
 await import("./cron/cronInit.js");
