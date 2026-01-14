d592856423151797e4291495fefce9a65dda47c4
Ayush Rodrigues <joesj2905@gmail.com>
2026-01-14 13:32:18 +0000

    tidied up onboarding

--- vite/src/views/onboarding4/QuickstartView.tsx
+++ vite/src/views/onboarding4/QuickstartView.tsx
@@ -1,28 +1,12 @@
-import { AppEnv } from "@autumn/shared";
 import { useNavigate } from "react-router";
-import { useOrg } from "@/hooks/common/useOrg";
-import { useAxiosInstance } from "@/services/useAxiosInstance";
-import { getEnvFromPath } from "@/utils/genUtils";
+import { pushPage } from "@/utils/genUtils";
 import { AIChatView } from "./AIChatView";
 
 export default function QuickstartView() {
 	const navigate = useNavigate();
-	const { org, mutate: mutateOrg } = useOrg();
-	const axiosInstance = useAxiosInstance();
 
 	const handleSkipToDashboard = () => {
-		if (!org?.onboarded) {
-			// Fire and forget - persist to server
-			axiosInstance
-				.patch("/v1/organization", { onboarded: true })
-				.then(() => mutateOrg())
-				.catch(console.error);
-		}
-
-		// Navigate with state flag to prevent redirect back to onboarding
-		const curEnv = getEnvFromPath(window.location.pathname);
-		const path = curEnv === AppEnv.Sandbox ? "/sandbox/products" : "/products";
-		navigate(path, { state: { skipOnboardingRedirect: true } });
+		pushPage({ path: "/products", navigate });
 	};
 
 	return <AIChatView onBack={handleSkipToDashboard} />;
