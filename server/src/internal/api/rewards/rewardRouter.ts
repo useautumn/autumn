9a058492974f8aaf18fdefdef24400af6dfccd6f
John Yeo <johnyeocx@gmail.com>
2026-01-15 18:07:17 +0000

    migrated most routes on apiRouter

--- server/src/internal/api/rewards/rewardRouter.ts
+++ server/src/internal/api/rewards/rewardRouter.ts
@@ -1,4 +1,5 @@
-import express, { type Router } from "express";
+import { Hono } from "hono";
+import type { HonoEnv } from "@/honoUtils/HonoEnv.js";
 import {
 	handleCreateCoupon,
 	handleDeleteCoupon,
@@ -6,14 +7,9 @@ import {
 	handleUpdateCoupon,
 } from "./handlers/rewards/index.js";
 
-const rewardRouter: Router = express.Router();
+export const rewardRouter = new Hono<HonoEnv>();
 
-rewardRouter.post("", handleCreateCoupon);
-
-rewardRouter.delete("/:id", handleDeleteCoupon);
-
-rewardRouter.post("/:internalId", handleUpdateCoupon);
-
-rewardRouter.get("/:id", handleGetCoupon);
-
-export default rewardRouter;
+rewardRouter.post("", ...handleCreateCoupon);
+rewardRouter.delete("/:id", ...handleDeleteCoupon);
+rewardRouter.post("/:internalId", ...handleUpdateCoupon);
+rewardRouter.get("/:id", ...handleGetCoupon);
