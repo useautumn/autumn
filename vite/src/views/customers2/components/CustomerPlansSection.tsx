d2f88962d2316273e3c94d499c13cd70211a9b5c
Ayush Rodrigues <joesj2905@gmail.com>
2026-01-16 22:48:17 +0000

    split complete

--- vite/src/views/customers2/components/CustomerPlansSection.tsx
+++ vite/src/views/customers2/components/CustomerPlansSection.tsx
@@ -1,11 +1,5 @@
 import { CustomerProductsTable } from "./table/customer-products/CustomerProductsTable";
-import { CustomerPurchasesTable } from "./table/customer-purchases/CustomerPurchasesTable";
 
 export function CustomerPlansSection() {
-	return (
-		<div className="flex flex-col gap-6">
-			<CustomerProductsTable />
-			<CustomerPurchasesTable />
-		</div>
-	);
+	return <CustomerProductsTable />;
 }
