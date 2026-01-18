59c7181b676c0d7c402624733815916b229a7a01
Ayush Rodrigues <joesj2905@gmail.com>
2026-01-18 14:43:13 +0000

    made it clear template prompts are templates

--- vite/src/views/onboarding4/components/TemplatePrompts.tsx
+++ vite/src/views/onboarding4/components/TemplatePrompts.tsx
@@ -7,23 +7,36 @@ interface TemplatePromptsProps {
 
 export function TemplatePrompts({ onSelectTemplate }: TemplatePromptsProps) {
 	return (
-		<div className="flex items-center justify-center gap-3 relative z-10">
-			{PRICING_TEMPLATE_PROMPTS.map((template) => (
-				<Button
-					key={template.id}
-					type="button"
-					onClick={() => onSelectTemplate({ prompt: template.prompt })}
-					variant="secondary"
-					className="h-9! px-3! gap-3"
-				>
-					<img
-						src={template.icon}
-						alt={template.label}
-						className="size-4 object-contain opacity-50 dark:invert"
-					/>
-					<span className="text-sm font-medium text-t2">{template.label}</span>
-				</Button>
-			))}
+		<div className="flex flex-col items-center gap-3 relative z-10 bg-background/50 rounded-2xl pt-2 pb-4 px-4 border-border/50">
+			{/* Header with decorative lines */}
+
+			<div className="flex items-center gap-3 w-full justify-center">
+				<div className="h-px w-8 bg-border border-border/50" />
+				<span className=" text-t4 font-normal text-xs">Copy a template</span>
+				<div className="h-px w-8 bg-border border-border/50" />
+			</div>
+
+			{/* Template cards */}
+			<div className="flex items-center justify-center gap-3 flex-wrap">
+				{PRICING_TEMPLATE_PROMPTS.map((template) => (
+					<Button
+						key={template.id}
+						type="button"
+						onClick={() => onSelectTemplate({ prompt: template.prompt })}
+						variant="secondary"
+						className="h-9! px-3! gap-3"
+					>
+						<img
+							src={template.icon}
+							alt={template.label}
+							className="size-4 object-contain opacity-50 dark:invert"
+						/>
+						<span className="text-sm font-medium text-t2">
+							{template.label}
+						</span>
+					</Button>
+				))}
+			</div>
 		</div>
 	);
 }
