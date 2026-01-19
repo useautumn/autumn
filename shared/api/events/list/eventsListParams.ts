16b083ff393899f8ed90599b98b2e70bc150d4bf
John Yeo <johnyeocx@gmail.com>
2026-01-19 19:39:43 +0000

    feat: allow no customer ID in list events

--- shared/api/events/list/eventsListParams.ts
+++ shared/api/events/list/eventsListParams.ts
@@ -4,11 +4,12 @@ import { createPaginationParamsSchema } from "../../common/pagePaginationSchemas
 export const ApiEventsListParamsSchema = createPaginationParamsSchema({
 	defaultLimit: 100,
 }).extend({
-	customer_id: z.string().describe("Filter events by customer ID"),
+	customer_id: z.string().optional().describe("Filter events by customer ID"),
 	feature_id: z
 		.string()
 		.min(1)
 		.or(z.array(z.string().min(1)))
+		.optional()
 		.describe("Filter by specific feature ID(s)"),
 
 	custom_range: z
