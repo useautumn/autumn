31b9db3ebf59084316706965045bf04897a5e4de
John Yeo <johnyeocx@gmail.com>
2026-01-26 18:22:31 +0000

    fix: auth client

--- vite/src/lib/auth-client.ts
+++ vite/src/lib/auth-client.ts
@@ -1,10 +1,11 @@
+import { oauthProviderClient } from "@better-auth/oauth-provider/client";
+import { createAuthClient } from "better-auth/client";
 import {
 	adminClient,
 	emailOTPClient,
-	oauthProviderClient,
 	organizationClient,
 } from "better-auth/client/plugins";
-import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_BACKEND_URL,
 	plugins: [
