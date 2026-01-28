41e2f2826cd70b6f37e523de4278c1b69e7df408
Charlie Lamb <charlielamb20@icloud.com>
2026-01-28 13:05:03 +0000

    chore: email path fix for deployed server

--- server/src/internal/emails/sendOTPEmail.ts
+++ server/src/internal/emails/sendOTPEmail.ts
@@ -1,7 +1,7 @@
-import OTPEmail from "@emails/otpemail.js";
 import { logger } from "@/external/logtail/logtailUtils.js";
 import { createResendCli } from "@/external/resend/resendUtils.js";
 import { FROM_AUTUMN } from "./constants.js";
+import OTPEmail from "./OTPEmail.js";
 
 const sendOTPEmail = async ({ email, otp }: { email: string; otp: string }) => {
 	if (!process.env.RESEND_API_KEY || !process.env.RESEND_DOMAIN) {
