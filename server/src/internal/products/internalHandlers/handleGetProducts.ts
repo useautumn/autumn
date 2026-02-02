db30de9fc44250c2df4091df7b59e5517e78c364
Charlie Lamb <charlielamb20@icloud.com>
2026-02-02 12:43:52 +0000

    chore: clean up cache logic

--- server/src/internal/products/internalHandlers/handleGetProducts.ts
+++ server/src/internal/products/internalHandlers/handleGetProducts.ts
@@ -1,13 +1,7 @@
 import { createRoute } from "@/honoMiddlewares/routeHandler";
 import { ProductService } from "@/internal/products/ProductService";
 import { getGroupToDefaults } from "@/internal/products/productUtils";
-import { sortFullProducts } from "@/internal/products/productUtils/sortProductUtils";
 import { mapToProductV2 } from "@/internal/products/productV2Utils";
-import { queryWithCache } from "@/utils/cacheUtils/queryWithCache";
-import {
-	buildProductsCacheKey,
-	PRODUCTS_CACHE_TTL,
-} from "../productCacheUtils";
 
 /**
  * GET /products/products
@@ -19,23 +13,9 @@ export const handleGetProducts = createRoute({
 	handler: async (c) => {
 		const { db, org, env, features } = c.get("ctx");
 
-		const products = await queryWithCache({
-			key: buildProductsCacheKey({ orgId: org.id, env, queryParams: {} }),
-			ttl: PRODUCTS_CACHE_TTL,
-			fn: async () => {
-				const prods = await ProductService.listFull({
-					db,
-					orgId: org.id,
-					env: env,
-				});
-				sortFullProducts({ products: prods });
-				return prods;
-			},
-		});
+		const products = await ProductService.listFull({ db, orgId: org.id, env });
 
-		const groupToDefaults = getGroupToDefaults({
-			defaultProds: products,
-		});
+		const groupToDefaults = getGroupToDefaults({ defaultProds: products });
 
 		return c.json({
 			products: products.map((p) =>
