e0df7a795046699d8a019bc6f6df39f5c194cbb1
Charlie Lamb <charlielamb20@icloud.com>
2026-02-03 17:40:53 +0000

    chore: improve skeleton for line items in checkout page

--- apps/checkout/src/components/checkout/order-summary/OrderSummarySkeleton.tsx
+++ apps/checkout/src/components/checkout/order-summary/OrderSummarySkeleton.tsx
@@ -15,9 +15,12 @@ function PlanGroupSectionSkeleton() {
 			</div>
 		{/* Line item */}
 		<div className="px-3 py-2">
-				<div className="flex items-center justify-between">
-					<Skeleton className="h-4 w-20" />
-					<Skeleton className="h-4 w-12" />
+				<div className="flex items-center justify-between gap-4">
+					<div className="flex flex-col gap-1.5 min-w-0">
+						<Skeleton className="h-2.5 w-20" />
+						<Skeleton className="h-2 w-32" />
+					</div>
+					<Skeleton className="h-2.5 w-12 shrink-0" />
 				</div>
 			</div>
 		</div>
