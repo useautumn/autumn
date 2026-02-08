fa6a09197441c65f15ed9a1cd7e7de428943dbe1
John Yeo <johnyeocx@gmail.com>
2026-02-07 19:58:05 -0800

    wip

--- scripts/testGroups/all.sh
+++ scripts/testGroups/all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 source "$(dirname "$0")/config.sh"
 
+export TEST_FILE_CONCURRENCY=2
+
 BUN_PARALLEL_V2 \
-  'integration/billing/update-subscription' \
+  'integration/billing/attach/scheduled-switch' \
+  # 'integration/cron' \
+  # 'integration/crud/customers' \
+  # 'integration/billing/migrations' \
   # 'integration/billing/stripe-webhooks' \
   # 'integration/billing/autumn-webhooks' \
-  # 'integration/billing/migrations' \
-  # 'integration/billing/cron' \
-  # 'integration/crud/customers' \
-  # 'integration/billing/attach' \
+  # 'integration/billing/update-subscription' \
 
 
 # 'integration/billing/attach' \
