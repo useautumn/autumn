0ab419bcc9ee206725dcfdd9144795345f548055
John Yeo <johnyeocx@gmail.com>
2026-02-11 07:54:11 -0800

    fix: inverted condition

--- server/src/external/stripe/createStripePrice/createStripePrice.ts
+++ server/src/external/stripe/createStripePrice/createStripePrice.ts
@@ -83,7 +83,7 @@ const checkCurStripePrice = async ({
 	}
 
 	let stripePrepaidPriceV2: Stripe.Price | undefined;
-	if (config.stripe_prepaid_price_v2_id) {
+	if (!config.stripe_prepaid_price_v2_id) {
 		stripePrepaidPriceV2 = undefined;
 	} else {
 		stripePrepaidPriceV2 = await getStripePrice({
