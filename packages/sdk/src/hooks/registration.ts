3541f5ea5e68845ac2b6ab798e8004ca79a935c7
John Yeo <johnyeocx@gmail.com>
2026-02-20 10:49:21 +0000

    chore: fix spekeasy cli installation

--- packages/sdk/src/hooks/registration.ts
+++ packages/sdk/src/hooks/registration.ts
@@ -6,7 +6,7 @@ import type { Hooks } from "./types.js";
  * in this file or in separate files in the hooks folder.
  */
 
-export function initHooks(hooks: Hooks) {
+export function initHooks(_hooks: Hooks) {
 	// Add hooks by calling hooks.register{ClientInit/BeforeCreateRequest/BeforeRequest/AfterSuccess/AfterError}Hook
 	// with an instance of a hook that implements that specific Hook interface
 	// Hooks are registered per SDK instance, and are valid for the lifetime of the SDK instance
