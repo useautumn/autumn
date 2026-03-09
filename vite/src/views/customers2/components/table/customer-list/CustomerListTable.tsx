2aac9b3a8b7a63a7476695a3dd934638d9858f69
Ayush Rodrigues <joesj2905@gmail.com>
2026-03-09 16:26:15 +0000

    docs wip

--- vite/src/views/customers2/components/table/customer-list/CustomerListTable.tsx
+++ vite/src/views/customers2/components/table/customer-list/CustomerListTable.tsx
@@ -168,7 +168,7 @@ export function CustomerListTable({
 							icon={<ArrowSquareOutIcon size={16} />}
 							onClick={() => {
 								window.open(
-									"https://docs.useautumn.com/documentation/getting-started/setup/react",
+									"https://docs.useautumn.com/documentation/getting-started/setup",
 									"_blank",
 								);
 							}}
