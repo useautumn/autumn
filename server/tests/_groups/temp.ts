9d998e4ab3eec6435066952af36172a84960919a
John Yeo <johnyeocx@gmail.com>
2026-03-13 11:51:42 +0000

    updated temp

--- server/tests/_groups/temp.ts
+++ server/tests/_groups/temp.ts
@@ -5,16 +5,14 @@ export const temp: TestGroup = {
 	description: "Default product behavior verification tests",
 	tier: "domain",
 	paths: [
-		// Cancel immediately with default tests
-
-		"attach/carry-over-balances",
-		"attach/carry-over-usages",
-		"integration/billing/attach/checkout/autumn-checkout/autumn-checkout-basic.test.ts",
-		"integration/billing/attach/immediate-switch/paid-features/immediate-switch-prepaid-no-options-basic.test.ts",
-		"integration/billing/attach/immediate-switch/paid-features/immediate-switch-prepaid-no-options-advanced.test.ts",
-		"integration/billing/attach/free-trial/override/trial-override-basic.test.ts",
-		"integration/billing/attach/free-trial/override/trial-override-merge.test.ts",
-		"integration/billing/attach/free-trial/trial-upgrade.test.ts",
-		"integration/billing/attach/free-trial/trial-entity-upgrade.test.ts",
+		"integration/billing/attach/carry-over-balances",
+		"integration/billing/attach/carry-over-usages",
+		// "integration/billing/attach/checkout/autumn-checkout/autumn-checkout-basic.test.ts",
+		// "integration/billing/attach/immediate-switch/paid-features/immediate-switch-prepaid-no-options-basic.test.ts",
+		// "integration/billing/attach/immediate-switch/paid-features/immediate-switch-prepaid-no-options-advanced.test.ts",
+		// "integration/billing/attach/free-trial/override/trial-override-basic.test.ts",
+		// "integration/billing/attach/free-trial/override/trial-override-merge.test.ts",
+		// "integration/billing/attach/free-trial/trial-upgrade.test.ts",
+		// "integration/billing/attach/free-trial/trial-entity-upgrade.test.ts",
 	],
 };
