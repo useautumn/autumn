10509bfa208825d4f5992d11e1b9f127ff51a4c2
Ridhwan Hussain <73362400+TheUntraceable@users.noreply.github.com.>
2026-03-18 18:44:16 +0000

    Remove prefix whitespace

--- apps/docs/mintlify/components/dynamic-param-field.jsx
+++ apps/docs/mintlify/components/dynamic-param-field.jsx
@@ -1,4 +1,4 @@
- import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 /**
  * A wrapper around Mintlify's ParamField that dynamically switches
