386f8daa913bd9712dd89b5c1c3816b5bf30cf7a
Owen Greenhalgh <og2701@hotmail.com>
2026-03-19 11:50:39 +0000

    add upper bound % check

--- shared/api/billingControls/usageAlert.ts
+++ shared/api/billingControls/usageAlert.ts
@@ -1,6 +1,10 @@
 import type { z } from "zod/v4";
-import { DbUsageAlertSchema } from "../../models/cusModels/billingControls/usageAlert.js";
+import {
+	DbUsageAlertSchema,
+	UsageAlertThresholdType,
+} from "../../models/cusModels/billingControls/usageAlert.js";
 
 export const ApiUsageAlertSchema = DbUsageAlertSchema;
+export { UsageAlertThresholdType };
 
 export type ApiUsageAlert = z.infer<typeof ApiUsageAlertSchema>;
