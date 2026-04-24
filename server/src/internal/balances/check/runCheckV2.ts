d48504d44cc653554aa12e096eed6307330a33c6
Charlie Lamb <charlielamb20@icloud.com>
2026-04-24 20:05:29 +0100

    chore: fix unit tests

--- server/src/internal/balances/check/runCheckV2.ts
+++ server/src/internal/balances/check/runCheckV2.ts
@@ -5,19 +5,33 @@ import { getCheckDataV2 } from "./getCheckDataV2.js";
 import { getCheckResponseV2 } from "./getCheckResponseV2.js";
 import { runCheckWithTrackV2 } from "./runCheckWithTrackV2.js";
 
+type RunCheckV2Deps = {
+	getCheckDataV2: typeof getCheckDataV2;
+	getCheckResponseV2: typeof getCheckResponseV2;
+	runCheckWithTrackV2: typeof runCheckWithTrackV2;
+};
+
+const defaultDeps: RunCheckV2Deps = {
+	getCheckDataV2,
+	getCheckResponseV2,
+	runCheckWithTrackV2,
+};
+
 export const runCheckV2 = async ({
 	ctx,
 	body,
 	requiredBalance,
+	deps = defaultDeps,
 }: {
 	ctx: AutumnContext;
 	body: ParsedCheckParams;
 	requiredBalance: number;
+	deps?: RunCheckV2Deps;
 }): Promise<{
 	checkData: CheckDataV2;
 	response: CheckResponseV3;
 }> => {
-	const checkData = await getCheckDataV2({
+	const checkData = await deps.getCheckDataV2({
 		ctx,
 		body: body as ParsedCheckParams & { feature_id: string },
 		requiredBalance,
@@ -25,13 +39,13 @@ export const runCheckV2 = async ({
 
 	const response =
 		body.send_event || body.lock?.enabled
-			? await runCheckWithTrackV2({
+			? await deps.runCheckWithTrackV2({
 					ctx,
 					body,
 					requiredBalance,
 					checkData,
 				})
-			: await getCheckResponseV2({
+			: await deps.getCheckResponseV2({
 					checkData,
 					requiredBalance,
 				});
