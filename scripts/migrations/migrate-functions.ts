2c4cead5ca98b93706f93adad40afeb2c245745a
John Yeo <johnyeocx@gmail.com>
2026-04-26 22:32:13 +0100

    chore: update DB URL

--- scripts/migrations/migrate-functions.ts
+++ scripts/migrations/migrate-functions.ts
@@ -8,7 +8,7 @@ export const migrateFunctions = async () => {
 	const { initializeDatabaseFunctions } = await import(
 		"@server/db/initializeDatabaseFunctions"
 	);
-	const databaseUrl = process.env.DATABASE_URL;
+	const databaseUrl = process.env.DATABASE_V2_URL;
 
 	if (databaseUrl?.includes("us-west-3")) {
 		const { confirm } = await inquirer.prompt([
