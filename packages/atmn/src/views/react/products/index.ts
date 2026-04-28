aa370ccf84363b9f27f3d948eaef6535aca47a3d
Charlie Lamb <charlielamb20@icloud.com>
2026-04-28 11:42:07 +0100

    chore: knippy knip knip

--- packages/atmn/src/views/react/products/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProductsView } from "./ProductsView.js";
-export type { ProductsViewProps } from "./ProductsView.js";
-export { ProductSheet } from "./components/index.js";
-export type { ProductSheetProps } from "./components/index.js";
--- packages/atmn/src/views/react/push/index.ts
+++
@@ -1,1 +0,0 @@
-export { PushView } from "./Push.js";
