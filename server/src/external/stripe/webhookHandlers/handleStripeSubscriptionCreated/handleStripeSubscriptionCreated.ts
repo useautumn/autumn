20f8bd7bc0d46b01d07733dd423b02ac2fc27c14
Charlie Lamb <charlielamb20@icloud.com>
2026-05-05 13:38:19 +0100

    chore: subscription logic cleanup

--- server/src/external/stripe/webhookHandlers/handleStripeSubscriptionCreated/handleStripeSubscriptionCreated.ts
+++ server/src/external/stripe/webhookHandlers/handleStripeSubscriptionCreated/handleStripeSubscriptionCreated.ts
@@ -1,5 +1,3 @@
-import type Stripe from "stripe";
-import { getFullStripeSub } from "../../stripeSubUtils.js";
 import type { StripeWebhookContext } from "../../webhookMiddlewares/stripeWebhookContext.js";
 import { setupStripeSubscriptionCreatedContext } from "./setupStripeSubscriptionCreatedContext.js";
 import { autoSyncFromSubscription } from "./tasks/autoSyncFromSubscription.js";
@@ -10,21 +8,14 @@ export const handleStripeSubscriptionCreated = async ({
 }: {
 	ctx: StripeWebhookContext;
 }) => {
-	const { stripeCli, stripeEvent } = ctx;
-	const stripeObject = stripeEvent.data.object as Stripe.Subscription;
-	const subscription = await getFullStripeSub({
-		stripeCli,
-		stripeId: stripeObject.id,
-	});
-
-	await linkScheduledCustomerProductsToSubscription({ ctx, subscription });
-
 	const subscriptionCreatedContext =
-		await setupStripeSubscriptionCreatedContext({
-			ctx,
-			subscription,
-		});
+		await setupStripeSubscriptionCreatedContext({ ctx });
 	if (!subscriptionCreatedContext) return;
+
+	await linkScheduledCustomerProductsToSubscription({
+		ctx,
+		subscription: subscriptionCreatedContext.subscription,
+	});
 
 	await autoSyncFromSubscription({ ctx, subscriptionCreatedContext });
 };
