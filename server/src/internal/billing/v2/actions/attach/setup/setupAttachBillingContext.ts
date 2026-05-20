4268c652b0ae018cb327cf219ebfcc6abb9c4f80
johnyeo <johnyeocx@gmail.com>
2026-05-20 12:31:53 +0100

    fix: tax rate ID

--- server/src/internal/billing/v2/actions/attach/setup/setupAttachBillingContext.ts
+++ server/src/internal/billing/v2/actions/attach/setup/setupAttachBillingContext.ts
@@ -299,6 +299,7 @@ export const setupAttachBillingContext = async ({
 			params.success_url ?? orgToReturnUrl({ org: ctx.org, env: ctx.env }),
 		checkoutSessionParams: params.checkout_session_params,
 		userMetadata: params.metadata,
+		taxRateId: params.tax_rate_id,
 
 		externalId: params.subscription_id,
 
