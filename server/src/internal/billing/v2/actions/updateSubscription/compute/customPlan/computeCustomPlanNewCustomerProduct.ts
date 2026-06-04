88154ba5b837732f38ae2c98839694127c411e68
johnyeo <johnyeocx@gmail.com>
2026-06-04 19:14:57 +0100

    fix: carry over status

--- server/src/internal/billing/v2/actions/updateSubscription/compute/customPlan/computeCustomPlanNewCustomerProduct.ts
+++ server/src/internal/billing/v2/actions/updateSubscription/compute/customPlan/computeCustomPlanNewCustomerProduct.ts
@@ -90,7 +90,7 @@ export const computeCustomPlanNewCustomerProduct = ({
 				? { subscriptionId: params.processor_subscription_id }
 				: {}),
 
-			...(params.status ? { status: params.status } : {}),
+			status: params.status ?? currentCustomerProduct.status,
 
 			onTrialEnd:
 				trialContext?.onEnd ??
