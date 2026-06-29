9e727d7c65d09faabad190d23aa44087c0954aea
Charlie Lamb <charlielamb20@icloud.com>
2026-06-29 21:03:31 +0100

    fix useListPlans query result type

--- packages/autumn-js/src/react/hooks/useListPlans.ts
+++ packages/autumn-js/src/react/hooks/useListPlans.ts
@@ -1,19 +1,19 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import type { Plan } from "@useautumn/sdk";
+import type { ListPlansList } from "@useautumn/sdk";
 import type { ListPlansParams } from "../../types";
 import { useAutumnClient } from "../AutumnContext";
 import type { AutumnClientError } from "../client/AutumnClientError";
 import type { HookParams } from "./types";
 
-export type UseListPlansParams = HookParams<ListPlansParams, Plan[]>;
+export type UseListPlansParams = HookParams<ListPlansParams, ListPlansList[]>;
 
 export const useListPlans = (params: UseListPlansParams = {}) => {
 	const client = useAutumnClient({ caller: "useListPlans" });
 	const { queryOptions, ...listPlansParams } = params;
 
-	return useQuery<Plan[], AutumnClientError>({
+	return useQuery<ListPlansList[], AutumnClientError>({
 		queryKey: ["autumn", "plans", listPlansParams],
 		queryFn: async () => {
 			const response = await client.listPlans(listPlansParams);
