55218ee088ec3727a2d416ba903bfeb9bdc026b4
Charlie Lamb <charlielamb20@icloud.com>
2026-08-04 16:21:05 +0100

    feat: add referral program create contract to v2.3 spec

--- packages/openapi/v2.3/contracts/index.ts
+++ packages/openapi/v2.3/contracts/index.ts
@@ -76,6 +76,7 @@ import {
 	platformSyncRevenueCatContract,
 } from "./platformContract.js";
 import {
+	referralProgramsCreateContract,
 	referralsCreateCodeContract,
 	referralsRedeemCodeContract,
 	rewardsCreateContract,
@@ -155,6 +156,7 @@ export const v2_3ContractRouter = oc.router({
 	// Rewards & Referrals
 	referralsCreateCode: referralsCreateCodeContract,
 	referralsRedeemCode: referralsRedeemCodeContract,
+	referralProgramsCreate: referralProgramsCreateContract,
 	rewardsCreate: rewardsCreateContract,
 	rewardsList: rewardsListContract,
 	rewardsRedeemCode: rewardsRedeemCodeContract,
